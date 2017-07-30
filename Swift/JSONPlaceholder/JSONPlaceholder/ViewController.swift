e0ade5f7878187f08af6960964b4f93b9e2248e9
choonsiong <lee93287@gmail.com>
2017-07-31 04:22:23 +0800

    First commit

---
+++ Swift/JSONPlaceholder/JSONPlaceholder/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  JSONPlaceholder
+//
+//  Created by Lee Choon Siong on 2017/07/31.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let todoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
+        
+        guard let url = URL(string: todoEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest, completionHandler: {
+            _, _, _ in
+            print("\(#function) - completionHandler called")
+        })
+        
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
