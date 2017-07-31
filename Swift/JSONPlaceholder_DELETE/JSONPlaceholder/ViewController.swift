50db2f87e71036e59bc2853c4d75874c586db281
choonsiong <lee93287@gmail.com>
2017-07-31 19:23:44 +0800

    First commit

--- Swift/JSONPlaceholder_DELETE/JSONPlaceholder/ViewController.swift
+++ Swift/JSONPlaceholder_DELETE/JSONPlaceholder/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
         let firstTodoEndpoint = "https://jsonplaceholder.typicode.com/todos/1"
         
         guard let url = URL(string: firstTodoEndpoint) else {
@@ -22,12 +22,6 @@ class ViewController: UIViewController {
         let task = session.dataTask(with: urlRequest) { (data, response, error) in
             print("\(#function) - completionHandler called")
             
-            guard error == nil else {
-                print("Error: failed to DELETE on /todos/1")
-                print(error!)
-                return
-            }
-            
             guard let _ = data else {
                 print("Error: failed to call DELETE on /todos/1")
                 return
@@ -35,10 +29,5 @@ class ViewController: UIViewController {
         }
         
         task.resume()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
