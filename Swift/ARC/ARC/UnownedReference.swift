9e5cf61b81af63286c3f9dd572223e07d4ca9b75
choonsiong <lee93287@gmail.com>
2017-08-01 01:53:11 +0800

    First commit

---
+++ Swift/ARC/ARC/UnownedReference.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+class Customer {
+    let name: String
+    var card: CreditCard?
+    
+    init(name: String) {
+        self.name = name
+        print("\(#file): \(name) is being initialized")
+    }
+    
+    deinit {
+        print("\(#file): \(name) is being deinitialized")
+    }
+}
+
+class CreditCard {
+    let number: UInt64
+    unowned let customer: Customer
+    
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    
+    deinit {
+        print("\(#file): card \(number) is being deinitialized")
+    }
+}
