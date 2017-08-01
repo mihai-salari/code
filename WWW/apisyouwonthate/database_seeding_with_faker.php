ff2f5b25fea94da726b1017b58e045c3bb3964b7
choonsiong <lee93287@gmail.com>
2017-08-01 22:50:59 +0800

    First commit

---
+++ WWW/apisyouwonthate/database_seeding_with_faker.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'vendor/fzaninotto/faker/src/autoload.php';
+
+$faker = Faker\Factory::create();
+
+for ($i = 0; $i < 10; $i++) {
+	echo $faker->name, "<br />";
+	echo $faker->email, "<br />";
+	echo $i === 0 ? true : $faker->boolean, "<br />";
+	echo $faker->randomElement(['male', 'female', 'other']), "<br />";
+	echo $faker->numberBetween(-10, 10), "<br />";
+	//echo $faker->dateTimeBetween('-40 years', '-18 years')
+	echo $faker->boolean ? "{$faker->city}, {$faker->state}" : null, "<br />";
+	echo $faker->sentence(100), "<br />";
+	echo "<hr />";
+}
+?>
