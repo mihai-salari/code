9a8474617ade42ae5ae51f73eb24630de6a05a28
choonsiong <lee93287@gmail.com>
2017-08-02 06:53:12 +0800

    First commit

--- Swift/Subscript/SubscriptBasic.playground/Contents.swift
+++ Swift/Subscript/SubscriptBasic.playground/Contents.swift
@@ -1,2 +1,30 @@
 import Cocoa
 
+class Person {
+    let name: String
+    let age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+extension Person {
+    subscript(key: String) -> String? {
+        switch key {
+            case "name":
+            return name
+            case "age":
+            return String(age)
+        default:
+            return nil
+        }
+    }
+}
+
+let me = Person(name: "Foo", age: 30)
+
+print(me["name"])
+print(me["age"])
+print(me["gender"])
