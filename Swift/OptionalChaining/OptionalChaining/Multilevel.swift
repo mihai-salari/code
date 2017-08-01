1e692db767759a71d36b50d41e2daad47e52a748
choonsiong <lee93287@gmail.com>
2017-08-01 17:10:02 +0800

    First commit

---
+++ Swift/OptionalChaining/OptionalChaining/Multilevel.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+class House: Property {
+    var rooms = [Room]()
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        
+        set {
+            rooms[i] = newValue
+        }
+    }
+    
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    
+    var address: Address?
+}
+
+
+class Room {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber) \(street)"
+        } else if buildingNumber != nil {
+            return buildingNumber
+        } else {
+            return nil
+        }
+    }
+}
