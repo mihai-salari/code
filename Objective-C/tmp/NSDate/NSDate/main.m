8940be27af2add478fd2fc0faebb8b9a82bf32e8
choonsiong <lee93287@gmail.com>
2017-08-02 20:15:07 +0800

    First commit

--- Objective-C/tmp/NSDate/NSDate/main.m
+++ Objective-C/tmp/NSDate/NSDate/main.m
@@ -6,6 +6,9 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"This NSDate object lives at %p", now);
         NSLog(@"now is '%@'", now);
+        
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %.0f seconds since the start of 1970.", seconds);
     }
     
     return 0;
