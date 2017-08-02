0b1d58b52d1c8848654b94ce3ab2392c5097540b
choonsiong <lee93287@gmail.com>
2017-08-02 17:40:15 +0800

    First commit

--- Algorithm/Sorting-QuickSort/Sorting-QuickSort/main.swift
+++ Algorithm/Sorting-QuickSort/Sorting-QuickSort/main.swift
@@ -1,12 +1,21 @@
 import Foundation
 
+/*
+    Quick Sort - features a time complexity of 0(n log n) and applies a divide
+    and conquer strategy.
+ 
+    Quick sort applies a series of rules to "swap" pairs of values. Swap events
+    are performed in place so no additional structures are needed to process data.
+ 
+ */
+
 extension Array where Element: Comparable {
     mutating func quickSort() -> Array<Element> {
         func qSort(start startIndex: Int, _ pivot: Int) {
             if (startIndex < pivot) {
                 let iPivot = qPartition(start: startIndex, pivot)
-                qSort(start: startIndex, iPivot - 1)
-                qSort(start: iPivot + 1, pivot)
+                qSort(start: startIndex, iPivot - 1) // left side
+                qSort(start: iPivot + 1, pivot) // right side
             }
         }
         
@@ -21,13 +30,17 @@ extension Array where Element: Comparable {
         
         // compare range with pivot
         for currentIndex in wallIndex..<pivot {
-            print("current is: \(self[currentIndex]), pivot is: \(self[pivot])")
             print(self)
+            print("current is: \(self[currentIndex]), wallIndex is: \(wallIndex), pivot is: \(self[pivot])")
             
             if self[currentIndex] <= self[pivot] {
+                print("==> \(self[currentIndex]) <= \(self[pivot])")
+                
                 if wallIndex != currentIndex {
+                    print("==> wallIndex (\(wallIndex)) != currentIndex (\(currentIndex))")
+                    print("==> swapping \(self[currentIndex]) with \(self[wallIndex])")
                     swap(&self[currentIndex], &self[wallIndex])
-                    print("swapping > \(self)")
+                    print("==> array after swapped: \(self)")
                 }
                 
                 // advance wall
@@ -35,10 +48,15 @@ extension Array where Element: Comparable {
             }
         }
         
+        // current index meet pivot, prepare to swap wall with pivot
+        print("==> wallIndex is: \(wallIndex), current and pivot is: \(self[pivot])")
+        
         // move pivot to final position
         if wallIndex != pivot {
+            print("==> wallIndex != pivot")
+            print("==> swapping \(self[wallIndex]) with \(self[pivot])")
             swap(&self[wallIndex], &self[pivot])
-            print("swapping > \(self)")
+            print("==> array after swapped: \(self)")
         }
         
         return wallIndex
@@ -52,41 +70,67 @@ print(results)
 
 /*
  
- current is: 7, pivot is: 4
  [7, 2, 1, 6, 8, 5, 3, 4]
- current is: 2, pivot is: 4
+ current is: 7, wallIndex is: 0, pivot is: 4
  [7, 2, 1, 6, 8, 5, 3, 4]
- swapping > [2, 7, 1, 6, 8, 5, 3, 4]
- current is: 1, pivot is: 4
+ current is: 2, wallIndex is: 0, pivot is: 4
+ ==> 2 <= 4
+ ==> wallIndex (0) != currentIndex (1)
+ ==> swapping 2 with 7
+ ==> array after swapped: [2, 7, 1, 6, 8, 5, 3, 4]
  [2, 7, 1, 6, 8, 5, 3, 4]
- swapping > [2, 1, 7, 6, 8, 5, 3, 4]
- current is: 6, pivot is: 4
+ current is: 1, wallIndex is: 1, pivot is: 4
+ ==> 1 <= 4
+ ==> wallIndex (1) != currentIndex (2)
+ ==> swapping 1 with 7
+ ==> array after swapped: [2, 1, 7, 6, 8, 5, 3, 4]
  [2, 1, 7, 6, 8, 5, 3, 4]
- current is: 8, pivot is: 4
+ current is: 6, wallIndex is: 2, pivot is: 4
  [2, 1, 7, 6, 8, 5, 3, 4]
- current is: 5, pivot is: 4
+ current is: 8, wallIndex is: 2, pivot is: 4
  [2, 1, 7, 6, 8, 5, 3, 4]
- current is: 3, pivot is: 4
+ current is: 5, wallIndex is: 2, pivot is: 4
  [2, 1, 7, 6, 8, 5, 3, 4]
- swapping > [2, 1, 3, 6, 8, 5, 7, 4]
- swapping > [2, 1, 3, 4, 8, 5, 7, 6]
- current is: 2, pivot is: 3
+ current is: 3, wallIndex is: 2, pivot is: 4
+ ==> 3 <= 4
+ ==> wallIndex (2) != currentIndex (6)
+ ==> swapping 3 with 7
+ ==> array after swapped: [2, 1, 3, 6, 8, 5, 7, 4]
+ ==> wallIndex is: 3, current and pivot is: 4
+ ==> wallIndex != pivot
+ ==> swapping 6 with 4
+ ==> array after swapped: [2, 1, 3, 4, 8, 5, 7, 6]
  [2, 1, 3, 4, 8, 5, 7, 6]
- current is: 1, pivot is: 3
+ current is: 2, wallIndex is: 0, pivot is: 3
+ ==> 2 <= 3
  [2, 1, 3, 4, 8, 5, 7, 6]
- current is: 2, pivot is: 1
+ current is: 1, wallIndex is: 1, pivot is: 3
+ ==> 1 <= 3
+ ==> wallIndex is: 2, current and pivot is: 3
  [2, 1, 3, 4, 8, 5, 7, 6]
- swapping > [1, 2, 3, 4, 8, 5, 7, 6]
- current is: 8, pivot is: 6
+ current is: 2, wallIndex is: 0, pivot is: 1
+ ==> wallIndex is: 0, current and pivot is: 1
+ ==> wallIndex != pivot
+ ==> swapping 2 with 1
+ ==> array after swapped: [1, 2, 3, 4, 8, 5, 7, 6]
  [1, 2, 3, 4, 8, 5, 7, 6]
- current is: 5, pivot is: 6
+ current is: 8, wallIndex is: 4, pivot is: 6
  [1, 2, 3, 4, 8, 5, 7, 6]
- swapping > [1, 2, 3, 4, 5, 8, 7, 6]
- current is: 7, pivot is: 6
+ current is: 5, wallIndex is: 4, pivot is: 6
+ ==> 5 <= 6
+ ==> wallIndex (4) != currentIndex (5)
+ ==> swapping 5 with 8
+ ==> array after swapped: [1, 2, 3, 4, 5, 8, 7, 6]
  [1, 2, 3, 4, 5, 8, 7, 6]
- swapping > [1, 2, 3, 4, 5, 6, 7, 8]
- current is: 7, pivot is: 8
+ current is: 7, wallIndex is: 5, pivot is: 6
+ ==> wallIndex is: 5, current and pivot is: 6
+ ==> wallIndex != pivot
+ ==> swapping 8 with 6
+ ==> array after swapped: [1, 2, 3, 4, 5, 6, 7, 8]
  [1, 2, 3, 4, 5, 6, 7, 8]
+ current is: 7, wallIndex is: 6, pivot is: 8
+ ==> 7 <= 8
+ ==> wallIndex is: 7, current and pivot is: 8
  [1, 2, 3, 4, 5, 6, 7, 8]
  Program ended with exit code: 0
  
