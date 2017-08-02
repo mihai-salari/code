18f6d1a4c13ec6c00fb0cb7acfdc69b30e91a41d
choonsiong <lee93287@gmail.com>
2017-08-03 01:51:56 +0800

    First commit

--- Objective-C/tmp/NSDate/NSDate/main.m
+++ Objective-C/tmp/NSDate/NSDate/main.m
@@ -8,7 +8,19 @@ int main(int argc, const char * argv[]) {
         NSLog(@"now is '%@'", now);
         
         double seconds = [now timeIntervalSince1970];
-        NSLog(@"It has been %.0f seconds since the start of 1970.", seconds);
+        NSLog(@"It has been %.0f seconds since the start of 1970", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSLog(@"My calendar is %@", [cal calendarIdentifier]);
+        
+        unsigned long day = [cal ordinalityOfUnit:NSCalendarUnitDay
+                                           inUnit:NSCalendarUnitMonth
+                                          forDate:now];
+        
+        NSLog(@"This is day %lu of the month", day);
     }
     
     return 0;
