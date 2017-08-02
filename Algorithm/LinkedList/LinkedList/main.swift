a2764f8dbaf78d25a992e7f0a7c7ccc012665bb3
choonsiong <lee93287@gmail.com>
2017-08-02 18:21:10 +0800

    First commit

---
+++ Algorithm/LinkedList/LinkedList/main.swift
@@ -0,0 +1,100 @@
+import Foundation
+
+class LLNode<T> {
+    var key: T!
+    var next: LLNode?
+    var previous: LLNode?
+}
+
+public class LinkedList<T: Equatable> {
+    // new instance
+    private var head = LLNode<T>()
+    
+    // number of items
+    var count: Int {
+        if head.key == nil {
+            return 0
+        } else {
+            var current: LLNode = head
+            var x: Int = 1
+            
+            // cycle through items
+            while current.next != nil {
+                current = current.next!
+                x += 1
+            }
+            
+            return x
+        }
+    }
+    
+    // print keys for the class
+    func printAllKeys() {
+        var current: LLNode<T>! = head
+        
+        // assign next instance
+        while current != nil {
+            print("link item is: \(current.key)")
+            current = current.next
+        }
+    }
+    
+    // add item
+    func append(element key: T) {
+        guard head.key != nil else {
+            head.key = key
+            return
+        }
+        
+        var current: LLNode? = head
+        
+        // position node
+        while current != nil {
+            if current?.next == nil {
+                let childToUse = LLNode<T>()
+                
+                childToUse.key = key
+                childToUse.previous = current
+                current!.next = childToUse
+                break
+            } else {
+                current = current?.next
+            }
+        }
+    }
+    
+    // remove at specific index
+    func remove(at index: Int) {
+        // check empty conditions
+        if ((index < 0) || (index > (self.count - 1)) || (head.key == nil)) {
+            print("link index does not exist")
+            return
+        }
+        
+        var current: LLNode<T>? = head
+        var trailer: LLNode<T>?
+        var listIndex: Int = 0
+        
+        // determine if the removal is at the head
+        if index == 0 {
+            current = current?.next
+            head = current!
+            return
+        }
+        
+        // iterate through remaining items
+        while current != nil {
+            if listIndex == index {
+                // redirect the trailer and next pointers
+                trailer!.next = current?.next
+                current = nil
+                break
+            }
+            
+            // update assignments
+            trailer = current
+            current = current?.next
+            listIndex += 1
+        }
+    }
+}
