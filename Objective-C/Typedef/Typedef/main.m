6993200e5eab5cd07e9b8ebab9167ddbc197b08f
choonsiong <lee93287@gmail.com>
2017-08-06 03:34:58 +0800

    First commit

---
+++ Objective-C/Typedef/Typedef/main.m
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        typedef struct coordinate {
+            double x;
+            double y;
+        } Point;
+        
+        typedef int length;
+    }
+    
+    return 0;
+}
