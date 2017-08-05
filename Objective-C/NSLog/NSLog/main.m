67c1bbba8f85f6f76fb4131b2911e5603384d33c
choonsiong <lee93287@gmail.com>
2017-08-05 18:09:17 +0800

    First commit

--- Objective-C/NSLog/NSLog/main.m
+++ Objective-C/NSLog/NSLog/main.m
@@ -12,3 +12,18 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ 2017-08-05 18:08:26.591581+0800 NSLog[98840:6691022] Numbers:
+ A:         94
+ B:        123
+ 2017-08-05 18:08:26.591751+0800 NSLog[98840:6691022] Numbers:
+ A: 94
+ B: 123
+ 2017-08-05 18:08:26.591770+0800 NSLog[98840:6691022] Numbers:
+ A: 0000000094
+ B: 0000000123
+ Program ended with exit code: 0
+
+*/
