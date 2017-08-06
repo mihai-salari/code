88f132b549e7a36b40c1d21168fe12ae39bc6d70
choonsiong <lee93287@gmail.com>
2017-08-07 01:27:55 +0800

    First commit

---
+++ Objective-C/ClassProperties/ClassProperties/Person.m
@@ -0,0 +1,10 @@
+#import "Person.h"
+
+@implementation Person
+
+- (float)bodyMassIndex {
+    float h = [self heightInMeters];    
+    return [self weightInKilos] / (h * h);
+}
+
+@end
---
+++ Objective-C/ClassProperties/ClassProperties/main.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#import "Person.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        Person *foo = [[Person alloc] init];
+        foo.weightInKilos = 96;
+        foo.heightInMeters = 1.9;
+        
+        float height = foo.heightInMeters;
+        int weight = foo.weightInKilos;
+        
+        NSLog(@"Foo is %.2f meters tall and weighs %d kilograms", height, weight);
+        
+        float bmi = [foo bodyMassIndex];
+        NSLog(@"Foo has a BMI of %.1f", bmi);
+    }
+    return 0;
+}
