8617ddc61ae85e3e6462856fb27f00ce15fcdfef
choonsiong <lee93287@gmail.com>
2017-08-06 22:06:54 +0800

    First commit

--- Objective-C/ClassPerson/ClassPerson/main.m
+++ Objective-C/ClassPerson/ClassPerson/main.m
@@ -1,9 +1,19 @@
 #import <Foundation/Foundation.h>
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Person *foo = [[Person alloc] init];
+        [foo setWeightInKilos:96];
+        [foo setHeightInMeters:1.9];
+        
+        float height = [foo heightInMeters];
+        int weight = [foo weightInKilos];
+        NSLog(@"Foo is %.2f meters tall and weighs %d kilograms", height, weight);
+        
+        float bmi = [foo bodyMassIndex];
+        NSLog(@"Foo has a BMI of %.1f", bmi);
     }
+    
     return 0;
 }
