3e8e41b1c7d463aaa523932c9e1e390fad36c728
choonsiong <lee93287@gmail.com>
2017-08-06 22:02:40 +0800

    First commit

--- Objective-C/ClassPerson/ClassPerson/Person.h
+++ Objective-C/ClassPerson/ClassPerson/Person.h
@@ -2,4 +2,17 @@
 
 @interface Person : NSObject
 
+{
+    // Instance variables
+    float _heightInMeters;
+    int _weightInKilos;
+}
+
+// Instance methods
+- (float)heightInMeters;
+- (void)setHeightInMeters:(float)h;
+- (int)weightInKilos;
+- (void)setWeightInKilos:(int)w;
+- (float)bodyMassIndex;
+
 @end
--- Objective-C/ClassPerson/ClassPerson/Person.m
+++ Objective-C/ClassPerson/ClassPerson/Person.m
@@ -2,4 +2,24 @@
 
 @implementation Person
 
+- (float)heightInMeters {
+    return _heightInMeters;
+}
+
+- (void)setHeightInMeters:(float)h {
+    _heightInMeters = h;
+}
+
+- (int)weightInKilos {
+    return _weightInKilos;
+}
+
+- (void)setWeightInKilos:(int)w {
+    _weightInKilos = w;
+}
+
+- (float)bodyMassIndex {
+    return _weightInKilos / (_heightInMeters * _heightInMeters);
+}
+
 @end
