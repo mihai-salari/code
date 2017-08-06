2266c8f4068425c3ebbbfee87d4be3a8973bd0a4
choonsiong <lee93287@gmail.com>
2017-08-06 20:14:50 +0800

    First commit

--- Programs/InterestingName/InterestingName/main.m
+++ Programs/InterestingName/InterestingName/main.m
@@ -2,6 +2,48 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSString *properNamesFile = @"/usr/share/dict/propernames";
+        NSString *wordsFile = @"/usr/share/dict/words";
+        
+        // Read in a file as a huge string,
+        // ignoring the possibility of an error
+        NSString *nameString = [NSString stringWithContentsOfFile:properNamesFile
+                                                         encoding:NSUTF8StringEncoding
+                                                            error:NULL];
+        
+        //NSLog(@"nameString = %@", nameString);
+        
+        // Break it into an array of strings
+        NSArray *names = [nameString componentsSeparatedByString:@"\n"];
+        
+        // Go through the array one string at a time
+        for (NSString *n in names) {
+            // Look for the string "aa" in a case-insensitive manner
+            NSRange r = [n rangeOfString:@"AA" options:NSCaseInsensitiveSearch];
+            
+            // Was it found?
+            if (r.location != NSNotFound) {
+                NSLog(@"%@", n);
+            }
+        }
+        
+        // Find common names that are also regular words (lowercase)
+        NSString *wordString = [NSString stringWithContentsOfFile:wordsFile
+                                                         encoding:NSUTF8StringEncoding
+                                                            error:NULL];
+        NSArray *words = [wordString componentsSeparatedByString:@"\n"];
+        
+        for (NSString *w in words) {
+            //NSLog(@"word = %@", w);
+            
+            for (NSString *n in names) {
+                //NSLog(@"name = %@", n);
+                
+                if ([[n lowercaseString] isEqualToString:w]) {
+                    NSLog(@"word: %@, name: %@", w, n);
+                }
+            }
+        }
     }
     
     return 0;
