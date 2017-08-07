8e821ebba9be10e0c1a4b78e9c0c3b3c568c24bf
choonsiong <lee93287@gmail.com>
2017-08-08 01:41:44 +0800

    First commit

---
+++ Objective-C/NSStringMore/NSStringMore/main.m
@@ -0,0 +1,33 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSString *hello1 = @"Hello, World";
+        
+        NSString *hello2 = [NSString alloc];
+        hello2 = [hello2 initWithString:@"Hello, World"];
+        
+        NSString *hello3 = [NSString stringWithString:@"Hello, World"];
+        
+        NSLog(@"%@", hello1);
+        NSLog(@"%@", hello2);
+        NSLog(@"%@", hello3);
+        
+        NSLog(@"hello1 contains %lu characters", [hello1 length]);
+        
+        unichar ch = [hello1 characterAtIndex:1];
+        NSLog(@"The 2nd character in hello1 is '%C'", ch);
+        
+        NSRange range;
+        range.location = 0;
+        range.length = 5;
+        
+        NSLog(@"The first 5 characters in hello1 is '%@'", [hello1 substringWithRange:range]);
+        
+        NSLog(@"hello1 in lowercase: %@", [hello1 lowercaseString]);
+        
+        NSLog(@"hello1 is now: %@", [hello1 stringByReplacingOccurrencesOfString:@"World" withString:@"Objective-C"]);
+    }
+    
+    return 0;
+}
