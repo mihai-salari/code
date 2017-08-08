247a1f8a15c7ea47fd6f5ebbdb56f8a7a4ead637
choonsiong <lee93287@gmail.com>
2017-08-08 22:17:34 +0800

    First commit

--- Objective-C/NSDictionary/NSDictionary/main.m
+++ Objective-C/NSDictionary/NSDictionary/main.m
@@ -2,6 +2,18 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSDictionary *numberOfMoons = @{
+                                        @"Mercury": @0,
+                                        @"Venus": @0,
+                                        @"Earth": @1,
+                                        @"Mars": @2,
+                                        @"Jupiter": @67,
+                                        @"Saturn": @62,
+                                        @"Uranus": @27,
+                                        @"Neptune": @13,
+                                        };
+        
+        NSLog(@"Jupiter has %@ moons.", numberOfMoons[@"Jupiter"]);
     }
     
     return 0;
