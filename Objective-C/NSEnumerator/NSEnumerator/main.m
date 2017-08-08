d1a50f5c68caf72e5efdfb5be460306496fa7499
choonsiong <lee93287@gmail.com>
2017-08-08 17:41:30 +0800

    First commit

--- Objective-C/NSEnumerator/NSEnumerator/main.m
+++ Objective-C/NSEnumerator/NSEnumerator/main.m
@@ -2,6 +2,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSArray *names = [NSArray arrayWithObjects:@"Foo", @"Bar", @"Moo", nil];
+        NSEnumerator *enumerator = [names objectEnumerator];
+        NSString *name;
+        
+        while (name = [enumerator nextObject]) {
+            NSLog(@"Name: %@", name);
+        }
+        
+        NSEnumerator *reverseEnumerator = [names reverseObjectEnumerator];
+        
+        while (name = [reverseEnumerator nextObject]) {
+            NSLog(@"Name: %@", name);
+        }
     }
     
     return 0;
