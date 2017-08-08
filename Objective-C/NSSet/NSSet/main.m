b4303c5387207ee8a519990334a03db2a724be9a
choonsiong <lee93287@gmail.com>
2017-08-08 22:12:54 +0800

    First commit

--- Objective-C/NSSet/NSSet/main.m
+++ Objective-C/NSSet/NSSet/main.m
@@ -2,6 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSSet *_set = [[NSSet alloc] initWithObjects:@"Foo", @"Bar", nil];
+        
+        if ([_set containsObject:@"Foo"]) {
+            NSLog(@"I found Foo!");
+        }
     }
     
     return 0;
