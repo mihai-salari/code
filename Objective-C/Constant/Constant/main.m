085895211626e89aed8c6e538c8c375531d4974a
choonsiong <lee93287@gmail.com>
2017-08-09 17:02:39 +0800

    First commit

---
+++ Objective-C/Constant/Constant/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Constant
+//
+//  Created by Lee Choon Siong on 2017/08/09.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
