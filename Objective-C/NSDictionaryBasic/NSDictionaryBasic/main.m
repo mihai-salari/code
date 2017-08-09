7d36023c4a40117564d190b122b6c9d20327c266
choonsiong <lee93287@gmail.com>
2017-08-09 14:57:54 +0800

    First commit

--- Objective-C/NSDictionaryBasic/NSDictionaryBasic/main.m
+++ Objective-C/NSDictionaryBasic/NSDictionaryBasic/main.m
@@ -4,6 +4,15 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSDictionary *emptyDict = [NSDictionary dictionary]; // create empty dict
         NSLog(@"%@", emptyDict);
+        
+        NSDictionary *oneKeyOneValue = [NSDictionary dictionaryWithObject:@"Foo Bar" forKey:@"foo"];
+        NSLog(@"%@", oneKeyOneValue);
+        
+        NSArray *keys = [NSArray arrayWithObjects:@"Name", @"Age", @"Email", nil];
+        NSArray *values = [NSArray arrayWithObjects:@"Foo Bar", @40, @"foo.bar@example.com", nil];
+        NSDictionary *fooBar = [NSDictionary dictionaryWithObjects:values forKeys:keys];
+        
+        NSLog(@"%@", fooBar);
     }
     
     return 0;
