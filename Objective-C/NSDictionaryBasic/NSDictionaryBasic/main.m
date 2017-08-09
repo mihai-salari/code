b188d24331cc26ecd2d9fee7576489a1b76c34e7
choonsiong <lee93287@gmail.com>
2017-08-09 14:54:33 +0800

    First commit

---
+++ Objective-C/NSDictionaryBasic/NSDictionaryBasic/main.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSDictionary *emptyDict = [NSDictionary dictionary]; // create empty dict
+        NSLog(@"%@", emptyDict);
+    }
+    
+    return 0;
+}
