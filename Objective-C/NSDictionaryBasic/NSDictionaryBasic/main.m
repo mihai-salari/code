187ab2294e88799ea18f866a69219528e883881f
choonsiong <lee93287@gmail.com>
2017-08-09 15:06:44 +0800

    First commit

--- Objective-C/NSDictionaryBasic/NSDictionaryBasic/main.m
+++ Objective-C/NSDictionaryBasic/NSDictionaryBasic/main.m
@@ -13,6 +13,17 @@ int main(int argc, const char * argv[]) {
         NSDictionary *fooBar = [NSDictionary dictionaryWithObjects:values forKeys:keys];
         
         NSLog(@"%@", fooBar);
+        
+        NSDictionary *fooBar2 = [NSDictionary dictionaryWithObjectsAndKeys:@"Foo Bar", @"Name", @40, @"Age", nil];
+        NSLog(@"%@", fooBar2);
+        
+        NSLog(@"Number of items in fooBar: %lu", [fooBar count]);
+        
+        // perform multiple lookups in a single statement
+        NSArray *lookupKeys = [NSArray arrayWithObjects:@"Name", @"Email", @"Gender", @"Age", nil];
+        NSArray *lookupValues = [fooBar objectsForKeys:lookupKeys notFoundMarker:@"???"];
+        
+        NSLog(@"%@", lookupValues);
     }
     
     return 0;
