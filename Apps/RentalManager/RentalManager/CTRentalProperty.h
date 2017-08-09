175cd3edc8cfbf7a8d8605c966759426eef077ee
choonsiong <lee93287@gmail.com>
2017-08-09 20:21:02 +0800

    First commit

---
+++ Apps/RentalManager/RentalManager/CTRentalProperty.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+
+typedef enum CTPropertyType {
+    TownHouse,
+    Unit,
+    Mansion
+} CTPropertyType;
+
+@interface CTRentalProperty : NSObject {
+
+}
+
+@property (nonatomic) float rentalPrice;
+@property (nonatomic) NSString *address;
+@property (nonatomic) CTPropertyType propertyType;
+
+- (void)increaseRentalByPercent:(float)percent withMaximum:(float)max;
+- (void)decreaseRentalByPercent:(float)percent withMinimum:(float)min;
+
+@end
---
+++ Apps/RentalManager/RentalManager/CTRentalProperty.m
@@ -0,0 +1,5 @@
+#import "CTRentalProperty.h"
+
+@implementation CTRentalProperty
+
+@end
