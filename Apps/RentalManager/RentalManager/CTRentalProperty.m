77c895ec493e7962e8643f3dbe8ea7fc196d4592
choonsiong <lee93287@gmail.com>
2017-08-09 20:38:22 +0800

    First commit

--- Apps/RentalManager/RentalManager/CTRentalProperty.m
+++ Apps/RentalManager/RentalManager/CTRentalProperty.m
@@ -3,11 +3,13 @@
 @implementation CTRentalProperty
 
 - (void)increaseRentalByPercent:(float)percent withMaximum:(float)max {
-    NSLog(@"I will increase your rent!");
+    self.rentalPrice = self.rentalPrice * (100 + percent) / 100;
+    self.rentalPrice = fmin(self.rentalPrice, max);
 }
 
 - (void)decreaseRentalByPercent:(float)percent withMinimum:(float)min {
-    NSLog(@"I will decrease your rent!");
+    self.rentalPrice = self.rentalPrice * (100 - percent) / 100;
+    self.rentalPrice = fmax(self.rentalPrice, min);
 }
 
 @end
