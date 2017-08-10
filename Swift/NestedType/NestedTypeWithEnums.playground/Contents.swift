aec76369d295deb1198f94725dcc1e1139ef19fe
choonsiong <lee93287@gmail.com>
2017-08-10 21:51:22 +0800

    First commit

--- Swift/NestedType/NestedTypeWithEnums.playground/Contents.swift
+++ Swift/NestedType/NestedTypeWithEnums.playground/Contents.swift
@@ -1,2 +1,45 @@
 import Cocoa
 
+struct BlackjackCard {
+    enum Suit: Character {
+        case spades = "♠︎"
+        case hearts = "♥︎"
+        case diamonds = "♦︎"
+        case clubs = "♣︎"
+    }
+    
+    enum Rank: Int {
+        case two = 2
+        case three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
+        
+        struct Values {
+            let first: Int, second: Int?
+        }
+        
+        var values: Values {
+            switch self {
+            case .ace:
+                return Values(first: 1, second: 11)
+            case .jack, .queen, .king:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+    
+    let rank: Rank
+    let suit: Suit
+    
+    var desciption: String {
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        
+        return output
+    }
+}
