37b1897f9dc0aa13c85058bfe7657f4261c5cee3
choonsiong <lee93287@gmail.com>
2017-08-11 00:39:21 +0800

    First commit

--- Objective-C/Directories/Directories/main.m
+++ Objective-C/Directories/Directories/main.m
@@ -2,7 +2,24 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        // The function returns an array of paths
+        NSArray *desktops = NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, YES);
         
+        // The user has only one desktop directory
+        NSString *desktopPath = desktops[0];
+        NSLog(@"%@", desktopPath);
+        
+        NSArray *downloads = NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory, NSUserDomainMask, YES);
+        NSString *downloadPath = downloads[0];
+        NSLog(@"%@", downloadPath);
+        
+        /*NSArray *directories = @[NSApplicationDirectory, NSLibraryDirectory, NSUserDirectory, NSDocumentDirectory, NSDesktopDirectory, NSCachesDirectory, NSApplicationSupportDirectory, NSDownloadsDirectory, NSMoviesDirectory, NSMusicDirectory, NSPicturesDirectory, NSTrashDirectory];
+        
+        for (int i = 0; i < [directories count]; i++) {
+            NSArray *paths = NSSearchPathForDirectoriesInDomains(directories[i], NSUserDomainMask, YES);
+            NSString *path = paths[0];
+            NSLog(@"%@: %@", directories[i], path);
+        }*/
     }
     
     return 0;
