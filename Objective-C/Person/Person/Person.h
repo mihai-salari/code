4fae5efa3d15a84d177e3f6da6bfa0871586d7e1
choonsiong <lee93287@gmail.com>
2017-08-12 03:50:13 +0800

    First commit

--- Objective-C/Person/Person/Person.h
+++ Objective-C/Person/Person/Person.h
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    Male,
+    Female
+} Gender;
+
 @interface Person : NSObject
 
 @property (nonatomic, readonly) NSString *firstName;
@@ -7,6 +12,11 @@
 @property (nonatomic, readonly) NSDate *dateOfBirth;
 
 + (id)person;
+
+- (id)initWithName:(NSString *)name;
+- (id)initWithAge:(NSNumber *)age;
+- (id)initWithGender:(Gender)gender;
+- (id)initWithName:(NSString *)name age:(NSNumber *)age gender:(Gender)gender;
 
 - (void)saySomething:(NSString *)message;
 - (void)sayHello;
--- Objective-C/Person/Person/Person.m
+++ Objective-C/Person/Person/Person.m
@@ -1,9 +1,46 @@
 #import "Person.h"
 
+@interface Person()
+
+{
+    NSString *_fullName;
+    NSNumber *_age;
+    Gender gender;
+}
+
+
+@end
+
 @implementation Person
 
 + (id)person {
     return [[self alloc] init];
+}
+
+- (id)initWithName:(NSString *)name {
+    _fullName = name;
+    _age = 0;
+    gender = Male;
+    
+    return [self initWithName:_fullName age:_age gender:gender];
+}
+
+- (id)initWithAge:(NSNumber *)age {
+    _age = age;
+    _fullName = @"unknown";
+    gender = Male;
+    
+    return [self initWithName:_fullName age:_age gender:gender];
+}
+
+- (id)initWithName:(NSString *)name age:(NSNumber *)age gender:(Gender)gender {
+    Person *p = nil;
+    
+    if (nil == self) {
+        p = [[Person alloc] init];
+    }
+    
+    return p;
 }
 
 - (void)saySomething:(NSString *)message {
