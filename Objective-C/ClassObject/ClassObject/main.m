c979ea17a40ced40f442f007fb8233a0a4e52389
choonsiong <lee93287@gmail.com>
2017-08-12 02:21:29 +0800

    First commit

--- Objective-C/ClassObject/ClassObject/main.m
+++ Objective-C/ClassObject/ClassObject/main.m
@@ -1,7 +1,10 @@
 #import <Foundation/Foundation.h>
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        // Class object
+        NSLog(@"%@ at %p", [Person class], [Person class]);
     }
     
     return 0;
