274f422d132c060d29c636b383a313f6f2c5a594
choonsiong <lee93287@gmail.com>
2017-08-12 03:01:23 +0800

    First commit

--- Objective-C/Person/Person/main.m
+++ Objective-C/Person/Person/main.m
@@ -16,6 +16,14 @@ int main(int argc, const char * argv[]) {
         
         SmallPerson *sp2 = [SmallPerson person];
         [sp2 sayGoodbye];
+        
+        SmallPerson *sp3;
+        
+        if (nil == sp3) {
+            NSLog(@"Oh fuck, sp3 is nil");
+        } else {
+            NSLog(@"Oh fuck, sp3 is something");
+        }
     }
     
     return 0;
