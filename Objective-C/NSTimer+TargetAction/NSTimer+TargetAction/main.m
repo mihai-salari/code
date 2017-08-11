7be8dc47f7ece51267783ee957b9925c9af4fafa
choonsiong <lee93287@gmail.com>
2017-08-11 21:06:04 +0800

    First commit

--- Objective-C/NSTimer+TargetAction/NSTimer+TargetAction/main.m
+++ Objective-C/NSTimer+TargetAction/NSTimer+TargetAction/main.m
@@ -1,7 +1,16 @@
 #import <Foundation/Foundation.h>
+#import "Logger.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Logger *logger = [[Logger alloc] init];
+        NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2.0
+                                                          target:logger
+                                                        selector:@selector(updateLastTime:)
+                                                        userInfo:nil
+                                                         repeats:YES];
+        
+        [[NSRunLoop currentRunLoop] run];
     }
     
     return 0;
