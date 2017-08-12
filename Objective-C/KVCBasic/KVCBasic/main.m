0927229c0c7070b3b85d51adf9a17d2c16a0fa77
choonsiong <lee93287@gmail.com>
2017-08-12 18:36:23 +0800

    First commit

--- Objective-C/KVCBasic/KVCBasic/main.m
+++ Objective-C/KVCBasic/KVCBasic/main.m
@@ -1,7 +1,12 @@
 #import <Foundation/Foundation.h>
+#import "Dog.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Dog *dog = [[Dog alloc] init];
+        
+        [dog setValue:@42 forKey:@"number"];        
+        NSLog(@"Number of dog is: %@", [dog valueForKey:@"number"]);
     }
     
     return 0;
