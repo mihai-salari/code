6475df449e7d5e173213af5b157ae0945862847f
choonsiong <lee93287@gmail.com>
2017-08-14 00:41:26 +0800

    First commit

--- Objective-C/StaticAndDynamicTyping/StaticAndDynamicTyping/main.m
+++ Objective-C/StaticAndDynamicTyping/StaticAndDynamicTyping/main.m
@@ -3,6 +3,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSString *name = @"Foo Bar"; // static type
+        id fullName = @"Foo Bar"; // dynamic type with 'id'
     }
     
     return 0;
