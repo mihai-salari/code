fd771d10242e88dfb32d5329e5673357ee77a115
choonsiong <lee93287@gmail.com>
2017-08-14 00:18:14 +0800

    First commit

---
+++ C++/helloworld/helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Lee Choon Siong on 2017/08/14.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
