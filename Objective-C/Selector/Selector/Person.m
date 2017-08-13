b9d915bfe1e0568d80c1b213becbc60066102aee
choonsiong <lee93287@gmail.com>
2017-08-14 02:43:00 +0800

    First commit

--- Objective-C/Selector/Selector/Person.m
+++ Objective-C/Selector/Selector/Person.m
@@ -4,6 +4,7 @@
 
 - (void)printName {
     NSLog(@"My name is %@", self.name);
+    NSLog(@"Original message: %@", NSStringFromSelector(_cmd));
 }
 
 @end
--- Objective-C/Selector/Selector/main.m
+++ Objective-C/Selector/Selector/main.m
@@ -1,7 +1,13 @@
 #import <Foundation/Foundation.h>
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        SEL selPrintName = @selector(printName);
+                
+        Person *p = [Person new];
+        [p printName];
     }
+    
     return 0;
 }
