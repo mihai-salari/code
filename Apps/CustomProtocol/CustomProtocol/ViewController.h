ee46b5129a40a5428b0a555dbd21275add7e572f
choonsiong <lee93287@gmail.com>
2017-08-13 15:56:14 +0800

    First commit

---
+++ Apps/CustomProtocol/CustomProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomProtocol
+//
+//  Created by Lee Choon Siong on 2017/08/13.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Apps/CustomProtocol/CustomProtocol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomProtocol
+//
+//  Created by Lee Choon Siong on 2017/08/13.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
