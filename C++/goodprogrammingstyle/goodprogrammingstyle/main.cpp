0361f09a9300cf81e2a6d220e54a05600625bda9
choonsiong <lee93287@gmail.com>
2017-08-14 03:24:33 +0800

    First commit

---
+++ C++/goodprogrammingstyle/goodprogrammingstyle/main.cpp
@@ -0,0 +1,22 @@
+//
+//  goodprogrammingstyle - program to demonstrate good programming style
+//
+//  Author: cs
+//  Purpose: Demonstration of good commenting style
+//  Usage: -
+//  References: -
+//  File formats: -
+//  Restrictions: -
+//  Revision history: -
+//  Error handling: -
+//  Copyright and license: -
+//  Notes: -
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
