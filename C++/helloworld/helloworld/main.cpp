c883a5d7f05f3c43b723c0019ce5d1f304808380
choonsiong <lee93287@gmail.com>
2017-08-14 03:09:08 +0800

    First commit

--- C++/helloworld/helloworld/main.cpp
+++ C++/helloworld/helloworld/main.cpp
@@ -1,8 +1,18 @@
+/*
+ * helloworld - program to print out 'Hello, World!'.
+ *
+ * Author: cs
+ *
+ * Purpose: Demonstration of a simple program
+ *
+ * Usage: Run the program and the message appears
+ */
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
+    // tell the world 'hello'
     std::cout << "Hello, World!\n";
-    std::cout << "Hello, C++!\n";
     
     return 0;
 }
