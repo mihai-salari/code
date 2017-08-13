6743c1c589fdd3ded720888d153bb5b9ba3b3c01
choonsiong <lee93287@gmail.com>
2017-08-14 02:56:47 +0800

    First commit

--- Objective-C/Selector/Selector/main.m
+++ Objective-C/Selector/Selector/main.m
@@ -6,6 +6,7 @@ int main(int argc, const char * argv[]) {
         SEL selPrintName = @selector(printName);
                 
         Person *p = [Person new];
+        p.name = @"Foo";
         
         if ([p respondsToSelector:selPrintName]) {
             [p printName];
