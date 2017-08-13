a9eddd70101541bba982188f988f1ee65a59f2d1
choonsiong <lee93287@gmail.com>
2017-08-13 17:47:22 +0800

    First commit

--- Apps/CustomProtocol/CustomProtocol/main.m
+++ Apps/CustomProtocol/CustomProtocol/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  CustomProtocol
-//
-//  Created by Lee Choon Siong on 2017/08/13.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
