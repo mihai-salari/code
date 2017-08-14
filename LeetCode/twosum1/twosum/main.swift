888417fc0743cff58a4b07cb2addf1ed20e8fd9e
choonsiong <lee93287@gmail.com>
2017-08-14 21:13:46 +0800

    First commit

--- LeetCode/twosum1/twosum/main.swift
+++ LeetCode/twosum1/twosum/main.swift
@@ -17,16 +17,18 @@
 import Foundation
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    var result: [Int] = [-1, -1]
+    var result: [Int] = [-1, -1] // initialize result array with no match answer
     var found: Bool = false
     let numsSize = nums.count
     
-    var i = 0
+    var i = 0 // initialize index to 0
+    
     while i < nums.count {
-        result[0] = i
+        result[0] = i // assume current index is result
         
-        var j = i + 1
-        while j < numsSize {
+        var j = i + 1 // assign next index to j
+        
+        while j < numsSize { // iterate from j to last element in the array
             if nums[i] + nums[j] == target {
                 result[1] = j
                 
@@ -35,12 +37,15 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
                 return result
             }
             
+            // not possibel to find match with current j, so move to next
             j += 1
         }
         
+        // not possible to find match with current i, so move to next
         i += 1
     }
     
+    // not able to find any match
     if !found {
         result = [-1, -1] // No match
     }
