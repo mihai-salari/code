0159bb21db68ed50836b69ed60e18b06e56b1be4
choonsiong <lee93287@gmail.com>
2017-08-14 21:01:16 +0800

    First commit

--- C++/twobyteshort/twobyteshort/main.cpp
+++ C++/twobyteshort/twobyteshort/main.cpp
@@ -3,9 +3,21 @@
 short zip; // two bytes, range from -32768 to 32767
 
 int main(int argc, const char * argv[]) {
-    zip = 92126;
-    
+    zip = 32768; // overflow to -32768
     std::cout << zip << "\n";
+    
+    zip = 32769; // overflow to -32767
+    std::cout << zip << "\n";
+    
+    zip = 59359;
+    std::cout << zip << "\n";
+    
+    zip = 92126;
+    /*
+     92126 - 32767 = 59359
+     59359 - 32768 = 26591
+    std::cout << zip << "\n";
+     */
     
     return 0;
 }
