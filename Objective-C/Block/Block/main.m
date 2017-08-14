8c2b97a20fcb4ebd6dbaabeb421936ea4a26affe
choonsiong <lee93287@gmail.com>
2017-08-15 04:05:59 +0800

    First commit

--- Objective-C/Block/Block/main.m
+++ Objective-C/Block/Block/main.m
@@ -8,8 +8,17 @@ int main(int argc, const char * argv[]) {
         
         simpleBlock();
         
-        NSArray *blocks = @[simpleBlock]; // block is object, so we can store in array
+        NSArray *blocks = @[simpleBlock]; // block is object, we can store in array etc
         NSLog(@"block is %@", blocks[0]);
+        
+        void (^arrayBlock)(void) = blocks[0];
+        arrayBlock();
+        
+        double (^multiplyTwoValues)(double, double) = ^ double (double firstValue, double secondValue) {
+            return firstValue * secondValue;
+        };
+        
+        NSLog(@"9 x 9 = %g", multiplyTwoValues(9, 9));
     }
     
     return 0;
