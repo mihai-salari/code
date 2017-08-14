f7ecff70114fe513374e6bce7a907b41d5dc0984
choonsiong <lee93287@gmail.com>
2017-08-15 04:13:25 +0800

    First commit

--- Objective-C/Block/Block/main.m
+++ Objective-C/Block/Block/main.m
@@ -1,5 +1,16 @@
 #import <Foundation/Foundation.h>
 
+void testBlockInFunction() {
+    int number = 42;
+    
+    void (^testBlock)(void) = ^{
+        //printf("number is %d\n", number);
+        NSLog(@"number is %d", number);
+    };
+    
+    testBlock();
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         void (^simpleBlock)(void) = ^{
@@ -8,17 +19,22 @@ int main(int argc, const char * argv[]) {
         
         simpleBlock();
         
+        // Blocks is Objective-C object
         NSArray *blocks = @[simpleBlock]; // block is object, we can store in array etc
         NSLog(@"block is %@", blocks[0]);
         
         void (^arrayBlock)(void) = blocks[0];
         arrayBlock();
         
+        // Blocks can take arguments and return values
         double (^multiplyTwoValues)(double, double) = ^ double (double firstValue, double secondValue) {
             return firstValue * secondValue;
         };
         
         NSLog(@"9 x 9 = %g", multiplyTwoValues(9, 9));
+        
+        // Blocks can capture values from the enclosing scope
+        testBlockInFunction();        
     }
     
     return 0;
