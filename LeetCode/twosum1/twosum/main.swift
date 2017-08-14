960328041aafe9f7f331a2a8381594a82bffec56
choonsiong <lee93287@gmail.com>
2017-08-14 19:49:12 +0800

    First commit

--- LeetCode/twosum1/twosum/main.swift
+++ LeetCode/twosum1/twosum/main.swift
@@ -17,73 +17,36 @@
 import Foundation
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    let tmpNums = nums
-    let first = tmpNums[0]
-    var result = [Int]()
-    var count = 0
+    var result: [Int] = [-1, -1]
+    var found: Bool = false
+    let numsSize = nums.count
     
-    func r(_ nums: [Int], _ target: Int, _ result: inout [Int], _ count: inout Int) {
-        let tmpNums = nums
-        let first = tmpNums[0]
+    var i = 0
+    while i < nums.count {
+        result[0] = i
         
-        if (tmpNums.count - 1 == 0) { // only one element left, no need to go further
-            return
-        } else {
-            for n in 1...tmpNums.count-1 {
-                if (first + tmpNums[n]) == target {
-                    print("found!")
-                    result.append(count)
-                    result.append(count + n)
-                    
-                    break
-                }
+        var j = i + 1
+        while j < numsSize {
+            if nums[i] + nums[j] == target {
+                result[1] = j
+                
+                found = true
+                
+                return result
             }
+            
+            j += 1
         }
         
-        count += 1
-        
-        r(Array(nums.dropFirst(1)), target, &result, &count)
+        i += 1
     }
     
-    r(nums, target, &result, &count)
+    if !found {
+        result = [-1, -1] // No match
+    }
     
     return result
 }
 
 print(twoSum([9, 3, 1, -10, -2], 4))
 print(twoSum([3, 3], 6))
-
-/*
- 
- var result: [Int] = [-1, -1]
- var found: Bool = false
- let numsSize = nums.count
- 
- var i = 0
- while i < nums.count {
- result[0] = i
- 
- var j = i + 1
- while j < numsSize {
- if nums[i] + nums[j] == target {
- result[1] = j
- 
- found = true
- 
- return result
- }
- 
- j += 1
- }
- 
- i += 1
- }
- 
- if !found {
- result = [-1, -1] // No match
- }
- 
- return result
- }
- 
- */
