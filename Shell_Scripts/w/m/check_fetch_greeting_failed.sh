5025a08c124e396c100adef1c2225b83af9262cb
choonsiong <lee93287@gmail.com>
2017-08-14 14:45:54 +0800

    First commit

--- Shell_Scripts/w/m/check_fetch_greeting_failed.sh
+++ Shell_Scripts/w/m/check_fetch_greeting_failed.sh
@@ -9,14 +9,23 @@ if ! . $CONF_FILE; then
 	exit 1
 fi
 
+# grep the error message from all server.log files
 grep -i 'failed to fetch greeting' $JBOSS_SERVER_LOG* > $TMP_GREP_FILE
+
+# cut the needed fields
 cat $TMP_GREP_FILE | cut -d ' ' -f 1,2,9 > $TMP_RESULT_FILE
 
+# compare the latest grep result to previous one
 diff $RESULT_FILE $TMP_RESULT_FILE > $TMP_DIFF_RESULT
+
+# to remove the diff info message at the top of the file
 grep -i 'server.log' $TMP_DIFF_RESULT > $DIFF_RESULT
 
+# get number of lines in the diff result file, if 0 line means no new error found
 diff_line=$(wc -l "$DIFF_RESULT" | awk '{print $1}')
 
+# if number of lines is not 0, then we have new fetch error,
+# therefore, prepare the SMS content and fire out
 if [ $diff_line != "0" ]; then
 	SMS_TEXT="$SITE_NAME+Greeting+Error+$DATE+%A+$AS+Failed+to+fetch+greeting:+$diff_line"
 	
