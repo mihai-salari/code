cf4a6d167b061af6206886f4188084959c740158
choonsiong <lee93287@gmail.com>
2017-08-14 20:02:22 +0800

    First commit

--- LeetCode/twosum2/twosum/main.swift
+++ LeetCode/twosum2/twosum/main.swift
@@ -52,38 +52,3 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
 
 print(twoSum([9, 3, 1, -10, -2], 4))
 print(twoSum([3, 3], 6))
-
-/*
- 
- var result: [Int] = [-1, -1]
- var found: Bool = false
- let numsSize = nums.count
- 
- var i = 0
- while i < nums.count {
- result[0] = i
- 
- var j = i + 1
- while j < numsSize {
- if nums[i] + nums[j] == target {
- result[1] = j
- 
- found = true
- 
- return result
- }
- 
- j += 1
- }
- 
- i += 1
- }
- 
- if !found {
- result = [-1, -1] // No match
- }
- 
- return result
- }
- 
- */
