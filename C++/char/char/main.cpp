763c36939726512296904d06e418ccaf20fc5428
choonsiong <lee93287@gmail.com>
2017-08-15 01:59:55 +0800

    First commit

--- C++/char/char/main.cpp
+++ C++/char/char/main.cpp
@@ -3,13 +3,15 @@
 char char1;
 char char2;
 char char3;
+wchar_t char4; // wide character
 
 int main(int argc, const char * argv[]) {
     char1 = 'a';
     char2 = 'b';
     char3 = 'c';
+    char4 = L'☀︎';
     
-    std::cout << char1 << " " << char2 << " " << char3 << "\n";
+    std::cout << char1 << " " << char2 << " " << char3 << " " << char4 << "\n";
     
     return 0;
 }
