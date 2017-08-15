b468b7ad8d09ca09a6b8be91995cd488aa52890a
choonsiong <lee93287@gmail.com>
2017-08-15 18:24:02 +0800

    First commit

--- C++/readstring/readstring/main.cpp
+++ C++/readstring/readstring/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <string>
+
 int age;
+
+std::string name;
 
 int main(int argc, const char * argv[]) {
     std::cout << "Enter your age: ";
     std::cin >> age;
-
+    
     std::cout << "age = " << age << "\n";
-
+    
+    // reading string
+    std::getline(std::cin, name);
+    
+    std::cout << "name = " << name << "\n";
+    
     return 0;
 }
