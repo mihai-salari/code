1a2b2b7ca50636c1bd53f38584137ee484151b6c
choonsiong <lee93287@gmail.com>
2017-08-15 18:03:41 +0800

    First commit

--- C++/array/array/main.cpp
+++ C++/array/array/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+float data[5];
+float total;
+float average;
+
 int main(int argc, const char * argv[]) {
+    data[0] = 34.0;
+    data[1] = 27.0;
+    data[2] = 46.5;
+    data[3] = 82.0;
+    data[4] = 22.0;
     
+    total = data[0] + data[1] + data[2] + data[3] + data[4];
+    average = total / 5.0;
+    
+    std::cout << "Total: " << total << "\n";
+    std::cout << "Average: " << average << "\n";
     
     return 0;
 }
