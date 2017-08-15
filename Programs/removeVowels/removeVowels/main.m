59e807fd50d753abf59cfda28b57037e6dbc8537
choonsiong <lee93287@gmail.com>
2017-08-15 12:54:31 +0800

    First commit

--- Programs/removeVowels/removeVowels/main.m
+++ Programs/removeVowels/removeVowels/main.m
@@ -4,6 +4,9 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSArray *originalStrings = @[@"Kuala Lumpur", @"Johor", @"Penang", @"Sabah", @"Sarawak", @"Selangor"];
         NSLog(@"original strings: %@", originalStrings);
+        
+        NSMutableArray *devowelizedStrings = [NSMutableArray array];
+        NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
     }
     
     return 0;
