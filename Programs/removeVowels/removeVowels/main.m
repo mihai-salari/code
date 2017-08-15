634b89373f932fcc074d56d032c49e84908d4ddb
choonsiong <lee93287@gmail.com>
2017-08-15 12:53:15 +0800

    First commit

--- Programs/removeVowels/removeVowels/main.m
+++ Programs/removeVowels/removeVowels/main.m
@@ -2,7 +2,8 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        NSArray *originalStrings = @[@"Kuala Lumpur", @"Johor", @"Penang", @"Sabah", @"Sarawak", @"Selangor"];
+        NSLog(@"original strings: %@", originalStrings);
     }
     
     return 0;
