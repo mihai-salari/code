df052edcbcb6021f9054dc36b9a0965f41ed9d04
choonsiong <lee93287@gmail.com>
2017-08-15 12:59:51 +0800

    First commit

--- Programs/removeVowels/removeVowels/main.m
+++ Programs/removeVowels/removeVowels/main.m
@@ -7,6 +7,26 @@ int main(int argc, const char * argv[]) {
         
         NSMutableArray *devowelizedStrings = [NSMutableArray array];
         NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
+        
+        // Declare the block variable
+        void (^devowelizer)(id, NSUInteger, BOOL *);
+        
+        // Compose a block and assign it to the block variable
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            // Iterate over the array of vowels, replacing occurrences of each
+            // with an empty string
+            for (NSString *vowel in vowels) {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:vowel
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+            }
+            
+            [devowelizedStrings addObject:newString];
+        };
     }
     
     return 0;
