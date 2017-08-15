fc14c7007430d1b1bba6b38cf97c5c21d8bcbda9
choonsiong <lee93287@gmail.com>
2017-08-15 18:15:02 +0800

    First commit

--- C++/std+string/std+string/main.cpp
+++ C++/std+string/std+string/main.cpp
@@ -4,6 +4,9 @@
 std::string firstName;
 std::string lastName;
 std::string fullName;
+std::string newName;
+
+char firstCharacter;
 
 int main(int argc, const char * argv[]) {
     firstName = "Foo";
@@ -11,6 +14,16 @@ int main(int argc, const char * argv[]) {
     fullName = firstName + " " + lastName;
     
     std::cout << "Full Name = " << fullName << "\n";
+    
+    firstCharacter = firstName.at(0);
+    
+    std::cout << "First character of first name is '" << firstCharacter << "'.\n";
+    
+    std::cout << "First name is " << firstName.length() << " characters long.\n";
+    
+    newName = fullName.substr(4, 3); // position, length
+    
+    std::cout << "New name is " << newName << "\n";
     
     return 0;
 }
