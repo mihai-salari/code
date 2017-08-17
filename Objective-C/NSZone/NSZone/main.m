f5e0573f76a13b833211183024eb313152b5e919
choonsiong <lee93287@gmail.com>
2017-08-18 02:08:50 +0800

    First commit

--- Objective-C/NSZone/NSZone/main.m
+++ Objective-C/NSZone/NSZone/main.m
@@ -1,7 +1,16 @@
 #import <Foundation/Foundation.h>
 
+#define MB (1024 * 1024) // Megabytes
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSZone *myZone = NSCreateZone(2 * MB, 1 * MB, false);
+        
+        NSString *text = [[NSString allocWithZone:myZone] init];
+        text = @"hello";
+        
+        // or use default zone
+        NSZone *defaultZone = NSDefaultMallocZone();
     }
     
     return 0;
