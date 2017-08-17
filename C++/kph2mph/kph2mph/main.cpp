07d46a03440f3314d4332ee904b1f3df351701e3
choonsiong <lee93287@gmail.com>
2017-08-17 22:44:59 +0800

    First commit

--- C++/kph2mph/kph2mph/main.cpp
+++ C++/kph2mph/kph2mph/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
+    double kph;
+    double mph;
     
+    std::cout << "Enter speed in kph: ";
+    std::cin >> kph;
+    
+    mph = kph * 0.6213712;
+    
+    std::cout << "Speed is " << mph << "mph.\n";
     
     return 0;
 }
