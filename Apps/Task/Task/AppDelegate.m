2ada707e0e4a82406fa09dd6586bcbc6558dc351
choonsiong <lee93287@gmail.com>
2017-08-17 13:22:00 +0800

    First commit

--- Apps/Task/Task/AppDelegate.m
+++ Apps/Task/Task/AppDelegate.m
@@ -6,41 +6,47 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    CGRect windowFrame = [[UIScreen mainScreen] bounds];
+    // Create and configure the UIWindow instance
+    CGRect windowFrame = [[UIScreen mainScreen] bounds]; // get screen size
     UIWindow *mainWindow = [[UIWindow alloc] initWithFrame:windowFrame];
-    
-    UIViewController *mainVC = [[UIViewController alloc] init];
-    
     self.window = mainWindow;
-    self.window.rootViewController = mainVC;
     [self.window setBackgroundColor:[UIColor lightGrayColor]];
     
-    self.mainView = [[UIView alloc] initWithFrame:windowFrame];
+    // Create and configure the main view controller (we need one for the window's rootviewcontroller, since the default vc is deleted)
+    UIViewController *mainVC = [[UIViewController alloc] init];
+    self.window.rootViewController = mainVC;
+    
+    // Create and configure a main view as the parent for all other subviews
+    self.mainView = [[UIView alloc] initWithFrame:windowFrame]; // use the whole screen size
     [self.mainView setBackgroundColor:[UIColor whiteColor]];
     [self.window addSubview: self.mainView];
     
-    mainVC.view = self.mainView;
+    mainVC.view = self.mainView; // set the main view controller's view property to this main view
     
+    // Create the frames for our UI objects
     CGRect tableFrame = CGRectMake(0, 80, windowFrame.size.width, windowFrame.size.height - 100);
     CGRect fieldFrame = CGRectMake(20, 40, 200, 31);
     CGRect buttonFrame = CGRectMake(228, 40, 72, 31);
     
+    // Create and configure the UITableView instance
     self.taskTable = [[UITableView alloc] initWithFrame:tableFrame style:UITableViewStylePlain];
-    [self.taskTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+    [self.taskTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"]; // tell the table view which class to instantiate whenever it needs to create a new cell
     [self.mainView addSubview:self.taskTable];
     
+    // Create and configure the UITextField instance, where new tasks will be entered
     self.taskField = [[UITextField alloc] initWithFrame:fieldFrame];
     self.taskField.borderStyle = UITextBorderStyleRoundedRect;
     self.taskField.placeholder = @"Type a task, tap Insert";
     [self.mainView addSubview:self.taskField];
     
+    // Create and configure the UIButton instance
     self.insertButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     self.insertButton.frame = buttonFrame;
     [self.insertButton setTitle:@"Insert" forState:UIControlStateNormal];
     [self.mainView addSubview:self.insertButton];
     
+    // Put the window on the screen
     [self.window makeKeyAndVisible];
     
     return YES;
