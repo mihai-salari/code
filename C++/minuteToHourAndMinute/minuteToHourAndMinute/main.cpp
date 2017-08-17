584a4badf8aba5e0a5081dadeda4650d4cd01af7
choonsiong <lee93287@gmail.com>
2017-08-17 22:53:27 +0800

    First commit

---
+++ C++/minuteToHourAndMinute/minuteToHourAndMinute/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    double minutes;
+    int hour;
+    int minute;
+    
+    std::cout << "Enter time in minutes: ";
+    std::cin >> minutes;
+    
+    hour = minutes / 60;
+    minute = minutes - (hour * 60);
+    
+    std::cout << minutes << " is " << hour << " hour and " << minute << " minute.\n";
+    
+    return 0;
+}
