9d05375bb6e7db6b50b709914e5ebb3f255e7037
choonsiong <lee93287@gmail.com>
2017-08-17 12:43:59 +0800

    First commit

--- Apps/Task/Task/ViewController.h
+++ Apps/Task/Task/ViewController.h
@@ -2,6 +2,13 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void)addTask:(id)sender;
 
 @end
 
