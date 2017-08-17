63a81c729577d85e419048a249d46043f9a419aa
choonsiong <lee93287@gmail.com>
2017-08-17 15:33:15 +0800

    First commit

--- Objective-C/AddNewMethodAtRuntime/AddNewMethodAtRuntime/Person.m
+++ Objective-C/AddNewMethodAtRuntime/AddNewMethodAtRuntime/Person.m
@@ -2,4 +2,19 @@
 
 @implementation Person
 
+void aSimpleDynamicMethodIMP(id self, SEL _cmd) {
+    NSLog(@"You called a method named %@", NSStringFromSelector(_cmd));
+}
+
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    if (sel == @selector(aSimpleDynamicMethod)) {
+//        NSLog(@"Adding a method named %@ to class %@", NSStringFromSelector(sel), NSStringFromClass([self class]));
+        class_addMethod([self class], sel, (IMP)aSimpleDynamicMethodIMP, "v@:");
+        
+        return YES;
+    }
+    
+    return [super resolveInstanceMethod:sel];
+}
+
 @end
