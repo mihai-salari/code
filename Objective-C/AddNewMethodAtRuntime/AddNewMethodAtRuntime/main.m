3fa15a5778a3406cefe15966f9c5d06b841985c5
choonsiong <lee93287@gmail.com>
2017-08-17 16:07:07 +0800

    First commit

--- Objective-C/AddNewMethodAtRuntime/AddNewMethodAtRuntime/main.m
+++ Objective-C/AddNewMethodAtRuntime/AddNewMethodAtRuntime/main.m
@@ -3,8 +3,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        // We need to use id for dynamic type, else it will be type statically
         id p = [Person new];
         
+        // We cannot use following method call
+        //[p aSimpleDynamicMethod];
+        
+        // We need to use below method call to add a new method dynamically to runtime
+        // The method 'aSimpleDynamicMethod' is not declare anywhere in the interface
         [p performSelector:@selector(aSimpleDynamicMethod)];
         [p performSelector:@selector(aSimpleDynamicMethod)];
         [p performSelector:@selector(aSimpleDynamicMethod)];
