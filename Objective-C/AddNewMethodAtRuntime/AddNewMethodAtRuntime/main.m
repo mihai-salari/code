4171fbc3718f260d5a24c90e640fe843ff17f041
choonsiong <lee93287@gmail.com>
2017-08-17 15:22:44 +0800

    First commit

--- Objective-C/AddNewMethodAtRuntime/AddNewMethodAtRuntime/main.m
+++ Objective-C/AddNewMethodAtRuntime/AddNewMethodAtRuntime/main.m
@@ -1,9 +1,10 @@
 #import <Foundation/Foundation.h>
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Person *p = [Person new];
     }
+    
     return 0;
 }
