d5b04c3f0d3ad025d075339eed9a0b7df9b5ad3f
choonsiong <lee93287@gmail.com>
2017-08-17 12:51:03 +0800

    First commit

--- Apps/Task/Task/AppDelegate.h
+++ Apps/Task/Task/AppDelegate.h
@@ -3,6 +3,13 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (nonatomic) UITableView *taskTable;
+@property (nonatomic) UITextField *taskField;
+@property (nonatomic) UIButton *insertButton;
+
+@property (nonatomic) NSMutableArray *tasks;
+
+- (void)addTask:(id)sender;
 
 
 @end
