19f50b55e71317bfaddced15e12339ce3611ef6d
choonsiong <lee93287@gmail.com>
2017-08-18 17:10:40 +0800

    First commit

--- C/array/array/main.c
+++ C/array/array/main.c
@@ -7,5 +7,9 @@ int main(int argc, const char * argv[]) {
     // Error
     //number2 = number1;
     
+    number2[0] = number1[0];
+    number2[1] = number1[1];
+    number2[2] = number1[2];
+    
     return 0;
 }
