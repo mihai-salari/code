d41651f5359e6890823ed6095ef201bd46e3777b
choonsiong <lee93287@gmail.com>
2017-08-19 00:49:44 +0800

    First commit

--- C/macro/macro/main.c
+++ C/macro/macro/main.c
@@ -1,15 +1,6 @@
-//
-//  main.c
-//  macro
-//
-//  Created by Lee Choon Siong on 2017/08/19.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
     return 0;
 }
