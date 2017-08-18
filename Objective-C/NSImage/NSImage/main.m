9d4cd35a2fc02b11d999d94029617e540712f34b
choonsiong <lee93287@gmail.com>
2017-08-18 14:36:13 +0800

    First commit

---
+++ Objective-C/NSImage/NSImage/main.m
@@ -0,0 +1,14 @@
+#import <Cocoa/Cocoa.h>
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        id url = [NSURL URLWithString:@"https://pbs.twimg.com/media/DHccSZNUIAApnlS.jpg"];
+        id image = [[NSImage alloc] initWithContentsOfURL:url];
+        id tiff = [image TIFFRepresentation];
+        
+        [tiff writeToFile:@"/tmp/image.tiff" atomically: YES];
+    }
+    
+    return 0;
+}
