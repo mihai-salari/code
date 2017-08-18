025d3a4b63e4ee7b3685921a6072f5fdb001ad1e
choonsiong <lee93287@gmail.com>
2017-08-19 00:50:54 +0800

    First commit

--- C/macro/macro/main.c
+++ C/macro/macro/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+#define INCHES_PER_FOOT 12
+
 int main(int argc, const char * argv[]) {
+
+    printf("There are %d inches in a foot.\n", INCHES_PER_FOOT);
     
     return 0;
 }
