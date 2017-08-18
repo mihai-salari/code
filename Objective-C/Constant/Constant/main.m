f26b7257a98aefb049578411b12ecd63c8d6e48e
choonsiong <lee93287@gmail.com>
2017-08-18 14:47:37 +0800

    First commit

--- Objective-C/Constant/Constant/main.m
+++ Objective-C/Constant/Constant/main.m
@@ -4,7 +4,12 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        const int maximumAge = 100;
+        
+        NSLog(@"maximumAge = %i", maximumAge);
+        
         NSLog(@"\u03c0 is %f", M_PI);
+        
         NSLog(@"%d is larger", MYMAX(10, 12));
     }
     
