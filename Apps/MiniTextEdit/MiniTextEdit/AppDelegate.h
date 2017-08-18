9c484856f063fffa6410ff9bef3dd905295ec4d8
choonsiong <lee93287@gmail.com>
2017-08-18 14:23:38 +0800

    First commit

--- Apps/MiniTextEdit/MiniTextEdit/AppDelegate.h
+++ Apps/MiniTextEdit/MiniTextEdit/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  MiniTextEdit
-//
-//  Created by Lee Choon Siong on 2017/08/18.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
