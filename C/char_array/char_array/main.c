4892dd9e6e8dd269d6d66c85641fccbca9bf1fa8
choonsiong <lee93287@gmail.com>
2017-08-18 18:04:42 +0800

    First commit

--- C/char_array/char_array/main.c
+++ C/char_array/char_array/main.c
@@ -2,11 +2,13 @@
 
 int main(int argc, const char * argv[]) {
     char name[] = "Foo";
+    char cocoa[] = { 'C', 'o', 'c', 'o', 'a', '\0' };
     
     // cannot assign
     //name = "Bar";
     
     printf("Name is %s\n", name);
+    printf("Cocoa is %s\n", cocoa);
     
     return 0;
 }
