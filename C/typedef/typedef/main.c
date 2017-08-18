c5f4655c8130ef5b4677607e9368896971533cf9
choonsiong <lee93287@gmail.com>
2017-08-18 14:58:21 +0800

    First commit

---
+++ C/typedef/typedef/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    typedef int Age;
+    
+    Age foo = 33;
+    
+    printf("foo is %d yrs old.\n", foo);
+    
+    return 0;
+}
