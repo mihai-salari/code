b527ea5f26c4b37e8272ce7b1a3edb0d54a74651
choonsiong <lee93287@gmail.com>
2017-08-18 19:40:01 +0800

    First commit

--- C/pointer/pointer/main.c
+++ C/pointer/pointer/main.c
@@ -4,5 +4,8 @@ int main(int argc, const char * argv[]) {
     int number = 4;
     int *numberPtr = &number;
     
+    printf("number = %i\n", number);
+    printf("numberPtr = %i\n", *numberPtr);
+    
     return 0;
 }
