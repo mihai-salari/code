923194728abc8e6ee39b7c750fc3d02023298bac
choonsiong <lee93287@gmail.com>
2017-08-18 16:25:10 +0800

    First commit

---
+++ C/else_not_required/else_not_required/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int age = 38;
+    
+    if (age >= 40) {
+        printf("Middle age");
+    } else if (age >= 20) {
+        printf("Going to be middle age");
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
