d5342887560daff679d0ecf463ae564c888f4854
choonsiong <lee93287@gmail.com>
2017-08-20 03:22:24 +0800

    First commit

--- C/type_cast/type_cast/main.c
+++ C/type_cast/type_cast/main.c
@@ -1,15 +1,7 @@
-//
-//  main.c
-//  type_cast
-//
-//  Created by Lee Choon Siong on 2017/08/20.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
     return 0;
 }
