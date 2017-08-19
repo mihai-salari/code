8fb68b9bbb3a0fc33b4f7c5ac5ca3736e38311d0
choonsiong <lee93287@gmail.com>
2017-08-19 20:47:51 +0800

    First commit

--- Objective-C/tmp/FractionSimple/FractionSimple/main.m
+++ Objective-C/tmp/FractionSimple/FractionSimple/main.m
@@ -6,6 +6,8 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         int numerator = 1;
         int denominator = 3;
+        
+        NSLog(@"The fraction is %i/%i", numerator, denominator);
     }
     
     return 0;
