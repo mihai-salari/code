394053b7cdaeda57e2c6c93bb890541c40126cbc
choonsiong <lee93287@gmail.com>
2017-08-20 04:41:34 +0800

    First commit

--- Objective-C/tmp/Calculator/Calculator/main.m
+++ Objective-C/tmp/Calculator/Calculator/main.m
@@ -33,3 +33,14 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ 2017-08-20 04:40:56.453095+0800 Calculator[14094:736730] The final result is 50
+ 2017-08-20 04:40:56.453244+0800 Calculator[14094:736730] The reciprocal is 0.02
+ 2017-08-20 04:40:56.453268+0800 Calculator[14094:736730] The squared is 2500
+ 2017-08-20 04:40:56.453285+0800 Calculator[14094:736730] The final result is 0
+ 2017-08-20 04:40:56.453301+0800 Calculator[14094:736730] 150
+ Program ended with exit code: 0
+
+*/
