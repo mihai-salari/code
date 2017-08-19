b15766ae939180725fc1db06fa5d9fc2b6fa6313
choonsiong <lee93287@gmail.com>
2017-08-20 03:36:46 +0800

    First commit

--- Objective-C/tmp/Calculator/Calculator/main.m
+++ Objective-C/tmp/Calculator/Calculator/main.m
@@ -49,6 +49,15 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Calculator *cal = [[Calculator alloc] init];
+        
+        [cal setAccumulator:100.0];
+        [cal add:200];
+        [cal divide:15];
+        [cal subtract:10];
+        [cal multiply:5];
+        
+        NSLog(@"The final result is %g", [cal accumulator]);
     }
     
     return 0;
