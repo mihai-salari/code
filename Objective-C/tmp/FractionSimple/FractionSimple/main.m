ee34c802296ea1dbac4eb45cc2567ba9675ce049
choonsiong <lee93287@gmail.com>
2017-08-19 20:46:45 +0800

    First commit

---
+++ Objective-C/tmp/FractionSimple/FractionSimple/main.m
@@ -0,0 +1,11 @@
+// A simple program to work with fractions
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
