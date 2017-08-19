f7bc0a97fc61622d7d29ecba2ce61512c6596bb1
choonsiong <lee93287@gmail.com>
2017-08-20 03:35:26 +0800

    First commit

--- Objective-C/tmp/Calculator/Calculator/main.m
+++ Objective-C/tmp/Calculator/Calculator/main.m
@@ -1,5 +1,52 @@
 #import <Foundation/Foundation.h>
 
+@interface Calculator: NSObject
+
+- (double)accumulator;
+- (void)setAccumulator:(double)value;
+- (void)clear;
+
+- (void)add:(double)value;
+- (void)subtract:(double)value;
+- (void)multiply:(double)value;
+- (void)divide:(double)value;
+
+@end
+
+@implementation Calculator {
+    double accumulator;
+}
+
+- (double)accumulator {
+    return accumulator;
+}
+
+- (void)setAccumulator:(double)value {
+    accumulator = value;
+}
+
+- (void)clear {
+    accumulator = 0;
+}
+
+- (void)add:(double)value {
+    accumulator += value;
+}
+
+- (void)subtract:(double)value {
+    accumulator -= value;
+}
+
+- (void)multiply:(double)value {
+    accumulator *= value;
+}
+
+- (void)divide:(double)value {
+    accumulator /= value;
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     }
