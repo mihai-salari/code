26dfcd284a3894216f38752b45c1c044320e7825
choonsiong <lee93287@gmail.com>
2017-08-19 21:44:29 +0800

    First commit

--- Objective-C/tmp/FractionObject/FractionObject/main.m
+++ Objective-C/tmp/FractionObject/FractionObject/main.m
@@ -2,6 +2,33 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction: NSObject
+
+- (void)print;
+- (void)setNumerator:(int)n;
+- (void)setDenominator:(int)d;
+
+@end
+
+@implementation Fraction {
+    int numerator;
+    int denominator;
+}
+
+- (void)print {
+    printf("%i/%i\n", numerator, denominator);
+}
+
+- (void)setNumerator:(int)n {
+    
+}
+
+- (void)setDenominator:(int)d {
+    
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
