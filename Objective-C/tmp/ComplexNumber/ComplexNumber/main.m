8f61a4d35f353e91856549fda721fdf19af1ca00
choonsiong <lee93287@gmail.com>
2017-08-20 04:07:09 +0800

    First commit

--- Objective-C/tmp/ComplexNumber/ComplexNumber/main.m
+++ Objective-C/tmp/ComplexNumber/ComplexNumber/main.m
@@ -1,8 +1,12 @@
 #import <Foundation/Foundation.h>
+#import "ComplexNumber.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        ComplexNumber *c = [ComplexNumber new];
+        [c setReal:9];
+        [c setImaginary:10];
+        [c print];
     }
     
     return 0;
