a759525da0e56911266f2b3d8b4bd1f48372ddd6
choonsiong <lee93287@gmail.com>
2017-08-19 18:08:26 +0800

    First commit

--- Objective-C/HelloWorld/HelloWorld/main.m
+++ Objective-C/HelloWorld/HelloWorld/main.m
@@ -2,6 +2,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int number;
+        
+        printf("number = %i\n", number);
         
         char *name = "Foo Bar"; // C style string
         NSString *fullName = @"Foo Bar"; // Objective-C NSString
@@ -9,7 +12,7 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Hello, World!");
         
         printf("name is %s\n", name);
-        NSLog(@"fullName is %@", fullName);        
+        NSLog(@"fullName is %@", fullName);
     }
     
     return 0;
