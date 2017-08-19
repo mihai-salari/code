2a72d7bbb5fa3b22d3a5253a4d20dae4ba53bd3e
choonsiong <lee93287@gmail.com>
2017-08-19 20:51:44 +0800

    First commit

--- Objective-C/tmp/FractionSimple/FractionSimple/main.m
+++ Objective-C/tmp/FractionSimple/FractionSimple/main.m
@@ -7,10 +7,15 @@ int main(int argc, const char * argv[]) {
         // Without using OOP, we might need to create many different
         // numerator and denominator variables to represent
         // the values that are needed
+        
         int numerator = 1;
         int denominator = 3;
         
+        int numerator2 = 2;
+        int denominator2 = 9;
+        
         NSLog(@"The fraction is %i/%i", numerator, denominator);
+        NSLog(@"Another fraction is %i/%i", numerator2, denominator2);
     }
     
     return 0;
