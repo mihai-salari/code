6bf523eca581f19ad9a5a9a21f0f516223866898
choonsiong <lee93287@gmail.com>
2017-08-19 16:26:39 +0800

    First commit

--- Objective-C/HelloWorld/HelloWorld/main.m
+++ Objective-C/HelloWorld/HelloWorld/main.m
@@ -1,17 +1,11 @@
-//
-//  main.m
-//  HelloWorld
-//
-//  Created by Lee Choon Siong on 2017/08/19.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+
         NSLog(@"Hello, World!");
+        
     }
+    
     return 0;
 }
