de9b8cf7a7d99de70c8802383464db5eb0b8f5b6
choonsiong <lee93287@gmail.com>
2017-08-20 04:15:21 +0800

    First commit

--- Objective-C/tmp/Rectangle/Rectangle/main.m
+++ Objective-C/tmp/Rectangle/Rectangle/main.m
@@ -1,7 +1,15 @@
 #import <Foundation/Foundation.h>
+#import "Rectangle.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Rectangle *r = [Rectangle new];
+        
+        [r setWidth:20];
+        [r setHeight:5];
+        
+        NSLog(@"Rectangle r with size of %i x %i, area = %i", [r width], [r height], [r area]);
+        NSLog(@"Rectangle r with size of %i x %i, perimeter = %i", [r width], [r height], [r perimeter]);
     }
     
     return 0;
