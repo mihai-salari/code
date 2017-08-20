636e6000d573d0f43fdc8fa616bc51c8094abaaf
choonsiong <lee93287@gmail.com>
2017-08-20 18:31:34 +0800

    First commit

--- Objective-C/tmp/GreatestCommonDivisor/GreatestCommonDivisor/main.m
+++ Objective-C/tmp/GreatestCommonDivisor/GreatestCommonDivisor/main.m
@@ -10,6 +10,14 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"Please type in two nonnegative integers.");
         scanf("%u%u", &u, &v);
+        
+        while (v != 0) {
+            temp = u % v;
+            u = v;
+            v = temp;
+        }
+        
+        NSLog(@"The greatest common divisor is %u", u);
     }
     
     return 0;
