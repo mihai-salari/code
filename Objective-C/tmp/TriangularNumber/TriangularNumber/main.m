96f0056ad96592fa83f583d6b40b7da0d3fa155f
choonsiong <lee93287@gmail.com>
2017-08-20 15:44:11 +0800

    First commit

--- Objective-C/tmp/TriangularNumber/TriangularNumber/main.m
+++ Objective-C/tmp/TriangularNumber/TriangularNumber/main.m
@@ -2,7 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int n, triangularNumber = 0;
         
+        NSLog(@"Table of Triangular Numbers");
+        NSLog(@"n\tSum from 1 to n");
+        NSLog(@"----------------------------");
     }
     
     return 0;
