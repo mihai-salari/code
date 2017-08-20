0ca93a7df45258acbffdcd439427aa4f33494aa7
choonsiong <lee93287@gmail.com>
2017-08-20 19:21:13 +0800

    First commit

---
+++ Objective-C/tmp/ReverseDigits/ReverseDigits/main.m
@@ -0,0 +1,20 @@
+// Reverse the digits of a number
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int number, right_digit;
+        
+        printf("Enter a number: ");
+        scanf("%i", &number);
+        
+        while (number != 0) {
+            right_digit = number % 10;
+            printf("%i", right_digit);
+            number = number / 10;
+        }
+    }
+    
+    return 0;
+}
