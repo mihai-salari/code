60cfd84676e73ecced351ddcdc4230ec4458a067
choonsiong <lee93287@gmail.com>
2017-08-20 18:35:33 +0800

    First commit

---
+++ Programs/GreatestCommonDivisor/GreatestCommonDivisor/main.swift
@@ -0,0 +1,7 @@
+// Find the GCD of two non-negative integers U and V
+// Algorithm by Euclid
+// Step 1 - If v equals 0, then we are done and the gcd is equal to U
+// Step 2 - Calculate temp = u % v, u = v, v = temp and go back to step 1
+
+import Foundation
+
