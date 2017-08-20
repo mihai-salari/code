ff7748b9011deb1c7dc30e5fe0b39cafc837b8f9
choonsiong <lee93287@gmail.com>
2017-08-20 22:19:06 +0800

    First commit

--- Objective-C/tmp/SumOfDigits/SumOfDigits/main.m
+++ Objective-C/tmp/SumOfDigits/SumOfDigits/main.m
@@ -2,6 +2,26 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int digit = 0;
+        int remainder = 0;
+        int number = 0;
+        
+        printf("Give me a digit: ");
+        scanf("%i", &digit);
+        
+        if (digit < 10) {
+            printf("answer = %i\n", digit);
+            
+            return 0;
+        }
+        
+        do {
+            remainder = digit % 10;
+            digit = digit / 10;
+            number += remainder;
+        } while (remainder != 0);
+        
+        printf("answer = %i\n", number);
         
     }
     
