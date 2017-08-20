2bf68a9ec868265644524267f8e55daca17313c6
choonsiong <lee93287@gmail.com>
2017-08-20 16:02:08 +0800

    First commit

--- Objective-C/tmp/TriangularNumberNested/TriangularNumberNested/main.m
+++ Objective-C/tmp/TriangularNumberNested/TriangularNumberNested/main.m
@@ -2,7 +2,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int n, input, triangularNumber, count;
         
+        for (count = 1; count <= 5; ++count) {
+            printf("What triangular number do you want? ");
+            scanf("%i", &input);
+            
+            triangularNumber = 0;
+            
+            for (n = 1; n <= input; ++n) {
+                triangularNumber += n;
+            }
+            
+            NSLog(@"Triangular number %i is %i", input, triangularNumber);
+        }
     }
     
     return 0;
