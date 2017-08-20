098d293f253d7f9fb3323c6c2a2dd5ced6e29b41
choonsiong <lee93287@gmail.com>
2017-08-20 18:27:42 +0800

    First commit

--- Objective-C/tmp/GreatestCommonDivisor/GreatestCommonDivisor/main.m
+++ Objective-C/tmp/GreatestCommonDivisor/GreatestCommonDivisor/main.m
@@ -1,7 +1,12 @@
+// Find the GCD of two non-negative integers U and V.
+// 1: If v equals 0, then we are done and the gcd is equal to U.
+// 2: Calculate temp = u % v, u = v, v = temp and go back to step 1.
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
     }
     
     return 0;
