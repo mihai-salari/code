a2f40f1549acbc840b8a5c7e0fc53f8255cc9d07
choonsiong <lee93287@gmail.com>
2017-08-20 15:31:29 +0800

    First commit

--- Objective-C/tmp/TriangularNumberSimpleWay/TriangularNumberSimpleWay/main.m
+++ Objective-C/tmp/TriangularNumberSimpleWay/TriangularNumberSimpleWay/main.m
@@ -2,7 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int triangularNumber;
         
+        triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+        
+        NSLog(@"The eighth triangular number is %i", triangularNumber);
     }
     
     return 0;
