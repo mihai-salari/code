f44ddea564451803d1e6a3c180e1d8adc095125c
choonsiong <lee93287@gmail.com>
2017-08-22 02:20:37 +0800

    First commit

--- Objective-C/tmp/test5/test5/main.m
+++ Objective-C/tmp/test5/test5/main.m
@@ -2,6 +2,13 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int a, b;
+        
+        printf("Enter two numbers, separate by space: ");
+        scanf("%i%i", &a, &b);
+        
+        printf("You entered: %i %i\n", a, b);        
+        
     }
     
     return 0;
