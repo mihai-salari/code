d26617506e5f8520439a85c13f4e6e96b45c1371
choonsiong <lee93287@gmail.com>
2017-08-21 21:16:30 +0800

    First commit

--- Objective-C/tmp/PrimeNumber/PrimeNumber/main.m
+++ Objective-C/tmp/PrimeNumber/PrimeNumber/main.m
@@ -4,6 +4,23 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int p, d, isPrime;
+        
+        for (p = 2; p <= 100; p++) {
+            isPrime = 1;
+            
+            for (d = 2; d < p; d++) {
+                if (p % d == 0) {
+                    isPrime = 0;
+                }
+            }
+            
+            if (isPrime != 0) {
+                printf("%i ", p);
+            }
+        }
+        
+        printf("\n");
     }
     
     return 0;
