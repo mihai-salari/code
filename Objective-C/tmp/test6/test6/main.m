a9b1caa66239b49c449acc66a5377401924c0c73
choonsiong <lee93287@gmail.com>
2017-08-22 02:24:51 +0800

    First commit

---
+++ Objective-C/tmp/test6/test6/main.m
@@ -0,0 +1,24 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int a, b;
+        char c = 'n';
+        
+        while (c == 'y' || c == 'Y') {
+            printf("Enter two numbers, separate by space: ");
+            scanf("%i%i", &a, &b);
+            
+            if (a % b == 0) {
+                printf("%i is evenly divisible by %i\n", a, b);
+            } else {
+                printf("%i is not evenly divisible by %i\n", a, b);
+            }
+            
+            printf("Continue (y/n)? ");
+            scanf(" %c", &c);
+        }
+    }
+    
+    return 0;
+}
