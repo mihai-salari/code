149d30b07185c7d7d632d9b4e384ad5f505283d5
choonsiong <lee93287@gmail.com>
2017-08-21 17:08:26 +0800

    First commit

---
+++ Objective-C/tmp/SingleCharacter/SingleCharacter/main.m
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        char c;
+        
+        printf("Enter a single character: ");
+        scanf("%c", &c);
+        
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            printf("An alphabetic character\n");
+        } else if (c >= '0' && c <= '9') {
+            printf("A digit\n");
+        } else {
+            printf("A special character?\n");
+        }
+    }
+    
+    return 0;
+}
