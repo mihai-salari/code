db4a589b3ee91478f3b37e4f6bb698a2d06cdeb9
choonsiong <lee93287@gmail.com>
2017-08-22 02:50:28 +0800

    First commit

--- Programs/PrintingCalculator/PrintingCalculator/main.m
+++ Programs/PrintingCalculator/PrintingCalculator/main.m
@@ -3,9 +3,41 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        double number;
+        char operator;
+        bool firstTime = YES;
+        
+        Calculator *cal = [Calculator new];
+        
         printf("Begin Calculations");
         
-        
+        while (operator != 'E') {
+            scanf("%lf %c", &number, &operator);
+            
+            // Make sure user set accumulator before perform
+            // additional calculations
+            if (firstTime) {
+                if (operator != 'S') {
+                    printf("Set accumulator with command 'S'\n");
+                    printf("e.g. 10 S");
+                } else {
+                    firstTime = NO;
+                }
+            }
+            
+            switch (operator) {
+                case '+':
+                    [cal add:number];
+                    break;
+                case 'S':
+                    [cal setAccumulator:number];
+                    break;
+                default:
+                    break;
+            }
+            
+            printf("= %lf\n", [cal accumulator]);
+        }
         
         printf("End of Calculations");
     }
