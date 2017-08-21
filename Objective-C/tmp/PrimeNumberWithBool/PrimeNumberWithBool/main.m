ba74025370f7e4e66d606d821675660d7033756d
choonsiong <lee93287@gmail.com>
2017-08-21 22:55:15 +0800

    First commit

--- Objective-C/tmp/PrimeNumberWithBool/PrimeNumberWithBool/main.m
+++ Objective-C/tmp/PrimeNumberWithBool/PrimeNumberWithBool/main.m
@@ -4,20 +4,21 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int p, d, isPrime;
+        int p, d;
+        bool isPrime;
         
         for (p = 2; p <= 100; p++) {
-            isPrime = 1; // true
+            isPrime = YES; // true
             
             // a test to divide p by the integers 2 through p - 1
             for (d = 2; d < p; d++) {
                 // if the remainder is 0, it is not a prime
                 if (p % d == 0) {
-                    isPrime = 0; // false
+                    isPrime = NO; // false
                 }
             }
             
-            if (isPrime != 0) {
+            if (isPrime) {
                 printf("%i ", p);
             }
         }
