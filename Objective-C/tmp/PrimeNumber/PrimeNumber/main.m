777d05f120ee525007aeece15d85f61ebfa7e2b8
choonsiong <lee93287@gmail.com>
2017-08-21 21:13:47 +0800

    First commit

---
+++ Objective-C/tmp/PrimeNumber/PrimeNumber/main.m
@@ -0,0 +1,10 @@
+// Program to generate a table of prime numbers
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    }
+    
+    return 0;
+}
