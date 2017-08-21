7f852e68c7d6c25cfa74183fc2407d1c6eaf52ef
choonsiong <lee93287@gmail.com>
2017-08-21 15:32:58 +0800

    First commit

--- Objective-C/tmp/LeapYear/LeapYear/main.m
+++ Objective-C/tmp/LeapYear/LeapYear/main.m
@@ -1,3 +1,7 @@
+// A year is a leap year if it is evenly divisible by 4.
+// A year that is divisible by 100 is not a leap year unless it is also
+// divisible by 400.
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
