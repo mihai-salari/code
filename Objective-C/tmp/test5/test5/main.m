1d98df3eb1ab0fc34b0f583760399afdfddce1ce
choonsiong <lee93287@gmail.com>
2017-08-22 02:21:56 +0800

    First commit

--- Objective-C/tmp/test5/test5/main.m
+++ Objective-C/tmp/test5/test5/main.m
@@ -7,7 +7,13 @@ int main(int argc, const char * argv[]) {
         printf("Enter two numbers, separate by space: ");
         scanf("%i%i", &a, &b);
         
-        printf("You entered: %i %i\n", a, b);        
+        //printf("You entered: %i %i\n", a, b);
+        
+        if (a % b == 0) {
+            printf("%i is evenly divisible by %i\n", a, b);
+        } else {
+            printf("%i is not evenly divisible by %i\n", a, b);
+        }
         
     }
     
