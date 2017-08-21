0d498937f4cedd381d39a98b7e3b1e9815ebd0f0
choonsiong <lee93287@gmail.com>
2017-08-21 15:35:20 +0800

    First commit

--- Objective-C/tmp/LeapYear/LeapYear/main.m
+++ Objective-C/tmp/LeapYear/LeapYear/main.m
@@ -2,6 +2,15 @@
 // A year that is divisible by 100 is not a leap year unless it is also
 // divisible by 400.
 
+/* Algorithm:
+ 
+ if (year is not divisible by 4) then (it is a common year)
+ else if (year is not divisible by 100) then (it is a leap year)
+ else if (year is not divisible by 400) then (it is a common year)
+ else (it is a leap year)
+ 
+ */
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
