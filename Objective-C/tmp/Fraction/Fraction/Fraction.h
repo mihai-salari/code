66fa97424bfaaa7af54c6062dd604310dbf351c3
choonsiong <lee93287@gmail.com>
2017-08-22 20:17:25 +0800

    First commit

---
+++ Objective-C/tmp/Fraction/Fraction/Fraction.h
@@ -0,0 +1,13 @@
+//
+//  Fraction.h
+//  Fraction
+//
+//  Created by Lee Choon Siong on 2017/08/22.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fraction : NSObject
+
+@end
---
+++ Objective-C/tmp/Fraction/Fraction/Fraction.m
@@ -0,0 +1,13 @@
+//
+//  Fraction.m
+//  Fraction
+//
+//  Created by Lee Choon Siong on 2017/08/22.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import "Fraction.h"
+
+@implementation Fraction
+
+@end
