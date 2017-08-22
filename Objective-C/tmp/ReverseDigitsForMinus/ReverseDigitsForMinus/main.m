45f98d5907d112e7a322651694e3f85acaec7836
choonsiong <lee93287@gmail.com>
2017-08-22 15:35:02 +0800

    First commit

--- Objective-C/tmp/ReverseDigitsForMinus/ReverseDigitsForMinus/main.m
+++ Objective-C/tmp/ReverseDigitsForMinus/ReverseDigitsForMinus/main.m
@@ -1,17 +1,44 @@
-//
-//  main.m
-//  ReverseDigitsForMinus
-//
-//  Created by Lee Choon Siong on 2017/08/22.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
+// Reverse the digits of a number
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int number, right_digit;
+        
+        printf("Enter a number: ");
+        scanf("%i", &number);
+        
+        // use a do while loop to get rid of this if statement
+        if (number == 0) {
+            printf("%i\n", number);
+            
+            return 0;
+        }
+        
+        while (number != 0 && number > 0) {
+            right_digit = number % 10;
+            printf("%i", right_digit);
+            number = number / 10;
+        }
+        
+        if (number < 0) {
+            number = -number;
+        }
+        
+        while (number != 0) {
+            right_digit = number % 10;
+            printf("%i", right_digit);
+            
+            if (number / 10 == 0) { // to add the minus sign
+                printf("-");
+            }
+            
+            number = number / 10;
+        }
+        
+        printf("\n");
     }
+    
     return 0;
 }
