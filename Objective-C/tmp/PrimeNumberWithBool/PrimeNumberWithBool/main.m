cf49b17731c7e32a8b48482df7014cbecea0548e
choonsiong <lee93287@gmail.com>
2017-08-22 16:12:22 +0800

    First commit

--- Objective-C/tmp/PrimeNumberWithBool/PrimeNumberWithBool/main.m
+++ Objective-C/tmp/PrimeNumberWithBool/PrimeNumberWithBool/main.m
@@ -28,3 +28,8 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+ 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+ Program ended with exit code: 0
+*/
