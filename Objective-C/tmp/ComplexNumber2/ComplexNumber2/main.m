ef63a4690587c25f79519e3d326c57d6eec9d41e
choonsiong <lee93287@gmail.com>
2017-08-23 01:00:45 +0800

    First commit

---
+++ Objective-C/tmp/ComplexNumber2/ComplexNumber2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ComplexNumber2
+//
+//  Created by Lee Choon Siong on 2017/08/23.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
