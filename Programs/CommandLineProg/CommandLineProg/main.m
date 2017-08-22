8d925a34713ea90c92f208469705b942e19470b9
choonsiong <lee93287@gmail.com>
2017-08-22 18:55:47 +0800

    First commit

--- Programs/CommandLineProg/CommandLineProg/main.m
+++ Programs/CommandLineProg/CommandLineProg/main.m
@@ -14,3 +14,26 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+ 
+ To compile the program from command-line:
+
+ 18:54:40 ~/Code/Programs/CommandLineProg/CommandLineProg
+ 18:54:40 ~/Code/Programs/CommandLineProg/CommandLineProg pwd
+ /Users/choonsiong/Code/Programs/CommandLineProg/CommandLineProg
+ 18:54:41 ~/Code/Programs/CommandLineProg/CommandLineProg
+ 18:54:41 ~/Code/Programs/CommandLineProg/CommandLineProg ls -l
+ total 24
+ -rw-r--r--@ 1 choonsiong  staff  208 Aug 22 18:53 Person.h
+ -rw-r--r--@ 1 choonsiong  staff  691 Aug 22 18:53 Person.m
+ -rw-r--r--@ 1 choonsiong  staff  419 Aug 22 18:54 main.m
+ 18:54:43 ~/Code/Programs/CommandLineProg/CommandLineProg
+ 18:54:43 ~/Code/Programs/CommandLineProg/CommandLineProg clang -fobjc-arc Person.m main.m -o prog
+ 18:55:08 ~/Code/Programs/CommandLineProg/CommandLineProg ./prog
+ P's first name is Foo
+ P's last name is Bar
+ P's full name is Foo Bar
+ 18:55:10 ~/Code/Programs/CommandLineProg/CommandLineProg
+
+*/
