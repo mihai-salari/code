8e586e10e4571b0a3a2ac6be465fe4d5d43a9528
choonsiong <lee93287@gmail.com>
2017-08-22 16:18:03 +0800

    First commit

--- Objective-C/tmp/PrimeNumber2/PrimeNumber2/main.m
+++ Objective-C/tmp/PrimeNumber2/PrimeNumber2/main.m
@@ -10,11 +10,18 @@ int main(int argc, const char * argv[]) {
         for (p = 2; p <= 100; p++) {
             isPrime = YES; // true
             
+            if (p != 2 && p % 2 == 0) {
+                // number can be evenly divided by 2, is not a prime
+                isPrime = NO;
+                continue;
+            }
+            
             // a test to divide p by the integers 2 through p - 1
             for (d = 2; d < p; d++) {
                 // if the remainder is 0, it is not a prime
                 if (p % d == 0) {
                     isPrime = NO; // false
+                    break;
                 }
             }
             
@@ -31,5 +38,8 @@ int main(int argc, const char * argv[]) {
 
 /*
  2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+ 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+ 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
  Program ended with exit code: 0
  */
+
