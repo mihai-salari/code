ec8afe5e7ea9e992c8dad91ba676937ea3602003
choonsiong <lee93287@gmail.com>
2017-08-28 02:45:11 +0800

    First commit

--- Haskell/sort_with_quicksort.hs
+++ Haskell/sort_with_quicksort.hs
@@ -2,7 +2,7 @@
 -- sorts a list using the quicksort algorithm
 
 f [] = []
-f (x:xs) = f ys ++ [x] ++ f zs
+f (x:xs) = f ys ++ [x] ++ f zs -- a list xs that starts with value x, and recursively sort ys and zs with function f
 	where
-		ys = [a | a <- xs, a <= x]
-		zs = [b | b <- xs, b > x]
+		ys = [a | a <- xs, a <= x] -- take all the elements a that are less than x, put in list ys
+		zs = [b | b <- xs, b > x] -- take all the elements b that are larger than x, put in list zs
