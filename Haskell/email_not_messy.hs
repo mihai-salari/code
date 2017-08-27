88a349fed3bc9d98784bc54265a54fc7bb205f82
choonsiong <lee93287@gmail.com>
2017-08-28 00:34:41 +0800

    First commit

--- Haskell/email_not_messy.hs
+++ Haskell/email_not_messy.hs
@@ -1,3 +1,14 @@
 toPart recipient = "Dear " ++ recipient ++ ",\n"
 bodyPart bookTitle = "Thanks for buying \"" ++ bookTitle ++ "\".\n"
 fromPart author = "Thanks,\n" ++ author
+
+createEmail recipient bookTitle author = (toPart recipient) ++ (bodyPart bookTitle) ++ (fromPart author)
+
+main = do
+	print "Who is the email for?"
+	recipient <- getLine
+	print "What is the title?"
+	title <- getLine
+	print "Who is the Author?"
+	author <- getLine
+	print (createEmail recipient title author)
