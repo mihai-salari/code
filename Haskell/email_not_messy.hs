d04cedd10511ca4411083af96e111643e365f093
choonsiong <lee93287@gmail.com>
2017-08-28 00:37:00 +0800

    First commit

--- Haskell/email_not_messy.hs
+++ Haskell/email_not_messy.hs
@@ -1,6 +1,6 @@
 toPart recipient = "Dear " ++ recipient ++ ",\n"
 bodyPart bookTitle = "Thanks for buying \"" ++ bookTitle ++ "\".\n"
-fromPart author = "Thanks,\n" ++ author
+fromPart author = "\nThanks,\n" ++ author
 
 createEmail recipient bookTitle author = (toPart recipient) ++ (bodyPart bookTitle) ++ (fromPart author)
 
