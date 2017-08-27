85791173d59bf3234d752ad19964a58f75a3e5e2
choonsiong <lee93287@gmail.com>
2017-08-27 20:38:38 +0800

    First commit

--- Haskell/count_chars.hs
+++ Haskell/count_chars.hs
@@ -1,6 +1,6 @@
 -- count_chars.hs
 -- counts the number of characters in its inputs
--- $ runghc count_chars < foo.txt
+-- $ runghc count_chars < chars.txt
 
 main = interact wordCount
-	where wordCount input = show (length (words (words input))) ++ "\n"
+	where wordCount input = show (length input) ++ "\n"
