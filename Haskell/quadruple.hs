dba9d91729c5c07eb8ef8e13c7154ef4a4eb44d9
choonsiong <lee93287@gmail.com>
2017-08-29 03:06:23 +0800

    First commit

--- Haskell/quadruple.hs
+++ Haskell/quadruple.hs
@@ -1,4 +1,11 @@
 -- quadruple.hs
 
+-- first function definition
 double x = x + x
+
+-- second function definition
 quadruple x = double (double x)
+
+factorial n = product [1..n]
+
+average ns = sum ns `div` length ns
