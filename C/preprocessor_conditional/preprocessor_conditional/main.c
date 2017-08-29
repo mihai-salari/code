5a790fa92c4ce8beb5bf3c18303a7eadacaede85
choonsiong <lee93287@gmail.com>
2017-08-30 03:55:28 +0800

    First commit

--- C/preprocessor_conditional/preprocessor_conditional/main.c
+++ C/preprocessor_conditional/preprocessor_conditional/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 // Using conditional to turn on and off debug message.
+// First with #define
+// Second with the use of different values for debugging level, and hence
+// display different level of debugging message
 
 //#define kDebug
 #define kDebugLevel 0
