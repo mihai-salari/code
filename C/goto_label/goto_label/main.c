87e310677e629a432106d0fc57f6091b1771815c
choonsiong <lee93287@gmail.com>
2017-08-30 03:29:58 +0800

    First commit

--- C/goto_label/goto_label/main.c
+++ C/goto_label/goto_label/main.c
@@ -2,10 +2,19 @@
 
 int main(int argc, const char * argv[]) {
     int i = 0;
-    game: while (i < 10) {
+    
+    while (i < 10) {
         printf("i is %d\n", i);
         
-        i += 1;
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0 && i != 0) {
+                goto next;
+            } else {
+                printf("for: i is %d\n", i);
+            }
+        }
+        
+    next: i += 1;
     }
     
     return 0;
