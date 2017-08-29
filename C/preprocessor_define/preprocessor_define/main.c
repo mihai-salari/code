836931f4cc12f5bb8b867a02378cd58f29954a29
choonsiong <lee93287@gmail.com>
2017-08-30 03:47:45 +0800

    First commit

--- C/preprocessor_define/preprocessor_define/main.c
+++ C/preprocessor_define/preprocessor_define/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+// C style
 #define AGE 37
+
+// Apple style
+#define kAge 37
 
 int main(int argc, const char * argv[]) {
     printf("Age is %i\n", AGE);
+    printf("Age is %i\n", kAge);
     
     return 0;
 }
