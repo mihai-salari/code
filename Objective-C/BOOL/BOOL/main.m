fe2f7a6d188ff76d28bd586421c1fd37e168d4ab
choonsiong <lee93287@gmail.com>
2017-08-29 17:22:22 +0800

    First commit

--- Objective-C/BOOL/BOOL/main.m
+++ Objective-C/BOOL/BOOL/main.m
@@ -18,7 +18,13 @@ NSString *boolString(BOOL yesNo) {
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        BOOL areTheyDifferent;
+        areTheyDifferent = areIntegersDifferent(8, 8);
         
+        NSLog(@"Are %d and %d different? %@", 8, 8, boolString(areTheyDifferent));
+        
+        areTheyDifferent = areIntegersDifferent(42, 38);
+        NSLog(@"Are %d and %d different? %@", 42, 38, boolString(areTheyDifferent));
     }
     
     return 0;
