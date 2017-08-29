15678484a6c01b5033f724139ecd94fe95cdbbdb
choonsiong <lee93287@gmail.com>
2017-08-29 22:46:09 +0800

    First commit

--- C/enum_problem/enum_problem/main.c
+++ C/enum_problem/enum_problem/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
+enum HTTPError {
+    ok=200,
+    created, accepted, // 201, 202
+    bad_request=400,
+    unauthorized, payment_required, // 401, 402
+    internal_server_error=500,
+    not_implemented, bad_gateway, service_unavailable // 501, 502, 503
+};
+
 int main(int argc, const char * argv[]) {
-        
+    enum HTTPError status_code = service_unavailable;
+    
+    int some_numeric_code = 1024;
+    
+    status_code = some_numeric_code; // NOTE THIS ERROR!!! 1024 should not allow!!!
+    
+    switch (status_code) {
+        case 200:
+        case 201:
+        case 202:
+            printf("OK\n");
+            break;
+            
+        default:
+            printf("NOT OK\n");
+            break;
+    }
+    
+    
     
     return 0;
 }
