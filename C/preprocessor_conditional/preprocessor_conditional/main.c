c174d24da038432c625def1afcd5cd20ac75cbc6
choonsiong <lee93287@gmail.com>
2017-08-30 03:51:04 +0800

    First commit

---
+++ C/preprocessor_conditional/preprocessor_conditional/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  preprocessor_conditional
+//
+//  Created by Lee Choon Siong on 2017/08/30.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
