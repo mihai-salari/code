4d9c6ec8e31fa94abe0db693f96aafb9ba1d4609
choonsiong <lee93287@gmail.com>
2017-08-29 19:14:08 +0800

    First commit

--- Objective-C/_ObjectiveWay/_ObjectiveWay/main.m
+++ Objective-C/_ObjectiveWay/_ObjectiveWay/main.m
@@ -1,16 +1,27 @@
 #import <Foundation/Foundation.h>
 #import "Shape.h"
+#import "Circle.h"
+#import "Rectangle.h"
+#import "Egg.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Shape *shapes[3];
+        Shape *shapes[4];
         
-        Shape *shape0 = [[Shape alloc] init];
         ShapeRect rect0 = {0, 0, 10, 30};
+        Shape *shape0 = [[Shape alloc] init];
+        // Using properties
         shape0.bounds = rect0;
         shape0.fillColor = kRed;
         shape0.shapeType = kCircle;
         shapes[0] = shape0;
+        
+        ShapeRect rect1 = {0, 0, 10, 30};
+        shapes[1] = [Circle new];
+        // Using normal setter methods
+        [shapes[1] setBounds:rect1];
+        [shapes[1] setFillColor:kRed];
+        [shapes[1] setShapeType:kCircle];
         
         for (int i = 0; i < 3; i++) {
             id shape = shapes[i];
