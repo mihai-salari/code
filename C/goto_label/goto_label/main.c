e26ca638ff22ffa87c5a2b3f6cc94c5ef8768f5f
choonsiong <lee93287@gmail.com>
2017-08-30 03:27:00 +0800

    First commit

--- C/goto_label/goto_label/main.c
+++ C/goto_label/goto_label/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    
+    int i = 0;
+    game: while (i < 10) {
+        printf("i is %d\n", i);
+        
+        i += 1;
+    }
     
     return 0;
 }
