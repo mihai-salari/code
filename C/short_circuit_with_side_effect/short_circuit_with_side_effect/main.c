2970c0d5a619b32f1e63fe137241828daf9c9741
choonsiong <lee93287@gmail.com>
2017-08-29 23:22:00 +0800

    First commit

--- C/short_circuit_with_side_effect/short_circuit_with_side_effect/main.c
+++ C/short_circuit_with_side_effect/short_circuit_with_side_effect/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-void printAge() {
+int printAge() {
     printf("I am 37 years old.\n");
+    
+    return 1; // we need the 1 for TRUE below
 }
 
 int main(int argc, const char * argv[]) {
+    int age = 37;
+    
+    if (age >= 30 && printAge()) {
+        printf("Done!\n");
+    } else {
+        printf("Failed!\n");
+    }
+    
     return 0;
 }
