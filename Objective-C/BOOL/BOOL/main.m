edb6a78d63a8afcb2c65a71d0736d9de30886ebf
choonsiong <lee93287@gmail.com>
2017-08-29 17:20:25 +0800

    First commit

--- Objective-C/BOOL/BOOL/main.m
+++ Objective-C/BOOL/BOOL/main.m
@@ -1,9 +1,25 @@
 #import <Foundation/Foundation.h>
 
+BOOL areIntegersDifferent(int first, int second) {
+    if (first == second) {
+        return (NO);
+    } else {
+        return (YES);
+    }
+}
+
+NSString *boolString(BOOL yesNo) {
+    if (yesNo == NO) {
+        return(@"NO");
+    } else {
+        return(@"YES");
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
     }
+    
     return 0;
 }
