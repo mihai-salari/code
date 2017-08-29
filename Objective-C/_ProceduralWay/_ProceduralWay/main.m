eb163b7cbd13c4d2e782461e482f9204190ddbaf
choonsiong <lee93287@gmail.com>
2017-08-29 18:03:42 +0800

    First commit

--- Objective-C/_ProceduralWay/_ProceduralWay/main.m
+++ Objective-C/_ProceduralWay/_ProceduralWay/main.m
@@ -1,9 +1,36 @@
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    kCircle,
+    kRectangle,
+    kEgg
+} ShapeType;
+
+typedef enum {
+    kRed,
+    kGreen,
+    kBlue
+} ShapeColor;
+
+typedef struct {
+    int x;
+    int y;
+    int width;
+    int height;
+} ShapeRect;
+
+typedef struct {
+    ShapeType type;
+    ShapeColor fillColor;
+    ShapeRect bounds;
+} Shape;
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Shape shapes[3];
+        
+        Sh
     }
+    
     return 0;
 }
