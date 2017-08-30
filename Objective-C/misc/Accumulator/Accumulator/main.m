e219d45ed7a513643b7dccd1569687c54219052d
choonsiong <lee93287@gmail.com>
2017-08-30 20:21:37 +0800

    First commit

--- Objective-C/misc/Accumulator/Accumulator/main.m
+++ Objective-C/misc/Accumulator/Accumulator/main.m
@@ -1,8 +1,13 @@
 #import <Foundation/Foundation.h>
+#import "Accumulator.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Accumulator *acm = [Accumulator new];
+        [acm zeroTotal];
+        [acm addToTotalWith:1];
         
+        NSLog(@"Total = %d\n", [acm total]);
     }
     
     return 0;
