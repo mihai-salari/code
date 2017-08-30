ea2078407a7fe9a35dc9df5ef91bc41149e67fe6
choonsiong <lee93287@gmail.com>
2017-08-30 15:01:27 +0800

    First commit

--- C/misc/fibonacci/fibonacci/main.c
+++ C/misc/fibonacci/fibonacci/main.c
@@ -3,17 +3,22 @@
 #define number 20
 
 int fib(int n) {
-    return 2 + fib(n - 1)
+    int f0 = 0;
+    int f1 = 1;
+    int sum = f0 + f1;
+    
+    if (n == 0) {
+        return f0;
+    } else if (n == 1) {
+        return sum;
+    }
+    
+    return sum + fib(n - 1);
 }
 
 int main(int argc, const char * argv[]) {
-    if (number == 1) {
-        printf("Fibonacci Number 1 is: 0\n");
-    } else if (number == 2) {
-        printf("Fibonacci Number 2 is: 1\n");
-    } else {
-        printf("Fibonacci Number %d is: %d\n", number, 2 + fib(number));
-    }
+    
+    printf("%d\n", fib(number));
     
     return 0;
 }
