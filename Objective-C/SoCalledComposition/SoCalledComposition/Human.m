fe42b30e61afe7962656e1699289188ff8da35fc
choonsiong <lee93287@gmail.com>
2017-08-31 03:41:15 +0800

    First commit

--- Objective-C/SoCalledComposition/SoCalledComposition/Human.m
+++ Objective-C/SoCalledComposition/SoCalledComposition/Human.m
@@ -2,6 +2,15 @@
 
 @implementation Human
 
+- (instancetype)init {
+    if (self = [super init]) {
+        self.hand = [Hand new];
+        self.foot = [Foot new];
+    }
+    
+    return self;
+}
+
 - (void)print {
     
 }
