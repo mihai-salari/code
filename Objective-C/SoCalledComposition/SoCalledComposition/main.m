25862423f026ed440903a11819d8be21d05ecd8f
choonsiong <lee93287@gmail.com>
2017-08-31 03:55:50 +0800

    First commit

--- Objective-C/SoCalledComposition/SoCalledComposition/main.m
+++ Objective-C/SoCalledComposition/SoCalledComposition/main.m
@@ -5,6 +5,17 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Human *man = [Human new];
         [man print];
+        
+        Human *woman = [Human new];
+        [woman print];
+        
+        Hand *hand = [Hand new];
+        woman.hand = hand;
+        
+        Foot *foot = [Foot new];
+        woman.foot = foot;
+        
+        [woman print];
     }
     
     return 0;
