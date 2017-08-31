cac801ff9bd36aa8515359a0540bcdea6b19ab7c
choonsiong <lee93287@gmail.com>
2017-09-01 00:54:38 +0800

    First commit

--- Objective-C/AbstractClass/AbstractClass/MenuItem.h
+++ Objective-C/AbstractClass/AbstractClass/MenuItem.h
@@ -1,11 +1,3 @@
-//
-//  MenuItem.h
-//  AbstractClass
-//
-//  Created by Lee Choon Siong on 2017/09/01.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface MenuItem : NSObject
