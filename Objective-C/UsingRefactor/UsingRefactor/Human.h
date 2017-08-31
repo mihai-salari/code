bd0d76f471118ba2da0db36744fee61f041515b8
choonsiong <lee93287@gmail.com>
2017-08-31 20:03:31 +0800

    First commit

---
+++ Objective-C/UsingRefactor/UsingRefactor/Human.h
@@ -0,0 +1,13 @@
+//
+//  Human.h
+//  UsingRefactor
+//
+//  Created by Lee Choon Siong on 2017/08/31.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Human : NSObject
+
+@end
---
+++ Objective-C/UsingRefactor/UsingRefactor/Human.m
@@ -0,0 +1,13 @@
+//
+//  Human.m
+//  UsingRefactor
+//
+//  Created by Lee Choon Siong on 2017/08/31.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import "Human.h"
+
+@implementation Human
+
+@end
