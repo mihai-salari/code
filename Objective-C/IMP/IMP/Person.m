779e2086e1b969acab6f3b6de71648fdbb59519e
choonsiong <lee93287@gmail.com>
2017-08-31 18:56:34 +0800

    First commit

--- Objective-C/IMP/IMP/Person.m
+++ Objective-C/IMP/IMP/Person.m
@@ -1,11 +1,3 @@
-//
-//  Person.m
-//  IMP
-//
-//  Created by Lee Choon Siong on 2017/08/31.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import "Person.h"
 
 @implementation Person
