8b19684fb055d3d03fd5a491036c42be1e3325bd
choonsiong <lee93287@gmail.com>
2017-09-01 03:33:49 +0800

    First commit

--- Objective-C/ClassFailedInitialization/ClassFailedInitialization/main.m
+++ Objective-C/ClassFailedInitialization/ClassFailedInitialization/main.m
@@ -1,8 +1,15 @@
 #import <Foundation/Foundation.h>
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Person *p = [Person new]; // This will failed
         
+        if (p == nil) {
+            NSLog(@"Error: not able to create instance of Person");
+        } else {
+            NSLog(@"Person is %@", p);
+        }
     }
     
     return 0;
