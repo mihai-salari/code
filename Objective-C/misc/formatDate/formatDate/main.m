131c3d029940ed5f5a333dd00446a84968e7fc73
choonsiong <lee93287@gmail.com>
2017-08-31 21:54:58 +0800

    First commit

--- Objective-C/misc/formatDate/formatDate/main.m
+++ Objective-C/misc/formatDate/formatDate/main.m
@@ -2,17 +2,22 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSLog(@"Hello, World!");
-        
         NSDate *date = [NSDate date];
         NSDateFormatter *df = [NSDateFormatter new];
         
-        [df setDateFormat:@"EEE MMM d, yyyy"];
+        [df setDateFormat:@"MMM d, yyyy, EEE"];
         
         NSString *dateString = [df stringFromDate:date];
         
-        NSLog(@"Today is %@", dateString);
+        NSLog(@"Today is %@.", dateString);
     }
     
     return 0;
 }
+
+/*
+ 
+2017-08-31 21:54:23.527328+0800 formatDate[8213:613094] Today is Aug 31, 2017, Thu.
+Program ended with exit code: 0
+
+*/
