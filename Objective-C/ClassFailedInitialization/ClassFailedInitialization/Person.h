e297744bda94fd004ceb27a0fff930b4227b2204
choonsiong <lee93287@gmail.com>
2017-09-01 03:30:49 +0800

    First commit

--- Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.h
+++ Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.h
@@ -2,4 +2,7 @@
 
 @interface Person : NSObject
 
+@property NSMutableArray *array1;
+@property NSArray *array2;
+
 @end
--- Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.m
+++ Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.m
@@ -2,4 +2,25 @@
 
 @implementation Person
 
+- (instancetype)init {
+    if (self = [super init]) {
+        _array1 = [[NSMutableArray alloc] init];
+        
+        if (_array1 == nil) {
+            // Now allow in latest Xcode
+            // [self release];
+            return nil;
+        }
+        
+        _array2 = [[NSArray alloc] initWithContentsOfFile:@"/tmp/foo.txt"];
+        
+        if (_array2 == nil) {
+            //[self release];
+            return nil;
+        }
+    }
+    
+    return self;
+}
+
 @end
