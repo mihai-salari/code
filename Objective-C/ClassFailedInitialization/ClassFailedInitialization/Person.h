23ce72a18a568ecb4bbf65b5a701f9906f0256ab
choonsiong <lee93287@gmail.com>
2017-09-01 03:15:21 +0800

    First commit

--- Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.h
+++ Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.h
@@ -1,11 +1,3 @@
-//
-//  Person.h
-//  ClassFailedInitialization
-//
-//  Created by Lee Choon Siong on 2017/09/01.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface Person : NSObject
--- Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.m
+++ Objective-C/ClassFailedInitialization/ClassFailedInitialization/Person.m
@@ -1,11 +1,3 @@
-//
-//  Person.m
-//  ClassFailedInitialization
-//
-//  Created by Lee Choon Siong on 2017/09/01.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import "Person.h"
 
 @implementation Person
