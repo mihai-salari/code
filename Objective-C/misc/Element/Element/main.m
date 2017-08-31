ef159ba06516213c4ccfce691f1fd82827b6cc5b
choonsiong <lee93287@gmail.com>
2017-08-31 22:34:15 +0800

    First commit

--- Objective-C/misc/Element/Element/main.m
+++ Objective-C/misc/Element/Element/main.m
@@ -3,7 +3,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Atom *atom = [[Atom alloc] init];
         
+        NSLog(@"Atom chemical element name: %@", atom.chemicalElement);
     }
     
     return 0;
