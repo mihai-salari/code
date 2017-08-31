5702b36fc8eee35b032ecddb4878e75dbc762628
choonsiong <lee93287@gmail.com>
2017-08-31 20:04:12 +0800

    First commit

--- Objective-C/UsingRefactor/UsingRefactor/Human.h
+++ Objective-C/UsingRefactor/UsingRefactor/Human.h
@@ -1,11 +1,3 @@
-//
-//  Human.h
-//  UsingRefactor
-//
-//  Created by Lee Choon Siong on 2017/08/31.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface Human : NSObject
--- Objective-C/UsingRefactor/UsingRefactor/main.m
+++ Objective-C/UsingRefactor/UsingRefactor/main.m
@@ -1,8 +1,11 @@
 #import <Foundation/Foundation.h>
+#import "Human.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Human *h = [Human new];
         
+        NSLog(@"Human is %@", h);
     }
     
     return 0;
