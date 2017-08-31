ee35eff77be9a62d99416fd0758135c639fb8199
choonsiong <lee93287@gmail.com>
2017-08-31 17:19:00 +0800

    First commit

---
+++ Objective-C/NilReceiver/NilReceiver/main.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSString *someText = nil;
+        
+        NSLog(@"This is some text: %@", someText);
+    }
+    
+    return 0;
+}
