ba8712b2b98e8a4e0249968f7dc8e61b4e406b9b
choonsiong <lee93287@gmail.com>
2017-08-31 23:01:23 +0800

    First commit

--- Objective-C/misc/Element/Element/main.m
+++ Objective-C/misc/Element/Element/main.m
@@ -1,11 +1,14 @@
 #import <Foundation/Foundation.h>
 #import "Atom.h"
+#import "Nuclear.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Atom *atom = [[Atom alloc] init];
         
         NSLog(@"Atom chemical element name: %@", atom.chemicalElement);
+        
+        NSLog(@"Atomic number = %lu", [atom atomicNumber]);
     }
     
     return 0;
