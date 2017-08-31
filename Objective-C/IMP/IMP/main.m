4186e01071d2495f9f66292d45057ffa3bca3253
choonsiong <lee93287@gmail.com>
2017-08-31 19:15:12 +0800

    First commit

--- Objective-C/IMP/IMP/main.m
+++ Objective-C/IMP/IMP/main.m
@@ -4,8 +4,27 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Person *p = [Person new];
+        [p setName:@"Foo Bar"];
         
+        // Execute with method call
         
+        int i;
+        
+        /*
+        for (i = 0; i < 1000000; i++) {
+            printf("%d: ", i);
+            [p printMyName];
+        }*/
+        
+        IMP printMyNameIMP = [p methodForSelector:@selector(printMyName)];
+        
+        // Execute with direct function call
+        
+        for (i = 0; i < 1000000; i++) {
+            printf("%d: ", i);
+            printf(*printMyNameIMP);
+            printf("\n");
+        }
     }
     
     return 0;
