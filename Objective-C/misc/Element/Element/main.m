d7e188f4b0cbc459012866e93c0812d70c9a3f16
choonsiong <lee93287@gmail.com>
2017-09-01 21:09:11 +0800

    First commit

--- Objective-C/misc/Element/Element/main.m
+++ Objective-C/misc/Element/Element/main.m
@@ -19,8 +19,18 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"Hydrogen chemical element name: %@", h.chemicalElement);
         
-        Atom *hydrogen = [Hydrogen hydrogenWithNeutrons:0];
-        [hydrogen logInfo];
+        Atom *h1 = [Hydrogen hydrogenWithNeutrons:0];
+        [h1 logInfo];
+        
+        Atom *h2 = [[Hydrogen alloc] initWithNeutrons:1];
+        [h2 logInfo];
+        
+        Hydrogen *h3 = [Hydrogen hydrogenWithNeutrons:2];
+        [h3 logInfo];
+        
+        NSLog(@"h1's class is %@", [h1 class]);
+        NSLog(@"h2's class is %@", [h2 class]);
+        NSLog(@"h3's superclass is %@", [h3 superclass]);
     }
     
     return 0;
