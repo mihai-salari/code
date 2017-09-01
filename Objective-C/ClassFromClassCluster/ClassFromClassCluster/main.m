5aab5783c6ff612e2ce5f25f3687208c1cfdf84e
choonsiong <lee93287@gmail.com>
2017-09-02 02:35:49 +0800

    First commit

--- Objective-C/ClassFromClassCluster/ClassFromClassCluster/main.m
+++ Objective-C/ClassFromClassCluster/ClassFromClassCluster/main.m
@@ -2,6 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSNumber *someNumber = @1.234;
+        
+        Class someClass = [someNumber class];
+        
+        NSLog(@"%@", someClass); // Class cluster in work
     }
     
     return 0;
