5b87a9729a5e9ec14109a288ff4b646d606c39eb
choonsiong <lee93287@gmail.com>
2017-09-02 03:33:19 +0800

    First commit

---
+++ Objective-C/ClassSingleton/ClassSingleton/God.m
@@ -0,0 +1,13 @@
+//
+//  God.m
+//  ClassSingleton
+//
+//  Created by Lee Choon Siong on 2017/09/02.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import "God.h"
+
+@implementation God
+
+@end
--- Objective-C/ClassSingleton/ClassSingleton/main.m
+++ Objective-C/ClassSingleton/ClassSingleton/main.m
@@ -1,5 +1,7 @@
 #import <Foundation/Foundation.h>
 
+// A singleton is a class with only a single-shared instance.
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
