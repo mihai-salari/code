276301ce5aea4f673aa0a96a4421107f7d88b2ec
choonsiong <lee93287@gmail.com>
2017-09-02 04:01:04 +0800

    First commit

--- Objective-C/ClassVariableMimicking/ClassVariableMimicking/Coupon.h
+++ Objective-C/ClassVariableMimicking/ClassVariableMimicking/Coupon.h
@@ -1,11 +1,3 @@
-//
-//  Coupon.h
-//  ClassVariableMimicking
-//
-//  Created by Lee Choon Siong on 2017/09/02.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface Coupon : NSObject
--- Objective-C/ClassVariableMimicking/ClassVariableMimicking/Coupon.m
+++ Objective-C/ClassVariableMimicking/ClassVariableMimicking/Coupon.m
@@ -1,11 +1,3 @@
-//
-//  Coupon.m
-//  ClassVariableMimicking
-//
-//  Created by Lee Choon Siong on 2017/09/02.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import "Coupon.h"
 
 @implementation Coupon
