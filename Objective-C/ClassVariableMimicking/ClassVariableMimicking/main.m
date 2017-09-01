6e490cc82518a4f73cc5bc1fd826045b8186173b
choonsiong <lee93287@gmail.com>
2017-09-02 04:36:14 +0800

    First commit

--- Objective-C/ClassVariableMimicking/ClassVariableMimicking/main.m
+++ Objective-C/ClassVariableMimicking/ClassVariableMimicking/main.m
@@ -1,22 +1,26 @@
 #import <Foundation/Foundation.h>
 #import "Coupon.h"
+#import "SubCoupon.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"%d coupons left", [Coupon numberCouponsLeft]);
+        NSLog(@"%d sub-coupons left", [SubCoupon numberCouponsLeft]);
         
         for (int i = 0; i < 100; i++) {
             NSLog(@"creating new coupon #%d", i);
             __unused Coupon *cp = [Coupon new];
+            NSLog(@"creating new sub-coupon #%d", i);
+            __unused SubCoupon *subCp = [SubCoupon new];
         }
         
         NSLog(@"%d coupons left", [Coupon numberCouponsLeft]);
+        NSLog(@"%d sub-coupons left", [SubCoupon numberCouponsLeft]);
         
         __unused Coupon *cp = [Coupon new];
         
         NSLog(@"%d coupons left", [Coupon numberCouponsLeft]);
-        
-        
+        NSLog(@"%d sub-coupons left", [SubCoupon numberCouponsLeft]);
     }
     
     return 0;
