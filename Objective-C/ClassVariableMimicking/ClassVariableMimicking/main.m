2c061b43da12dd6670e3b3307461741c0fe7382e
choonsiong <lee93287@gmail.com>
2017-09-02 04:07:30 +0800

    First commit

--- Objective-C/ClassVariableMimicking/ClassVariableMimicking/main.m
+++ Objective-C/ClassVariableMimicking/ClassVariableMimicking/main.m
@@ -1,8 +1,13 @@
 #import <Foundation/Foundation.h>
+#import "Coupon.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSLog(@"%d coupons left", [Coupon numberCouponsLeft]);
         
+        __unused Coupon *cp = [Coupon new];
+        
+        NSLog(@"%d coupons left", [Coupon numberCouponsLeft]);
     }
     
     return 0;
