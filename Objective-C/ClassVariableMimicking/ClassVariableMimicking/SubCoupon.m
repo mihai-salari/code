a109ee575f1945f9321bb11c522332a9b0b7b24e
choonsiong <lee93287@gmail.com>
2017-09-02 04:33:21 +0800

    First commit

--- Objective-C/ClassVariableMimicking/ClassVariableMimicking/SubCoupon.m
+++ Objective-C/ClassVariableMimicking/ClassVariableMimicking/SubCoupon.m
@@ -1,5 +1,30 @@
 #import "SubCoupon.h"
 
+#define INITIAL_NEW_COUPON_ALLOCATION 50
+
+static int availableNewCoupons = INITIAL_NEW_COUPON_ALLOCATION;
+
+@interface SubCoupon ()
+
++ (int)availableCoupons;
++ (void)setAvailableCoupons:(int)inAvailableCoupons;
+
+@end
+
 @implementation SubCoupon
+
++ (int)availableCoupons {
+    return availableNewCoupons;
+}
+
++ (void)setAvailableCoupons:(int)inAvailableCoupons {
+    availableNewCoupons = inAvailableCoupons;
+}
+
+// Overrides superclass implementation to have its own value
+// for the initial allocation
++ (void)resetCoupon {
+    [self setAvailableCoupons:INITIAL_NEW_COUPON_ALLOCATION];
+}
 
 @end
