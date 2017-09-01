5ceadcf56ae26e3dd2713b6cac3a704bf4c5f0de
choonsiong <lee93287@gmail.com>
2017-09-02 00:19:56 +0800

    First commit

--- Objective-C/CStringFromNSString/CStringFromNSString/main.m
+++ Objective-C/CStringFromNSString/CStringFromNSString/main.m
@@ -2,6 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSString *name = @"Foo Bar";
+        
+        const char *c_name = [name UTF8String];
+        
+        printf("My name is %s\n", c_name);
     }
     
     return 0;
