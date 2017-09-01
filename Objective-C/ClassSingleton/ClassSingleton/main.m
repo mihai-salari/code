5bb6f1d7c92687db9aed766f49199f980c086dfb
choonsiong <lee93287@gmail.com>
2017-09-02 03:42:19 +0800

    First commit

--- Objective-C/ClassSingleton/ClassSingleton/main.m
+++ Objective-C/ClassSingleton/ClassSingleton/main.m
@@ -19,3 +19,12 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ 2017-09-02 03:41:43.814177+0800 ClassSingleton[31224:2293537] I am The God from planet 0x1004053e0.
+ 2017-09-02 03:41:43.814361+0800 ClassSingleton[31224:2293537] I am The God from planet 0x1002036b0.
+ 2017-09-02 03:41:43.814384+0800 ClassSingleton[31224:2293537] I am The God from planet 0x1002036b0.
+ Program ended with exit code: 0
+
+*/
