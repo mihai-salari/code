ae743e0bba2315f92267e912b357043ca506c8aa
choonsiong <lee93287@gmail.com>
2017-09-02 04:26:21 +0800

    First commit

--- Objective-C/ClassVariableMimicking/ClassVariableMimicking/Coupon.m
+++ Objective-C/ClassVariableMimicking/ClassVariableMimicking/Coupon.m
@@ -13,24 +13,47 @@ static int availableCoupons = INITIAL_COUPON_ALLOCATION; // visible to this file
 
 @implementation Coupon
 
-- (id)init {
-    if (self = [super init]) {
-        if (availableCoupons == 0) {
-            return nil;
-        }
-        
-        availableCoupons -= 1;
-    }
-    
-    return self;
-}
-
 + (int)numberCouponsLeft {
     return availableCoupons;
 }
 
 + (void)resetCoupon {
     availableCoupons = INITIAL_COUPON_ALLOCATION;
+}
+
++ (int)availableCoupons {
+    return availableCoupons;
+}
+
++ (void)setAvailableCoupons:(int)inAvailableCoupons {
+    availableCoupons = inAvailableCoupons;
+}
+
+// Without fixing subclass issue from the use of 'class variable'
+//- (id)init {
+//    if (self = [super init]) {
+//        if (availableCoupons == 0) {
+//            return nil;
+//        }
+//        
+//        availableCoupons -= 1;
+//    }
+//    
+//    return self;
+//}
+
+// Fix subclassing problem while using 'class variable'
+// Note that we are using accessors here to retrieve or set 'availableCoupons'
+- (id)init {
+    if (self = [super init]) {
+        if ([[self class] availableCoupons] == 0) {
+            return nil;
+        }
+        
+        [[self class] setAvailableCoupons:[[self class] availableCoupons] - 1];
+    }
+    
+    return self;
 }
 
 - (void)dealloc {
