907a7afbc24eb37cdd3c9a49e88095ae9a0c373f
choonsiong <lee93287@gmail.com>
2017-09-02 19:18:19 +0800

    First commit

--- Objective-C/MethodForDynamicType/MethodForDynamicType/main.m
+++ Objective-C/MethodForDynamicType/MethodForDynamicType/main.m
@@ -4,6 +4,18 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         id someone = [Person new];
+        
+        if ([someone isMemberOfClass:[Person class]]) {
+            [someone setName:@"Foo Bar"];
+        }
+        
+        SEL action = @selector(greetWithName);
+        
+        if ([someone respondsToSelector:action]) {
+            [someone performSelector:action];
+        } else {
+            NSLog(@"I don't know what you meant!");
+        }
     }
     
     return 0;
