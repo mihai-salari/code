ffc973901370b64955aa520266e462e4d3d55ffa
choonsiong <lee93287@gmail.com>
2017-09-02 19:13:32 +0800

    First commit

--- Objective-C/MethodForDynamicType/MethodForDynamicType/Person.m
+++ Objective-C/MethodForDynamicType/MethodForDynamicType/Person.m
@@ -1,11 +1,3 @@
-//
-//  Person.m
-//  MethodForDynamicType
-//
-//  Created by Lee Choon Siong on 2017/09/02.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import "Person.h"
 
 @implementation Person
