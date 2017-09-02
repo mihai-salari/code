795be646b3f48df4b619364bb0bcb43dda00a0a1
choonsiong <lee93287@gmail.com>
2017-09-03 03:50:41 +0800

    First commit

--- Objective-C/WithARC/WithARC/main.m
+++ Objective-C/WithARC/WithARC/main.m
@@ -3,13 +3,13 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-//        Address *a = [Address new];        
-//        OrderItem *oi = [OrderItem new];
+        //Address *a = [Address new];
+        //OrderItem *oi = [OrderItem new];
         OrderEntry *entry = [[OrderEntry alloc] initWithOrderID:@"123"];
         
         NSLog(@"New order, ID = %@, item = %@", entry.orderID, entry.item.name);
         
-        NSLog(@"%@", entry);
+        //NSLog(@"%@", entry);
     }
     
     return 0;
