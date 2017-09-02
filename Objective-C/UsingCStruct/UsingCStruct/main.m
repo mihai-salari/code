63728aba5c422580f2cafe8e87ae062cfeaebe47
choonsiong <lee93287@gmail.com>
2017-09-03 04:06:17 +0800

    First commit

--- Objective-C/UsingCStruct/UsingCStruct/main.m
+++ Objective-C/UsingCStruct/UsingCStruct/main.m
@@ -7,10 +7,13 @@ typedef struct SomeStruct {
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        SomeStruct *foo = malloc(sizeof(SomeStruct));
+        SomeStruct *foo = (SomeStruct *)malloc(sizeof(SomeStruct));
         
         foo->a = 10;
         foo->b = 20;
+        
+        printf("a is %d\n", foo->a);
+        printf("b is %d\n", foo->b);
         
     }
     
