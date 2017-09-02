d0b30a771db9713d8fcd856f1d08858bd856f0c3
choonsiong <lee93287@gmail.com>
2017-09-03 02:22:28 +0800

    First commit

--- Objective-C/WithARC/WithARC/OrderEntry.m
+++ Objective-C/WithARC/WithARC/OrderEntry.m
@@ -1,11 +1,3 @@
-//
-//  OrderEntry.m
-//  WithARC
-//
-//  Created by Lee Choon Siong on 2017/09/03.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import "OrderEntry.h"
 
 @implementation OrderEntry
