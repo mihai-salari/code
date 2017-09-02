5e4892407d107609301237e375539342bac65cbd
choonsiong <lee93287@gmail.com>
2017-09-03 01:41:41 +0800

    First commit

--- Objective-C/WithARC/WithARC/Address.m
+++ Objective-C/WithARC/WithARC/Address.m
@@ -2,4 +2,14 @@
 
 @implementation Address
 
+- (instancetype)init {
+    if (self = [super init]) {
+        NSLog(@"Initializing Address object...");
+        
+        return self;
+    }
+    
+    return nil;
+}
+
 @end
