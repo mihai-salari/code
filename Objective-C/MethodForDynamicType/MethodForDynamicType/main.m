f9b16b25e31dec13373eb861d98ec2e26520c11c
choonsiong <lee93287@gmail.com>
2017-09-02 19:15:43 +0800

    First commit

--- Objective-C/MethodForDynamicType/MethodForDynamicType/main.m
+++ Objective-C/MethodForDynamicType/MethodForDynamicType/main.m
@@ -1,8 +1,9 @@
 #import <Foundation/Foundation.h>
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+        id someone = [Person new];
     }
     
     return 0;
