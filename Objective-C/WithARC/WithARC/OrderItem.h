e33717e7e7f37ff6356ab40fb3f1c55dd2b71a48
choonsiong <lee93287@gmail.com>
2017-09-03 04:28:44 +0800

    First commit

--- Objective-C/WithARC/WithARC/OrderItem.h
+++ Objective-C/WithARC/WithARC/OrderItem.h
@@ -1,5 +1,7 @@
 #import <Foundation/Foundation.h>
 
+@class OrderEntry; // there's a class called OrderEntry, and you don't fuck to need what is that class now ;)
+
 @interface OrderItem : NSObject
 
 @property NSString *name;
