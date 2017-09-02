37f57b839781e19643aada8e7b544d23d01e9c9e
choonsiong <lee93287@gmail.com>
2017-09-02 21:20:28 +0800

    First commit

--- Objective-C/ExceptionHandling/ExceptionHandling/main.m
+++ Objective-C/ExceptionHandling/ExceptionHandling/main.m
@@ -2,7 +2,17 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSArray *array = [NSArray array];
         
+        @try {
+            [array objectAtIndex:9];
+        }
+        
+        @catch (NSException *exception) {
+            NSLog(@"%@ %@", exception.name, exception.reason);
+        }
+        
+        NSLog(@"Execution continues");
     }
     
     return 0;
