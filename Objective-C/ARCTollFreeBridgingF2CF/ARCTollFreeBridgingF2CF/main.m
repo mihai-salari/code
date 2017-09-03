59065d3a9188e36b6177e356b5f65b0bd36b9abe
choonsiong <lee93287@gmail.com>
2017-09-04 02:38:13 +0800

    First commit

--- Objective-C/ARCTollFreeBridgingF2CF/ARCTollFreeBridgingF2CF/main.m
+++ Objective-C/ARCTollFreeBridgingF2CF/ARCTollFreeBridgingF2CF/main.m
@@ -4,7 +4,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSString *greeting = [[NSString alloc] initWithFormat:@"%@", @"I am Foundation String!"];
         
+        NSLog(@"greeting's class is %@", [greeting class]);
     }
     
     return 0;
