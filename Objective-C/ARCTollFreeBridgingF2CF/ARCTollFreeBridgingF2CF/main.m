0b5e414dd478bef50447b91273d7813cc6e77ef1
choonsiong <lee93287@gmail.com>
2017-09-04 02:42:02 +0800

    First commit

--- Objective-C/ARCTollFreeBridgingF2CF/ARCTollFreeBridgingF2CF/main.m
+++ Objective-C/ARCTollFreeBridgingF2CF/ARCTollFreeBridgingF2CF/main.m
@@ -7,6 +7,13 @@ int main(int argc, const char * argv[]) {
         NSString *greeting = [[NSString alloc] initWithFormat:@"%@", @"I am Foundation String!"];
         
         NSLog(@"greeting's class is %@", [greeting class]);
+        
+        CFStringRef cstr = (__bridge_retained CFStringRef)(greeting);
+        
+        printf("String is %s\n", cstr);
+        NSLog(@"string is %@", cstr);
+        
+        CFRelease(cstr);
     }
     
     return 0;
