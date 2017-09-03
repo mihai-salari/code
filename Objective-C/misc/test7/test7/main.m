f8904cc82e93f928014fceaaaaa65a2d854cba29
choonsiong <lee93287@gmail.com>
2017-09-03 17:47:41 +0800

    First commit

--- Objective-C/misc/test7/test7/main.m
+++ Objective-C/misc/test7/test7/main.m
@@ -1,17 +1,9 @@
-//
-//  main.m
-//  test7
-//
-//  Created by Lee Choon Siong on 2017/09/03.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
     }
+    
     return 0;
 }
