319b1f6a9ce37c4e890729c570839498554e763a
choonsiong <lee93287@gmail.com>
2017-09-05 03:11:55 +0800

    First commit

--- Apps/SimpleError/SimpleError/ViewController.h
+++ Apps/SimpleError/SimpleError/ViewController.h
@@ -2,6 +2,9 @@
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UITextField *name;
+@property (nonatomic, weak) IBOutlet UITextField *city;
+@property (nonatomic, weak) IBOutlet UITextField *price;
 
 @end
 
