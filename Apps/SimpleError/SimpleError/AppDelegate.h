392a46627289cdb820f22713c4396b59a1cbc017
choonsiong <lee93287@gmail.com>
2017-09-05 03:03:24 +0800

    First commit

---
+++ Apps/SimpleError/SimpleError/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleError
+//
+//  Created by Lee Choon Siong on 2017/09/05.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Apps/SimpleError/SimpleError/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleError
+//
+//  Created by Lee Choon Siong on 2017/09/05.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
