2cd73eb6cdc58894410efe3ce2cf6e7fc2688c59
choonsiong <lee93287@gmail.com>
2017-09-05 04:40:48 +0800

    First commit

--- Objective-C/NSDictionaryMore/NSDictionaryMore/main.m
+++ Objective-C/NSDictionaryMore/NSDictionaryMore/main.m
@@ -22,3 +22,34 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ 2017-09-05 04:36:54.689678+0800 NSDictionaryMore[83108:5031637] keys = (
+ a,
+ b,
+ c
+ )
+ 2017-09-05 04:36:54.690182+0800 NSDictionaryMore[83108:5031637] values = (
+ 1,
+ 2,
+ 3
+ )
+ 2017-09-05 04:36:54.690342+0800 NSDictionaryMore[83108:5031637] dict1 = {
+ (
+ a,
+ b,
+ c
+ ) =     (
+ 1,
+ 2,
+ 3
+ );
+ }
+ 2017-09-05 04:36:54.690367+0800 NSDictionaryMore[83108:5031637] dict1 count = 1
+ 2017-09-05 04:36:54.690430+0800 NSDictionaryMore[83108:5031637] key is a
+ 2017-09-05 04:36:54.690449+0800 NSDictionaryMore[83108:5031637] key is b
+ 2017-09-05 04:36:54.690461+0800 NSDictionaryMore[83108:5031637] key is c
+ Program ended with exit code: 0
+
+*/
