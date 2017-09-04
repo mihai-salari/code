37f7b489c72f47d5dec5f555188242252b502ec5
choonsiong <lee93287@gmail.com>
2017-09-04 18:29:58 +0800

    First commit

---
+++ Objective-C/Calculator2/Calculator2/Calculator.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+@interface Calculator : NSObject
+
+- (NSNumber *)sumAdd:(NSNumber *)a and:(NSNumber *)b;
+- (NSNumber *)sumAdd:(NSNumber *)a :(NSNumber *)b;
+
+@end
---
+++ Objective-C/Calculator2/Calculator2/Calculator.m
@@ -0,0 +1,17 @@
+#import "Calculator.h"
+
+@implementation Calculator
+
+- (NSNumber *)sumAdd:(NSNumber *)a and:(NSNumber *)b {
+    NSLog(@"Invoking method on %@ object with selector %@", [self className], NSStringFromSelector(_cmd));
+    
+    return [NSNumber numberWithInteger:([a integerValue] + [b integerValue])];
+}
+
+- (NSNumber *)sumAdd:(NSNumber *)a :(NSNumber *)b {
+    NSLog(@"Invoking method on %@ object with selector %@", [self className], NSStringFromSelector(_cmd));
+    
+    return [NSNumber numberWithInteger:([a integerValue] + [b integerValue])];
+}
+
+@end
