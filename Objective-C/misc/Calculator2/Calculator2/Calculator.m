66c3c58d4ef82f3643e1f5c151e9486ae3e3a856
choonsiong <lee93287@gmail.com>
2017-09-04 22:34:03 +0800

    First commit

--- Objective-C/misc/Calculator2/Calculator2/Calculator.m
+++ Objective-C/misc/Calculator2/Calculator2/Calculator.m
@@ -1,6 +1,31 @@
 #import "Calculator.h"
+#import <objc/runtime.h>
 
 @implementation Calculator
+
+id absoluteValue(id self, SEL _cmd, id value) {
+    NSInteger intVal = [value integerValue];
+    
+    if (intVal < 0) {
+        return [NSNumber numberWithInt:(intVal * -1)];
+    }
+    
+    return value;
+}
+
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    NSString *method = NSStringFromSelector(sel);
+    
+    if ([method hasPrefix:@"absoluteValue"]) {
+        class_addMethod([self class], sel, (IMP)absoluteValue, "@@:@");
+        
+        NSLog(@"Dynamically added instance method %@ to class %@", method, [self class]);
+        
+        return YES;
+    }
+    
+    return [super resolveInstanceMethod:sel];
+}
 
 - (NSNumber *)sumAdd:(NSNumber *)a and:(NSNumber *)b {
     NSLog(@"Invoking method on %@ object with selector %@", [self className], NSStringFromSelector(_cmd));
