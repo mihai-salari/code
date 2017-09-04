246ad2653629acc533d28b7519874483163dc494
choonsiong <lee93287@gmail.com>
2017-09-05 03:22:31 +0800

    First commit

--- Apps/SimpleError/SimpleError/ViewController.h
+++ Apps/SimpleError/SimpleError/ViewController.h
@@ -1,10 +1,23 @@
 #import <UIKit/UIKit.h>
 
+extern NSString * const LCSMissingValuesKey;
+extern NSString * const LCSAccountExpirationDateKey;
+
+extern NSString * const LCSErrorDomain;
+
+enum {
+    LCSValidationError = 1,
+    LCSAccountExpiredError = 2,
+    LCSWrongCredentialsError = 3
+};
+
 @interface ViewController : UIViewController
 
 @property (nonatomic, weak) IBOutlet UITextField *name;
 @property (nonatomic, weak) IBOutlet UITextField *city;
 @property (nonatomic, weak) IBOutlet UITextField *price;
+
++ (BOOL)publishAd:(NSDictionary *)ad error:(NSError **)err;
 
 @end
 
--- Apps/SimpleError/SimpleError/ViewController.m
+++ Apps/SimpleError/SimpleError/ViewController.m
@@ -1,10 +1,46 @@
 #import "ViewController.h"
 
+NSString * const LCSErrorDomain = @"com.example.API";
+NSString * const LCSMissingValuesKey = @"LCSMissingValuesKey";
+NSString * const LCSAccountExpirationDateKey = @"LCSAccountExpirationDateKey";
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
++ (BOOL)publishAd:(NSDictionary *)ad error:(NSError *__autoreleasing *)err {
+    if (ad == nil) {
+        @throw [NSException exceptionWithName:@"LCSBadAPICall"
+                                       reason:@"ad dictionary is nil"
+                                     userInfo:nil];
+    }
+    
+    NSMutableArray *missingValues = [NSMutableArray array];
+    
+    for (NSString *key in [@"name city price" componentsSeparatedByString:@" "]) {
+        if ([[ad objectForKey:key] length] == 0) {
+            [missingValues addObject:key];
+        }
+    }
+    
+    if ([missingValues count] > 0) {
+        if (err != NULL) {
+            NSString *description = @"The ad could not be published because some required values are missing.";
+            NSString *recoverySuggestion = @"Please provide the missing values and try again.";
+            
+            NSArray *keys = [NSArray arrayWithObjects:NSLocalizedDescriptionKey, NSLocalizedRecoverySuggestionErrorKey, LCSMissingValuesKey, nil];
+            NSArray *values = [NSArray arrayWithObjects:description, recoverySuggestion, missingValues, nil];
+            
+            NSDictionary *userDict = [NSDictionary dictionaryWithObject:values forKey:keys];
+        }
+        
+        return NO;
+    } else {
+        return YES;
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
