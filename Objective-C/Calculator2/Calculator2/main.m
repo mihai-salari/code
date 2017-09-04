0fc68ae5892d54c29d3d8015552d49c120394e94
choonsiong <lee93287@gmail.com>
2017-09-04 18:34:00 +0800

    First commit

--- Objective-C/Calculator2/Calculator2/main.m
+++ Objective-C/Calculator2/Calculator2/main.m
@@ -1,8 +1,15 @@
 #import <Foundation/Foundation.h>
+#import "Calculator.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Calculator *cal = [Calculator new];
         
+        NSNumber *n1 = @25;
+        NSNumber *n2 = @30;
+        NSNumber *n3 = @88;
+        
+        NSLog(@"Sum of %@ + %@ = %@", n1, n2, [cal sumAdd:n1 and:n2]);
     }
     
     return 0;
