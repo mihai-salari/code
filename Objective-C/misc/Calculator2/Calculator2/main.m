32daf0bf3a3c3d34adeecc6f6073a7b5810f2ede
choonsiong <lee93287@gmail.com>
2017-09-04 19:50:15 +0800

    First commit

--- Objective-C/misc/Calculator2/Calculator2/main.m
+++ Objective-C/misc/Calculator2/Calculator2/main.m
@@ -12,9 +12,13 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Sum of %@ + %@ = %@", n1, n2, [cal sumAdd:n1 and:n2]);
         NSLog(@"Sum of %@ + %@ = %@", n1, n3, [cal sumAdd:n1 :n3]);
         
-        SEL selector1 = @selector(sumAdd:and:);
+        SEL selector1 = @selector(sumAdd:and:); // create at compile time
         id sum1 = [cal performSelector:selector1 withObject:n1 withObject:n2];
         NSLog(@"Sum of %@ + %@ = %@", n1, n2, sum1);
+        
+        SEL selector2 = NSSelectorFromString(@"sumAdd::"); // create at runtime
+        id sum2 = [cal performSelector:selector2 withObject:n1 withObject:n3];
+        NSLog(@"Sum of %@ + %@ = %@", n1, n2, sum2);
     }
     
     return 0;
