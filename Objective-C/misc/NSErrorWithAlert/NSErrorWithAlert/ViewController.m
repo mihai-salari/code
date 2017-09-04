d0ed9918f19b6391a298f11025bd44a1d38d3a3b
choonsiong <lee93287@gmail.com>
2017-09-05 01:05:57 +0800

    First commit

--- Objective-C/misc/NSErrorWithAlert/NSErrorWithAlert/ViewController.m
+++ Objective-C/misc/NSErrorWithAlert/NSErrorWithAlert/ViewController.m
@@ -9,6 +9,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSError *error;
+    NSData *data = nil;
+    
+    // trying to load a nonexistent file
+    data = [NSData dataWithContentsOfFile:@"foo.txt"
+                                  options:NSDataReadingUncached
+                                    error:&error]; // pass the address of pointer variable
+    
+    // data == nil?
+    if (data) {
+        printf("data is not nil\n");
+        NSLog(@"Everything is fine ;)");
+    } else {
+        printf("data is nil\n");
+        NSLog(@"An error occurred: %@", [error localizedDescription]);
+        NSLog(@"Error Domain = %@", [error domain]);
+        NSLog(@"Error Code = %lu", [error code]);
+        NSLog(@"localizedFailureReason = %@", [error localizedFailureReason]);
+        NSLog(@"localizedRecoverySuggestion = %@", [error localizedRecoverySuggestion]);
+        NSLog(@"localizedRecoveryOptions = %@", [error localizedRecoveryOptions]);
+        
+        // Examining userInfo dictionary
+        NSLog(@"userInfo = %@", [error userInfo]);
+        
+        // A more complicated way to examining the error
+        NSDictionary *userInfo = [error userInfo];
+        NSString *errorMsg;
+        
+        if (userInfo && [userInfo objectForKey:NSUnderlyingErrorKey]) {
+            NSError *underlyingError = [userInfo objectForKey:NSUnderlyingErrorKey];
+            errorMsg = [underlyingError localizedDescription];
+        } else {
+            errorMsg = [error localizedDescription];
+        }
+        
+        NSLog(@"Error: %@", errorMsg);
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:errorMsg
+                                                       delegate:nil
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"OK", nil];
+        
+        [alert show];
+    }
 }
 
 
