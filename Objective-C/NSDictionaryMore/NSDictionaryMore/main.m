e676b77a106b101a72edd0313562ae4aa611e75a
choonsiong <lee93287@gmail.com>
2017-09-05 04:32:57 +0800

    First commit

--- Objective-C/NSDictionaryMore/NSDictionaryMore/main.m
+++ Objective-C/NSDictionaryMore/NSDictionaryMore/main.m
@@ -2,7 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSArray *keys = [NSArray arrayWithObjects:@"a", @"b", @"c", nil];
+        NSArray *values = [NSArray arrayWithObjects:@1, @2, @3, nil];
         
+        NSLog(@"keys = %@", keys);
+        NSLog(@"values = %@", values);
         
     }
     
