a77e5b7ffb3e82b7f5fcf9b63cf4fa4f6d07a5f5
choonsiong <lee93287@gmail.com>
2017-09-04 22:40:43 +0800

    First commit

--- Objective-C/DynamicMethodResolution/DynamicMethodResolution/main.m
+++ Objective-C/DynamicMethodResolution/DynamicMethodResolution/main.m
@@ -14,3 +14,12 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ 2017-09-04 22:39:25.670059+0800 DynamicMethodResolution[75897:4600100] Invoking instance method absoluteValue: on object of class Calculator
+ 2017-09-04 22:39:25.670268+0800 DynamicMethodResolution[75897:4600100] Dynamically added instance method absoluteValue: to class Calculator
+ 2017-09-04 22:39:25.670312+0800 DynamicMethodResolution[75897:4600100] Absolute value of -123 = 123
+ Program ended with exit code: 0
+
+*/
