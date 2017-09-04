904f3506b895a06c51d99ee60b1b7a1f320737f4
choonsiong <lee93287@gmail.com>
2017-09-05 04:38:07 +0800

    First commit

--- Objective-C/NSDictionaryMore/NSDictionaryMore/main.m
+++ Objective-C/NSDictionaryMore/NSDictionaryMore/main.m
@@ -8,6 +8,16 @@ int main(int argc, const char * argv[]) {
         NSLog(@"keys = %@", keys);
         NSLog(@"values = %@", values);
         
+        NSDictionary *dict1 = [NSDictionary dictionaryWithObject:values forKey:keys];
+        
+        NSLog(@"dict1 = %@", dict1);
+        NSLog(@"dict1 count = %lu", [dict1 count]); // Only one item!
+        
+        for (id key in dict1) {
+            for (NSString *k in key) { // Two level deep
+                NSLog(@"key is %@", k);
+            }
+        }
     }
     
     return 0;
