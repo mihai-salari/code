a1aa99d7ced139b5ab82f17c07374cc2786723f9
choonsiong <lee93287@gmail.com>
2017-09-04 18:07:47 +0800

    First commit

--- Objective-C/OneToMoreSelectorToMethods/OneToMoreSelectorToMethods/main.m
+++ Objective-C/OneToMoreSelectorToMethods/OneToMoreSelectorToMethods/main.m
@@ -1,8 +1,17 @@
 #import <Foundation/Foundation.h>
+#import "KlassA.h"
+#import "KlassB.h"
+
+// The selectors for both KlassA and KlassB are same, but Objective-C
+// runtime is able to call them correctly!
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        KlassA *a = [KlassA new];
+        [a sayHelloTo:@"Foo Bar"];
         
+        KlassB *b = [KlassB new];
+        [b sayHelloTo:@"Bar Foo"];
     }
     
     return 0;
