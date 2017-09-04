b44011f75c8f261885f21233be04d6f7ae3855a2
choonsiong <lee93287@gmail.com>
2017-09-04 22:39:36 +0800

    First commit

--- Objective-C/DynamicMethodResolution/DynamicMethodResolution/main.m
+++ Objective-C/DynamicMethodResolution/DynamicMethodResolution/main.m
@@ -3,7 +3,13 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        Calculator *c = [Calculator new];
+        SEL sel1 = NSSelectorFromString(@"absoluteValue:");
         
+        NSLog(@"Invoking instance method %@ on object of class %@", NSStringFromSelector(sel1), [c className]);
+        
+        id sum1 = [c performSelector:sel1 withObject:@-123];
+        NSLog(@"Absolute value of %@ = %@", @"-123", sum1);
     }
     
     return 0;
