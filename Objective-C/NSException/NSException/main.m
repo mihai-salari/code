e48ddad8a3e6fbb5252ad608032582147268999b
choonsiong <lee93287@gmail.com>
2017-09-05 16:26:54 +0800

    First commit

--- Objective-C/NSException/NSException/main.m
+++ Objective-C/NSException/NSException/main.m
@@ -2,7 +2,13 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSString *name = nil;
         
+        if (name == nil) {
+            @throw [NSException exceptionWithName:NSInvalidArgumentException
+                                           reason:@"name is nil"
+                                         userInfo:nil];
+        }
     }
     
     return 0;
