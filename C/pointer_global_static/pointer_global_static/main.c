044c3d64ccc935531bdc15a5eb2e71cbb8893367
choonsiong <lee93287@gmail.com>
2017-09-08 01:26:02 +0800

    First commit

--- C/pointer_global_static/pointer_global_static/main.c
+++ C/pointer_global_static/pointer_global_static/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+int *globalPtr;
+
+static int* staticPtr;
+
 int main(int argc, const char * argv[]) {
+    
+    if (globalPtr) {
+        printf("global pointer is not null\n");
+    } else {
+        printf("global pointer is null\n");
+    }
     
     return 0;
 }
