fc3711c388e75c57cf79db38499a4fe7464e3b1e
choonsiong <lee93287@gmail.com>
2017-09-07 23:53:53 +0800

    First commit

--- C/pointer_to_void/pointer_to_void/main.c
+++ C/pointer_to_void/pointer_to_void/main.c
@@ -3,5 +3,12 @@
 int main(int argc, const char * argv[]) {
     void *voidPtr; // pointer to void
     
+    int num = 10;
+    int *numPtr = &num;
+    
+    printf("Value of numPtr = %p\n", numPtr);
+    
+    voidPtr = numPtr;
+    
     return 0;
 }
