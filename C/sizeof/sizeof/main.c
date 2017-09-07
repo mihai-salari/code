16089914333ac8c139c6e9427f47f173d1c1b6f2
choonsiong <lee93287@gmail.com>
2017-09-08 00:51:33 +0800

    First commit

---
+++ C/sizeof/sizeof/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sizeof
+//
+//  Created by Lee Choon Siong on 2017/09/08.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
