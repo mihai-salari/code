82100824ac60c4b3fe9aa4d2e600d3389c366986
choonsiong <lee93287@gmail.com>
2017-09-07 21:38:18 +0800

    First commit

--- C/pointer_NULL/pointer_NULL/main.c
+++ C/pointer_NULL/pointer_NULL/main.c
@@ -5,7 +5,13 @@ int main(int argc, const char * argv[]) {
     int *intPtr = NULL; // does not point to anything yet...
     intPtr = 0;
     
-    intPtr = 100; // error
+    //intPtr = 100; // error
+    
+    if (intPtr) {
+        printf("I am not NULL\n");
+    } else {
+        printf("I am NULL\n");
+    }
     
     
     return 0;
