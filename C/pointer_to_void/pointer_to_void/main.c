8ac1b2975fcfb5352426adb2464e94f7aad468af
choonsiong <lee93287@gmail.com>
2017-09-07 23:56:06 +0800

    First commit

--- C/pointer_to_void/pointer_to_void/main.c
+++ C/pointer_to_void/pointer_to_void/main.c
@@ -3,6 +3,10 @@
 int main(int argc, const char * argv[]) {
     void *voidPtr; // pointer to void
     
+    // Any pointer can be assigned to a pointer to void.
+    // It can then be cast back to its original pointer type. When this happens
+    // the value will be equal to the original pointer value.
+    
     int num = 10;
     int *numPtr = &num;
     
@@ -13,6 +17,14 @@ int main(int argc, const char * argv[]) {
     numPtr = (int *)voidPtr;
     
     printf("Value of numPtr = %p\n", numPtr);
+    
+    if (voidPtr == numPtr) {
+        printf("Both pointers are equal");
+    } else {
+        printf("Both pointers are not equal");
+    }
+    
+    printf("\n");
     
     return 0;
 }
