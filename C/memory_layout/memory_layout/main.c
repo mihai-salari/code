3f6c244b8fa669cfe95c9b683507e6236c305c4a
choonsiong <lee93287@gmail.com>
2017-09-08 01:40:49 +0800

    First commit

--- C/memory_layout/memory_layout/main.c
+++ C/memory_layout/memory_layout/main.c
@@ -9,11 +9,14 @@ void funcFoo(void) {
     static int staticInt = 1234;
     static int *staticIntPtr = &staticInt;
     printf("staticIntPtr lives at %d\n", &staticIntPtr);
+    printf("staticIntPtr lives at %p\n", &staticIntPtr);
     printf("staticInt value lives at %d\n", &staticInt);
+    printf("staticInt value lives at %p\n", &staticInt);
     
     int normalInt = 1234;
     
     printf("normalInt lives at %d\n", &normalInt);
+    printf("normalInt lives at %p\n", &normalInt);
 }
 
 int main(int argc, const char * argv[]) {
@@ -32,3 +35,16 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ globalIntPtr lives at 4136
+ staticIntPtr lives at 4152
+ staticInt value lives at 4144
+ normalInt lives at 1606415836
+ main lives at 3632
+ char c lives at 1606415880
+ globalInt value lives at 4128
+ Program ended with exit code: 0
+
+*/
