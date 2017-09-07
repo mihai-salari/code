83a2d089bdd486c03afa90afddf5bbc96a4021e2
choonsiong <lee93287@gmail.com>
2017-09-07 18:31:04 +0800

    First commit

--- C/linked_list_array/linked_list_array/main.c
+++ C/linked_list_array/linked_list_array/main.c
@@ -11,8 +11,28 @@ int main(int argc, const char * argv[]) {
     foo->age = 20;
     
     printf("Foo's age is %d\n", foo->age);
+    printf("Size of Foo is %zu bytes\n", sizeof(foo));
 
     free(foo);
+    
+    Person *persons[100];
+    
+    int head = 0; // first index
+    int next = 1;
+    
+    for (head = 0; head < 100; head++) {
+        Person *p = (Person *)malloc(sizeof(Person));
+        p->age = arc4random_uniform(100) + 1;
+        p->next = next;
+        persons[head] = p;
+        
+        printf("Person's age is %d\n", p->age);
+        printf("Person's next is %d\n", p->next);
+        
+        next += 1;
+    }
+    
+    printf("Size of persons is %zu bytes\n", sizeof(persons));
     
     return 0;
 }
