3b25a5554fa67a392c4c325b5841274cfa406867
choonsiong <lee93287@gmail.com>
2017-09-07 21:33:58 +0800

    First commit

--- C/pointer_NULL/pointer_NULL/main.c
+++ C/pointer_NULL/pointer_NULL/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    // null pointer
     int *intPtr = NULL; // does not point to anything yet...
+    intPtr = 0;
+    
+    intPtr = 100; // error
+    
     
     return 0;
 }
