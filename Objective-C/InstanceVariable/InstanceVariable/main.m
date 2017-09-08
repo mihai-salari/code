8807297bdf0101f32e4caa7022c1ab703a61ed5b
choonsiong <lee93287@gmail.com>
2017-09-09 03:05:23 +0800

    First commit

--- Objective-C/InstanceVariable/InstanceVariable/main.m
+++ Objective-C/InstanceVariable/InstanceVariable/main.m
@@ -1,3 +1,8 @@
+// Instance variables declared either explicitly in the implementation section
+// (or implicitly by using the @synthesize directive) are made private, meaning
+// they're not accessible to subclasses directly by name. So, subclasses have no
+// choice but to use the inherited accessor methods in order to access their values.
+
 #import <Foundation/Foundation.h>
 #import "Person.h"
 #import "Foo.h"
@@ -7,6 +12,8 @@ int main(int argc, const char * argv[]) {
         Foo *foo = [Foo new];
         
         NSLog(@"Foo's name is %@, age is %d", [foo name], [foo age]);
+        
+        [foo printName];
     }
     
     return 0;
