a475f0db23bb11dc845f673335780b6bab0275e3
choonsiong <lee93287@gmail.com>
2017-09-09 03:39:11 +0800

    First commit

--- Objective-C/GlobalAndExternalVariableRealUse/GlobalAndExternalVariableRealUse/main.m
+++ Objective-C/GlobalAndExternalVariableRealUse/GlobalAndExternalVariableRealUse/main.m
@@ -1,9 +1,15 @@
 #import <Foundation/Foundation.h>
+#import "Foo.h"
 
 int gNumber = 0;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSLog(@"global number is %i", gNumber);
+        
+        Foo *f = [Foo new];
+        [f setGlobalNumber:42];
+        
         NSLog(@"global number is %i", gNumber);
     }
     
