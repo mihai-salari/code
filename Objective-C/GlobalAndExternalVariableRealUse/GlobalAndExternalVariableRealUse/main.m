9e846e45014b66e433f95ca7f46290c7f8d7a294
choonsiong <lee93287@gmail.com>
2017-09-09 03:35:37 +0800

    First commit

--- Objective-C/GlobalAndExternalVariableRealUse/GlobalAndExternalVariableRealUse/main.m
+++ Objective-C/GlobalAndExternalVariableRealUse/GlobalAndExternalVariableRealUse/main.m
@@ -1,17 +1,11 @@
-//
-//  main.m
-//  GlobalAndExternalVariableRealUse
-//
-//  Created by Lee Choon Siong on 2017/09/09.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
+
+int gNumber = 0;
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"global number is %i", gNumber);
     }
+    
     return 0;
 }
