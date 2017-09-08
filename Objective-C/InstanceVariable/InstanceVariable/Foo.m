000d9c15e86eef8c31f95c05c696852094ca8425
choonsiong <lee93287@gmail.com>
2017-09-09 03:07:00 +0800

    First commit

--- Objective-C/InstanceVariable/InstanceVariable/Foo.m
+++ Objective-C/InstanceVariable/InstanceVariable/Foo.m
@@ -7,11 +7,3 @@
 }
 
 @end
-
-/*
-
- 2017-09-09 03:03:28.583786+0800 InstanceVariable[38780:2165921] Foo's name is unknown, age is 0
- 2017-09-09 03:03:28.583955+0800 InstanceVariable[38780:2165921] My name is unknown
- Program ended with exit code: 0
-
-*/
--- Objective-C/InstanceVariable/InstanceVariable/main.m
+++ Objective-C/InstanceVariable/InstanceVariable/main.m
@@ -18,3 +18,11 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ 2017-09-09 03:03:28.583786+0800 InstanceVariable[38780:2165921] Foo's name is unknown, age is 0
+ 2017-09-09 03:03:28.583955+0800 InstanceVariable[38780:2165921] My name is unknown
+ Program ended with exit code: 0
+
+*/
