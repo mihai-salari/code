c74b35eb68011ddb08772d2bb52d21418e7323d3
choonsiong <lee93287@gmail.com>
2017-09-10 17:17:21 +0800

    First commit

--- Objective-C/StaticVariable/StaticVariable/main.m
+++ Objective-C/StaticVariable/StaticVariable/main.m
@@ -7,6 +7,13 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"I am %@", foo.name);
         NSLog(@"There is %i foo", [Foo fooCount]);
+        
+        for (int i = 0; i < 10; i++) {
+            Foo *f = [Foo fooWithName:[NSString stringWithFormat:@"Foo_%d", i]];
+            
+            NSLog(@"I am %@", f.name);
+            NSLog(@"There is %i instance of f", [Foo fooCount]);
+        }
     }
     
     return 0;
