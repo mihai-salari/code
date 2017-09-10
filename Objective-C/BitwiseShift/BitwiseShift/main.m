043d40f0c1b389e21d68189fde97890a4ccf3737
choonsiong <lee93287@gmail.com>
2017-09-10 21:43:56 +0800

    First commit

--- Objective-C/BitwiseShift/BitwiseShift/main.m
+++ Objective-C/BitwiseShift/BitwiseShift/main.m
@@ -2,7 +2,10 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        unsigned int a = 1;
         
+        printf("a << 1 is %d\n", a << 1);
+        printf("a >> 1 is %d\n", a >> 1);
     }
     
     return 0;
