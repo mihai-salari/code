334cabe41923a25dbd76e35a140413fc86c26dbf
choonsiong <lee93287@gmail.com>
2017-09-10 17:26:54 +0800

    First commit

---
+++ Objective-C/ClassInitializeMethod/ClassInitializeMethod/Foo.h
@@ -0,0 +1,13 @@
+//
+//  Foo.h
+//  ClassInitializeMethod
+//
+//  Created by Lee Choon Siong on 2017/09/10.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Foo : NSObject
+
+@end
---
+++ Objective-C/ClassInitializeMethod/ClassInitializeMethod/Foo.m
@@ -0,0 +1,13 @@
+//
+//  Foo.m
+//  ClassInitializeMethod
+//
+//  Created by Lee Choon Siong on 2017/09/10.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import "Foo.h"
+
+@implementation Foo
+
+@end
