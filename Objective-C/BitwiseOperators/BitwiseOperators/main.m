e7c766327157d06b316aa8a60b8d6f8c5ed360bc
choonsiong <lee93287@gmail.com>
2017-09-10 21:19:09 +0800

    First commit

--- Objective-C/BitwiseOperators/BitwiseOperators/main.m
+++ Objective-C/BitwiseOperators/BitwiseOperators/main.m
@@ -2,7 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int a = 0b0001;
+        int b = 0b0010;
         
+        printf("a is %d\n", a);
+        printf("b is %d\n", b);
     }
     
     return 0;
