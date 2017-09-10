d6209188b33ff1484b46b647640679744990c3c8
choonsiong <lee93287@gmail.com>
2017-09-10 21:41:25 +0800

    First commit

--- Objective-C/BitwiseOperation/BitwiseOperation/main.m
+++ Objective-C/BitwiseOperation/BitwiseOperation/main.m
@@ -13,3 +13,14 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
+
+/*
+
+ 2017-09-10 21:41:13.361746+0800 BitwiseOperation[60997:3791527] a0a00000 ffffa0a0 5f5fa0a0
+ 2017-09-10 21:41:13.361888+0800 BitwiseOperation[60997:3791527] 5f5f5f5f ffff ffff8888
+ 2017-09-10 21:41:13.361903+0800 BitwiseOperation[60997:3791527] 0 a0a0 fffff7f7
+ 2017-09-10 21:41:13.361916+0800 BitwiseOperation[60997:3791527] a0a0a0a0 ffffa0a0
+ 2017-09-10 21:41:13.361926+0800 BitwiseOperation[60997:3791527] ffffa0a0 a0a00000
+ Program ended with exit code: 0
+
+*/
