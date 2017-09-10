f6ea412d87bba5651baf79af65feda3e2c08a748
choonsiong <lee93287@gmail.com>
2017-09-10 21:38:50 +0800

    First commit

---
+++ Objective-C/BitwiseOperation/BitwiseOperation/main.m
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        unsigned int w1 = 0xA0A0A0A0, w2 = 0xFFFF0000, w3 = 0x00007777;
+        
+        NSLog(@"%x %x %x", w1 & w2 , w1 | w2, w1 ^ w2);
+        NSLog(@"%x %x %x", ~w1, ~w2, ~w3);
+        NSLog(@"%x %x %x", w1 ^ w1, w1 & ~w2, w1 | w2 | w3);
+    }
+    
+    return 0;
+}
