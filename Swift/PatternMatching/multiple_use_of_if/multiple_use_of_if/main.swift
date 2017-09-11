e510fb22d1aab859a4e8fea57925e64febb9d308
choonsiong <lee93287@gmail.com>
2017-09-11 21:23:56 +0800

    First commit

--- Swift/PatternMatching/multiple_use_of_if/multiple_use_of_if/main.swift
+++ Swift/PatternMatching/multiple_use_of_if/multiple_use_of_if/main.swift
@@ -10,6 +10,12 @@ let a = 5, b = 6
 let c: Number? = .integerValue(7)
 let d: Number? = .integerValue(8)
 
+// An if statement can have multiple conditions, separated by commas.
+// Conditions fall into one of three categories:
+// - simple logical test e.g. a == 10 || b > c
+// - optional binding e.g. let a = optionalA
+// - pattern matching e.g. case .some(let value) = theValue
+
 if a != b, let c = c, let d = d, case .integerValue(let cValue) = c, case .integerValue(let dValue) = d, dValue > cValue {
     print("a and b are different")
     print("d is greater than c")
