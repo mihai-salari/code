911ad48e9946d0a0c11b4381e672912fc139d78e
choonsiong <lee93287@gmail.com>
2017-09-11 19:47:40 +0800

    First commit

--- Swift/PatternMatching/type_casting_pattern_as/type_casting_pattern_as/main.swift
+++ Swift/PatternMatching/type_casting_pattern_as/type_casting_pattern_as/main.swift
@@ -4,12 +4,12 @@ let jsonArray: [Any] = [15, "George", 2.0]
 
 for element in jsonArray {
     switch element {
-    case is String:
-        print("Found a String")
-    case is Int:
-        print("Found an Int")
-    case is Double:
-        print("Found a Double")
+    case let text as String:
+        print("Found a String: \(text)")
+    case let number as Int:
+        print("Found an Int: \(number)")
+    case let number as Double:
+        print("Found a Double: \(number)")
     default:
         print("Found something")
     }
