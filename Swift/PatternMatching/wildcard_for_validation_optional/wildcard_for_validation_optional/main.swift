766f3698a9eda14f58c0412d2c871ffeee2d985e
choonsiong <lee93287@gmail.com>
2017-09-11 21:37:12 +0800

    First commit

--- Swift/PatternMatching/wildcard_for_validation_optional/wildcard_for_validation_optional/main.swift
+++ Swift/PatternMatching/wildcard_for_validation_optional/wildcard_for_validation_optional/main.swift
@@ -2,8 +2,8 @@ import Foundation
 
 let user: String? = "foo"
 
-// Just to make sure user has a value, we don't care what the value it
-// contains...
+// Just to make sure user variable has a value, we don't care what the value it
+// contains at the moment...
 guard let _ = user else {
     print("There is no user.")
     fatalError("Error: nil user exception")
