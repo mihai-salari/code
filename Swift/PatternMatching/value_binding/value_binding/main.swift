2915b6e9b2aaa4b8e0442e505a49219fecea418f
choonsiong <lee93287@gmail.com>
2017-09-11 18:55:38 +0800

    First commit

--- Swift/PatternMatching/value_binding/value_binding/main.swift
+++ Swift/PatternMatching/value_binding/value_binding/main.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-let point = (x: 1, y: 1)
+let point = (x: 1, y: 1, z: 0)
 
-// Match any value on the x-axis and y-axis
-if case (let x, let y) = point {
-    print("Point is x: \(x), y: \(y)")
+// Match any value on the x-axis and y-axis, and 0 on the z-axis
+if case (let x, let y, 0) = point {
+    print("Point is x: \(x), y: \(y), z: 0")
+}
+
+if case let (x, y, 0) = point {
+    print("Point is x: \(x), y: \(y), z: 0")
 }
