0634649b2388f45c0b49dc0ecffae6692bb49786
choonsiong <lee93287@gmail.com>
2017-09-11 22:37:06 +0800

    First commit

--- Swift/PatternMatching/switch_case_fizzbuzz/switch_case_fizzbuzz/main.swift
+++ Swift/PatternMatching/switch_case_fizzbuzz/switch_case_fizzbuzz/main.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 for i in 1...100 {
-    
+    switch (i % 3, i % 5) {
+    case (0, 0):
+        print("FizzBuzz", terminator: " ")
+    case (0, _):
+        print("Fizz", terminator: " ")
+    case (_, 0):
+        print("Buzz", terminator: " ")
+    case (_, _):
+        print(i, terminator: " ")
+    }
 }
+
+print("")
