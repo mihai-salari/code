110aaabf35c48a9aaf227ec2330abbd692011d2b
choonsiong <lee93287@gmail.com>
2017-09-11 23:56:41 +0800

    First commit

--- Swift/PatternMatching/pattern_matching_operator/pattern_matching_operator/main.swift
+++ Swift/PatternMatching/pattern_matching_operator/pattern_matching_operator/main.swift
@@ -1,8 +1,15 @@
 import Foundation
 
+// Using pattern matching operator
 let matched = (1...10 ~= 8)
 
 if matched {
+    print("True")
+} else {
+    print("False")
+}
+
+if case 1...10 = 5 {
     print("True")
 } else {
     print("False")
