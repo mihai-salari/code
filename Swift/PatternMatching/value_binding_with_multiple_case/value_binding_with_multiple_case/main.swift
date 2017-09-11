b0ccb1d9e8747f548196c238f91fdc8e97bf55e1
choonsiong <lee93287@gmail.com>
2017-09-11 21:18:24 +0800

    First commit

--- Swift/PatternMatching/value_binding_with_multiple_case/value_binding_with_multiple_case/main.swift
+++ Swift/PatternMatching/value_binding_with_multiple_case/value_binding_with_multiple_case/main.swift
@@ -8,6 +8,12 @@ enum Age {
 
 let fooAge = Age.some(value: 36)
 
+// An if statement can have multiple conditions, separated by commas.
+// Conditions fall into one of three categories:
+// - simple logical test e.g. a == 10 || b > c
+// - optional binding e.g. let a = optionalA
+// - pattern matching e.g. case .some(let value) = theValue
+
 if case .some(let age) = fooAge, case 30...40 = age {
     print("such an old man")
 } else {
