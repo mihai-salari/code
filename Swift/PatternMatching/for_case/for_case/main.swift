266f3aab6705ca3b25a4e4b8c4cf4e501d695db7
choonsiong <lee93287@gmail.com>
2017-09-11 18:06:30 +0800

    First commit

--- Swift/PatternMatching/for_case/for_case/main.swift
+++ Swift/PatternMatching/for_case/for_case/main.swift
@@ -1,2 +1,23 @@
 import Foundation
 
+let groupSizes = [1, 5, 4, 6, 2, 1, 3]
+
+for case 1 in groupSizes {
+    print("Found an individual!")
+}
+
+for case 1...3 in groupSizes {
+    print("Found a small group!")
+}
+
+/*
+ 
+ Found an individual!
+ Found an individual!
+ Found a small group!
+ Found a small group!
+ Found a small group!
+ Found a small group!
+ Program ended with exit code: 0
+ 
+*/
