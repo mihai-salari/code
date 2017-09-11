a491e11b9e1bbd7b858bfa326a26cb6411ad67ff
choonsiong <lee93287@gmail.com>
2017-09-11 22:33:18 +0800

    First commit

---
+++ Swift/PatternMatching/switch_case_fizzbuzz/switch_case_fizzbuzz/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  switch_case_fizzbuzz
+//
+//  Created by Lee Choon Siong on 2017/09/11.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
