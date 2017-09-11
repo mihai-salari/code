3105c17a4dae4c7408830ef3504c9653210bfa0e
choonsiong <lee93287@gmail.com>
2017-09-11 18:00:02 +0800

    First commit

---
+++ Swift/PatternMatching/switch_case2/switch_case2/main.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func process(population: Int) -> String {
+    switch population {
+    case 0:
+        return "zero people"
+    case 1:
+        return "only one person"
+    case 2...4:
+        return "a few people"
+    case 5...8:
+        return "several people"
+    case 9...15:
+        return "many people"
+    default:
+        return "too many people"
+    }
+}
