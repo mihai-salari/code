61d916b8484f90142666f561739dae963b7f011e
choonsiong <lee93287@gmail.com>
2017-09-12 01:17:14 +0800

    First commit

--- Swift/PatternMatching/example_monument_location/example_monument_location/main.swift
+++ Swift/PatternMatching/example_monument_location/example_monument_location/main.swift
@@ -1,2 +1,14 @@
 import Foundation
 
+let coordinate = (lat: 192.89483, long: -68.887463)
+
+switch coordinate {
+case (0, _):
+    print("on the equator")
+case (let x, _) where x > 0:
+    print("on the northern hemisphere")
+case (let x, _) where x < 0:
+    print("on the southern hemisphere")
+default:
+    print("not able to determine location")    
+}
