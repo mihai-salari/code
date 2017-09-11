c0b4a1b97feae92baa540f5bb71fb0fa86862023
choonsiong <lee93287@gmail.com>
2017-09-11 17:48:16 +0800

    First commit

--- Swift/PatternMatching/if_case2/if_case2/main.swift
+++ Swift/PatternMatching/if_case2/if_case2/main.swift
@@ -1,4 +1,13 @@
 import Foundation
 
-print("Hello, World!")
+func process(point: (x: Int, y: Int, z: Int)) -> String {
+    if case (0, 0, 0) = point {
+        return "at origin"
+    }
+    
+    return "not at origin"
+}
 
+let point = (x: 0, y: 0, z: 0)
+
+print("Point is \(process(point: point)).")
