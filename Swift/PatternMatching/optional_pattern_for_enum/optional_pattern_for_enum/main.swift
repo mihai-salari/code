6c5f032c99a4a8128038d03ade5368229f3c95b1
choonsiong <lee93287@gmail.com>
2017-09-11 19:37:06 +0800

    First commit

--- Swift/PatternMatching/optional_pattern_for_enum/optional_pattern_for_enum/main.swift
+++ Swift/PatternMatching/optional_pattern_for_enum/optional_pattern_for_enum/main.swift
@@ -1,2 +1,21 @@
 import Foundation
 
+enum ErrorCode {
+    case invalidUserName
+    case invalidPassword
+    case other(message: String?)
+}
+
+var result = ErrorCode.other(message: nil)
+result = ErrorCode.other(message: "Stupid user")
+
+switch result {
+case .invalidUserName:
+    print("Invalid user name")
+case .invalidPassword:
+    print("Invalid password")
+case .other(let value?): // This case will only execute if value is not nil
+    print("Error: \(value)")
+default:
+    print("Unknown error")
+}
