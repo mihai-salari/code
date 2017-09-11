d13d9adf80b2dbde548a97ff034e8880e2a6e899
choonsiong <lee93287@gmail.com>
2017-09-11 23:50:21 +0800

    First commit

--- Swift/PatternMatching/switch_case_fizzbuzz/switch_case_fizzbuzz/main.swift
+++ Swift/PatternMatching/switch_case_fizzbuzz/switch_case_fizzbuzz/main.swift
@@ -3,12 +3,12 @@ import Foundation
 for i in 1...100 {
     switch (i % 3, i % 5) {
     case (0, 0):
-        print("FizzBuzz", terminator: " ")
+        print("FizzBuzz", terminator: " ") // to end each line with a space character instead of a newline
     case (0, _):
         print("Fizz", terminator: " ")
     case (_, 0):
         print("Buzz", terminator: " ")
-    case (_, _):
+    case (_, _): // matches any value
         print(i, terminator: " ")
     }
 }
