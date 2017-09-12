86b9fc9d80961b933bf1bf106e656e71b560dd27
choonsiong <lee93287@gmail.com>
2017-09-12 15:51:21 +0800

    First commit

--- Shell_Scripts/sum_of_numbers.sh
+++ Shell_Scripts/sum_of_numbers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -
 
+# Following line use the tr command to replace newline character in each line
+# with a + character, and then send the output to bc for calculation.
+
 echo "$(cat /Users/choonsiong/Code/Shell_Scripts/tmp/numbers.txt | tr '\n' '+')" | bc
