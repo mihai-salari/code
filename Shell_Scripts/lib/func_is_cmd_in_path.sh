048ffd830a75029c661bfe89fc2ae93193e881f5
choonsiong <lee93287@gmail.com>
2017-09-12 18:52:43 +0800

    First commit

--- Shell_Scripts/lib/func_is_cmd_in_path.sh
+++ Shell_Scripts/lib/func_is_cmd_in_path.sh
@@ -1,37 +1,39 @@
-func_load_config() {
-	# Load configuration file
-	# func_load_config config_file
-	
-	config_file=$1
-	
-	if [ "$config" = "" ]; then
-		echo "Error: missing argument - configuration file name is required"
-		exit 1
-	fi
-	
-	if ! . $config_file; then
-		echo "Error: failed to load configuration file"
-		exit 1
-	fi
-	
-	exit 0
-}
+#!/bin/bash -
+
+# Verifies that a specified program is either valid as is or can
+# be found in the PATH directory list.
+
+#echo executing ${0:2}
 
 func_is_cmd_in_path() {
 	# Given a command and the PATH, tries to find the command. Returns 0
 	# if found and executable; 1 if not. Note that this temporarily modifies
 	# the IFS (internal field separator) but restores it upon completion.
 	
+	# Note:
+	# The original script try to replace the IFS with : so that it can retrieves
+	# each directory in the for-loop, but somehow it is not working. Therefore,
+	# I have use tr command to replace : with newline character and use echo
+	# to get the list of directory in the for-loop.
+	
 	cmd=$1
+	ourpath=$2
 	mypath=$(echo $2 | tr ':' '\n')
 	result=1
-
+	#oldIFS=$IFS
+	#IFS=":"
+		
+	#for directory in "$ourpath"
 	for directory in `echo $mypath`
 	do
+		#echo "$directory/$cmd"
+		#echo ""
 		if [ -x $directory/$cmd ] ; then
 			result=0 # If we're here, we found the command.
 		fi
 	done
+	
+	#IFS=$oldIFS
 	
 	return $result
 }
