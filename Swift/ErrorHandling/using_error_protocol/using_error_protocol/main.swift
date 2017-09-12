be3e0c9ea56a96b18dc3bf105b2d170419ec5a35
choonsiong <lee93287@gmail.com>
2017-09-12 20:00:55 +0800

    First commit

--- Swift/ErrorHandling/using_error_protocol/using_error_protocol/main.swift
+++ Swift/ErrorHandling/using_error_protocol/using_error_protocol/main.swift
@@ -15,3 +15,30 @@ class Pastry {
         self.numberOnHand = numberOnHand
     }
 }
+
+class Bakery {
+    var itemsForSale = [
+        "Cookie": Pastry(flavor: "ChocolateChip", numberOnHand: 20),
+        "PopTart": Pastry(flavor: "WildBerry", numberOnHand: 13),
+        "Donut": Pastry(flavor: "Sprinkles", numberOnHand: 24),
+        "HandPie": Pastry(flavor: "Cherry", numberOnHand: 6)
+    ]
+    
+    func orderPastry(item: String, amountRequested: Int, flavor: String) throws -> Int {
+        guard let pastry = itemsForSale[item] else {
+            throw BakeryError.doNotSell
+        }
+        
+        guard flavor == pastry.flavor else {
+            throw BakeryError.wrongFlavor
+        }
+        
+        guard amountRequested < pastry.numberOnHand else {
+            throw BakeryError.tooFew(numberOnHand: pastry.numberOnHand)
+        }
+        
+        pastry.numberOnHand -= amountRequested
+        
+        return pastry.numberOnHand
+    }
+}
