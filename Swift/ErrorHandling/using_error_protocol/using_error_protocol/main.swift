011b775c0ec6193e0f38a238a0f09bfecd3de93a
choonsiong <lee93287@gmail.com>
2017-09-12 19:56:24 +0800

    First commit

--- Swift/ErrorHandling/using_error_protocol/using_error_protocol/main.swift
+++ Swift/ErrorHandling/using_error_protocol/using_error_protocol/main.swift
@@ -1,1 +1,17 @@
 import Foundation
+
+enum BakeryError: Error {
+    case tooFew(numberOnHand: Int)
+    case doNotSell
+    case wrongFlavor
+}
+
+class Pastry {
+    let flavor: String
+    var numberOnHand: Int
+    
+    init(flavor: String, numberOnHand: Int) {
+        self.flavor = flavor
+        self.numberOnHand = numberOnHand
+    }
+}
