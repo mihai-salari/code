6032a0d7ba7cdaf60fc7034b2146dd3c98e07230
choonsiong <lee93287@gmail.com>
2017-09-12 23:00:59 +0800

    First commit

---
+++ Swift/ErrorHandling/example_pugbot/example_pugbot/PugBot.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+class PugBot {
+    let name: String
+    let correctPath: [Direction]
+    private var currentStepInPath = 0
+    
+    init(name: String, correctPath: [Direction]) {
+        self.correctPath = correctPath
+        self.name = name
+    }
+    
+    func turnLeft() throws {
+        guard currentStepInPath < correctPath.count else {
+            throw PugBotError.endOfPath
+        }
+        
+        let nextDirection = correctPath[currentStepInPath]
+        
+        guard nextDirection == .left else {
+            throw PugBotError.invalidMove(found: .left, expected: nextDirection)
+        }
+        
+        currentStepInPath += 1
+    }
+    
+    func turnRight() throws {
+        guard currentStepInPath < correctPath.count else {
+            throw PugBotError.endOfPath
+        }
+        
+        let nextDirection = correctPath[currentStepInPath]
+        
+        guard nextDirection == .right else {
+            throw PugBotError.invalidMove(found: .right, expected: nextDirection)
+        }
+        
+        currentStepInPath += 1
+    }
+    
+    func moveForward() throws {
+        guard currentStepInPath < correctPath.count else {
+            throw PugBotError.endOfPath
+        }
+        
+        let nextDirection
+    }
+}
