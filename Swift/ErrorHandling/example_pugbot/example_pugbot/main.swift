e54a0978549fc1f577dafd00f3290bdbf3ffd847
choonsiong <lee93287@gmail.com>
2017-09-12 23:21:44 +0800

    First commit

--- Swift/ErrorHandling/example_pugbot/example_pugbot/main.swift
+++ Swift/ErrorHandling/example_pugbot/example_pugbot/main.swift
@@ -18,3 +18,25 @@ do {
 } catch PugBotError.invalidMove {
     print("Invalid move error")
 }
+
+// Using a function that accept a function to handle error
+func moveSafely(move: () throws -> ()) -> String {
+    do {
+        try move()
+        return "Operation completed successfully"
+    } catch PugBotError.invalidMove(let found, let expected) {
+        return "The pugbot was supposed to move \(expected), but moved \(found) instead"
+    } catch PugBotError.endOfPath {
+        return "The pugbot tried to move past the end of the path"
+    } catch {
+        return "An unknown error occurred"
+    }
+}
+
+let result = moveSafely {
+    try myPug.moveForward()
+    try myPug.moveForward()
+    try myPug.moveForward()
+}
+
+print(result)
