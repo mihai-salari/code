5bfa292edd430a95dd49a6119b129a26bf4e4b15
choonsiong <lee93287@gmail.com>
2017-09-13 16:45:07 +0800

    First commit

--- Swift/MemoryManagement/unowned_reference/unowned_reference/Tutorial.swift
+++ Swift/MemoryManagement/unowned_reference/unowned_reference/Tutorial.swift
@@ -1,12 +1,16 @@
 import Foundation
 
 class Tutorial {
+    // each tutorial must has an author during this phase, it can't be nil
+    // therefore, we use unowned here instead of weak
+    unowned let author: Author
     let title: String
     let category: String
     let date: Date
     var editor: Editor?
     
-    init(title: String, category: String, date: Date) {
+    init(author: Author, title: String, category: String, date: Date) {
+        self.author = author
         self.title = title
         self.category = category
         self.date = date
--- Swift/MemoryManagement/unowned_reference/unowned_reference/main.swift
+++ Swift/MemoryManagement/unowned_reference/unowned_reference/main.swift
@@ -1,11 +1,14 @@
 import Foundation
 
 // So that we can set them to nil later
-var tutorial: Tutorial? = Tutorial(title: "Memory Management", category: "Swift", date: Date())
+var author: Author? = Author(name: "Cosmin", email: "cosmin@example.com")
+var tutorial: Tutorial? = Tutorial(author: author!, title: "Memory Management", category: "Swift", date: Date())
 var editor: Editor? = Editor(name: "Ray", email: "ray@example.com")
 
+author?.tutorial = tutorial
 tutorial?.editor = editor
 editor?.tutorial = tutorial
 
+author = nil
 tutorial = nil
 editor = nil
