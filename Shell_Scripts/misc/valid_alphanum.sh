fb4142d9112108a7d8cd37e5f3e70936fff0dcc5
choonsiong <lee93287@gmail.com>
2017-09-14 01:16:21 +0800

    First commit

--- Shell_Scripts/misc/valid_alphanum.sh
+++ Shell_Scripts/misc/valid_alphanum.sh
@@ -5,8 +5,13 @@
 valid_alphanum() {
 	# Validate arg: return 0 if all upper+lower+digits; 1 otherwise
 	
-	# Remove all unacceptable chars.
+	# Remove all unacceptable characters
 	valid_chars="$(echo $1 | sed -e 's/[^[:alnum:]]//g')"
 	
-	
+	if [ "valid_chars" = "$1" ]; then
+		return 0
+	else
+		return 1
+	fi
 }
+
