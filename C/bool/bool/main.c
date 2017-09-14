d44d65379ababe836c674b2348e7da7ec715b61f
choonsiong <lee93287@gmail.com>
2017-09-14 20:51:04 +0800

    First commit

---
+++ C/bool/bool/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, const char * argv[]) {
+    bool is_true = true;
+    
+    if (is_true) {
+        printf("It is true\n");
+    } else {
+        printf("It is not true\n");
+    }
+    
+    return 0;
+}
