e0db64d49c27375c44945832460e131eb125014c
choonsiong <lee93287@gmail.com>
2017-09-14 19:52:14 +0800

    First commit

--- C/dynamic_string/dynamic_string/main.c
+++ C/dynamic_string/dynamic_string/main.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
+    // Dynamic string using asprintf
+    
     char *fullName;
     
     asprintf(&fullName, "Albert Einstein");
-    
     printf("%s\n", fullName);
-    
     free(fullName);
+    
+    int total = 100;
+    float ratio = 1.123;
+    char *result;
+    
+    asprintf(&result, "total: %i, ratio: %f", total, ratio);
+    printf("%s\n", result);
+    free(result);
     
     return 0;
 }
