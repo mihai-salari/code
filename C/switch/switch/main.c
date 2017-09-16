267d8c8b6f550bf9bb46f7adcfcea038f9142bad
choonsiong <lee93287@gmail.com>
2017-09-16 22:27:32 +0800

    First commit

--- C/switch/switch/main.c
+++ C/switch/switch/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+enum Case {
+    A,
+    B,
+    C
+};
+
 int main(int argc, const char * argv[]) {
+    enum Case myCase = A;
+    
+    printf("myCase = %d\n", myCase);
+    
+    switch (myCase) {
+        case 0:
+            printf("A");
+            //break;
+        case 1:
+            printf("B");
+            //break;
+        case 2:
+            printf("C");
+            break;
+        default:
+            printf("Invalid case");
+            break;
+    }
+    
+    printf("\n");
     
     return 0;
 }
