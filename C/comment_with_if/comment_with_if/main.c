549349d7b04f46c304cc35ed16cca5caa3344a5e
choonsiong <lee93287@gmail.com>
2017-09-16 18:18:58 +0800

    First commit

---
+++ C/comment_with_if/comment_with_if/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+#if 0
+    printf("Hello World\n");
+#endif
+    
+#if 1
+    printf("Hello World\n");
+#endif
+    
+    return 0;
+}
