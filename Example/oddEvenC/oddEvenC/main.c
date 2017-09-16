80a173082180dfe0642cd711c01c131678ce2d3b
choonsiong <lee93287@gmail.com>
2017-09-16 22:50:52 +0800

    First commit

---
+++ Example/oddEvenC/oddEvenC/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define kMAX 100
+
+// To tell a number is even or odd
+
+int main(int argc, const char * argv[]) {
+    int number;
+    
+    for (number = 1; number <= kMAX; number++) {
+        if (number % 2 == 0) {
+            printf("%d is even\n", number);
+        } else {
+            printf("%d is odd\n", number);
+        }
+    }
+        
+    return 0;
+}
