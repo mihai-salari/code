9f733ecee42b3bd70b30292b33d475df44e42907
choonsiong <lee93287@gmail.com>
2017-09-17 04:17:36 +0800

    First commit

--- Example/nextPrimeC/nextPrimeC/main.c
+++ Example/nextPrimeC/nextPrimeC/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 // To find the next prime number greater than a specified number
 
 int main(int argc, const char * argv[]) {
+    bool isPrime;
+    int startingPoint, candidate, last, i;
+    
+    startingPoint = 1444;
+    
+    if (startingPoint < 2) {
+        candidate = 2; // 2 is the smaller prime number
+    } else if (startingPoint == 2) {
+        candidate = 3; // 3 is the next prime after 2
+    } else {
+        candidate = startingPoint;
+        
+        if (candidate % 2 == 0) { // test only odd numbers
+            candidate--;
+        }
+        
+        do {
+            // the value of candidate here will always be odd numbers after
+            // we filter out even numbers above
+            isPrime = true; // assume it is a prime
+            candidate += 2; // bump up to next number, because we minus one above, and we don't need to check for that number
+            printf("do: candidate = %d\n", candidate);
+            
+            last = sqrt(candidate);
+            printf("do: last = %d\n", last);
+            
+            // check to see candidate has any factors, from 3 to last
+            // loop through odd numbers only
+            for (i = 3; (i <= last) && isPrime; i += 2) {
+                if ((candidate % i) == 0) {
+                    isPrime = false;
+                }
+            }
+        } while (!isPrime);
+    }
+    
+    printf("The next prime after %d is %d\n", startingPoint, candidate);
     
     return 0;
 }
