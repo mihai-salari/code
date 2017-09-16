784b8b9d8ebe733111844fe75bc55802bfab572a
choonsiong <lee93287@gmail.com>
2017-09-17 05:43:58 +0800

    First commit

--- Example/sieveOfEratosthenesObjC/sieveOfEratosthenesObjC/main.m
+++ Example/sieveOfEratosthenesObjC/sieveOfEratosthenesObjC/main.m
@@ -1,5 +1,32 @@
 #import <Foundation/Foundation.h>
 
+/*
+ 
+ Sieve of Eratosthenes
+ =====================
+ 
+ 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, 5, ..., n).
+ 2. Initially let p equal to 2, the smallest prime number.
+ 3. Enumerate the multiples of p by counting to n from 2p in increments of p, 
+    and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked)
+ 4. Find the first number greater than p in the list that is not marked. If there 
+    was no such number, stop. Otherwise, let p now equal to this new number 
+    (which is the next prime), and repeat from step 3.
+ 5. When the algorithm terminates, the numbers remaining not marked in the list
+    are all the primes below n.
+ 
+ The main idea here is that every value given to p will be prime, because if it
+ were composite it would be marked as a multiple of some other, smaller prime.
+ Note that some of the numbers may be marked more than once. As a refinement,
+ it is sufficient to mark the numbers in step 3 starting from p^2, as all the smaller
+ multiples of p will have already been marked at that point. This means that the 
+ algorithm is allowed to terminate in step 4 when p^2 is greater than n.
+ 
+ In the program below, instead of marking the numbers, I removed those numbers in
+ the list and I have created a new list to hold all p.
+ 
+ */
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         int target = 121;
@@ -78,6 +105,8 @@ int main(int argc, const char * argv[]) {
             continue;
         }
         
+        // 5. When the algorithm terminates, the numbers remaining not marked in the
+        // list are all the primes below n.
         NSLog(@"Prime numbers = %@ (%ld)", resultList, [resultList count]);
     }
     
