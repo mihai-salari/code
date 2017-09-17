8f9c2c481c2dd447ac9ee655c9cf58d86f4caffc
choonsiong <lee93287@gmail.com>
2017-09-18 02:14:12 +0800

    First commit

---
+++ Apps/HelloWorldAppIcons/HelloWorldAppIcons/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorldAppIcons
+//
+//  Created by Lee Choon Siong on 2017/09/18.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Apps/HelloWorldAppIcons/HelloWorldAppIcons/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  HelloWorldAppIcons
+//
+//  Created by Lee Choon Siong on 2017/09/18.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
