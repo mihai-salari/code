73522a8d12923b01c853ac1211b17d4d306a2fac
choonsiong <lee93287@gmail.com>
2017-09-18 12:57:48 +0800

    First commit

--- Example/decimalToBinaryObjC/decimalToBinaryObjC/main.m
+++ Example/decimalToBinaryObjC/decimalToBinaryObjC/main.m
@@ -1,5 +1,16 @@
 #import <Foundation/Foundation.h>
 
+/* Algorithm:
+ To convert decimal representation to corresponding binary representation.
+ 
+ 1. If the number is even, emit a zero. If the number is odd, emit a one.
+ 2. Divide the number by two and throw away any fractional component or remainder.
+ 3. If the quotient is zero, the algorithm complete.
+ 4. If the quotient is not zero and the number is odd, insert a one before the current string.
+    If the quotient is not zero and the number is even, prefix your binary string with zero.
+ 5. Go back to step 2 and repeat.
+ */
+
 NSString *getBinaryRepresentation(int decimalNumber) {
     BOOL isComplete = NO;
     int d = decimalNumber;
