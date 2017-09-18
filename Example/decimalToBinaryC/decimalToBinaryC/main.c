0cba8cef1de38fa54dc7284f41430347d37b4297
choonsiong <lee93287@gmail.com>
2017-09-18 12:37:52 +0800

    First commit

---
+++ Example/decimalToBinaryC/decimalToBinaryC/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, const char * argv[]) {
+    bool isComplete = false;
+    int decimal = 10;
+    
+    
+    
+    while (!isComplete) {
+        
+    }
+    
+    return 0;
+}
---
+++ Example/decimalToBinaryObjC/decimalToBinaryObjC/main.m
@@ -0,0 +1,51 @@
+#import <Foundation/Foundation.h>
+
+NSString *getBinaryRepresentation(int decimalNumber) {
+    BOOL isComplete = NO;
+    int d = decimalNumber;
+    NSMutableArray *decimalNumbers = [NSMutableArray array];
+    
+    if (d % 2 == 0) {
+        [decimalNumbers addObject:@"0"];
+    } else {
+        [decimalNumbers addObject:@"1"];
+    }
+    
+    while (!isComplete) {
+        int a = d / 2;
+        d = a;
+        
+        if (a == 0) {
+            isComplete = YES;
+        } else if (a % 2 == 0) {
+            [decimalNumbers insertObject:@"0" atIndex:0];
+        } else {
+            [decimalNumbers insertObject:@"1" atIndex:0];
+        }
+    }
+    
+    int count = 0;
+    NSString *binary = [NSString string];
+    
+    while (count < [decimalNumbers count]) {
+        NSLog(@"%@", [decimalNumbers objectAtIndex:count]);
+        NSString *digit = [decimalNumbers objectAtIndex:count];
+        
+        [binary stringByAppendingString:digit];
+        
+        NSLog(@"%@", binary);
+        count++;
+    }
+    
+    NSLog(@"Decimal = %d", decimalNumber);
+    NSLog(@"Binary = %@", binary);
+    
+    return binary;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        getBinaryRepresentation(10);
+    }
+    return 0;
+}
