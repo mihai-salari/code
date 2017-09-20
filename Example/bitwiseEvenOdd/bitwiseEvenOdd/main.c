954703e5551d054edd117226f79ff2aa5513033b
choonsiong <lee93287@gmail.com>
2017-09-20 21:49:12 +0800

    First commit

--- Example/bitwiseEvenOdd/bitwiseEvenOdd/main.c
+++ Example/bitwiseEvenOdd/bitwiseEvenOdd/main.c
@@ -11,6 +11,8 @@
 int main(int argc, const char * argv[]) {
     int number = 18;
     
+    // When we bitwise AND number with 1 (e.g. 0b0000_0001), other bits are
+    // always resulted to 0 and are ignored as well.
     if((number & 1) == 0) {
         printf("even number\n");
     } else {
