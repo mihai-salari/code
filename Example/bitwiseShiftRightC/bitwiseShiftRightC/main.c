93257f8707fc72eca01aa9dcbf8806b8cdbcf10f
choonsiong <lee93287@gmail.com>
2017-09-20 23:43:38 +0800

    First commit

--- Example/bitwiseShiftRightC/bitwiseShiftRightC/main.c
+++ Example/bitwiseShiftRightC/bitwiseShiftRightC/main.c
@@ -7,24 +7,36 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, const char * argv[]) {
-    printf("int = %zu bytes\n", sizeof(int)); // 32-bits
+    printf("int = %zu bytes\n", sizeof(char)); // 32-bits
+    
+    char a = 0b01001000;
+    
+    printf("a >> 1 = %x\n", a >> 1);
+    
+    printf("a = %x\n", a);
+    printf("a = %d\n", a);
+    printf("a = %c\n", a);
     
     // logical shift right
     
-    int someValue = 99;
-    int bit30 = ((someValue & 0x0) != 0) ? 0x40000000 : 0;
+    // compute bit 6 (start with bit 0, end with bit 7)
+    char bit6 = ((a & 0x800) != 0) ? 0x40 : 0; // 0x4 = 0100, 0x8 = 1000
     
-    printf("bit30 = %x\n", bit30);
+    printf("aAnd = %x\n", a & 0x800);
+    printf("bit6 = %x\n", bit6);
     
-    // shift bits 0...30
-    someValue = (someValue & 0x7fffffff) >> 1;
+    printf("aAnd = %x\n", a & 0x7f); // 0x48
     
-    // merge in bit #30
-    someValue = someValue | bit30;
+    // shifts bits 0...6, 0x7 = 0111
+    a = (a & 0x7f) >> 1; // 0x48 -> 0x24
     
-    printf("someValue = %x\n", someValue);
+    // merge in bit #6
+    a = a | bit6;
+    
+    printf("a = %x\n", a);
     
     return 0;
 }
