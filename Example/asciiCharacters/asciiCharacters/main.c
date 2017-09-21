5d6fb2afc7f6a5217fb7b9c57eb9c6143dfb4747
choonsiong <lee93287@gmail.com>
2017-09-22 04:25:34 +0800

    First commit

---
+++ Example/asciiCharacters/asciiCharacters/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int A = 65;
+    int a = A + 32;
+    
+    printf("A is %c\n", A);
+    printf("a is %c\n", a);
+    
+    return 0;
+}
