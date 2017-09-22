053f64750a966a4748928e33f6b64ff84ef85f5f
choonsiong <lee93287@gmail.com>
2017-09-22 20:06:33 +0800

    First commit

--- Swift/PatternMatching/switch_case_partial_matching/switch_case_partial_matching/main.swift
+++ Swift/PatternMatching/switch_case_partial_matching/switch_case_partial_matching/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let coordinates = (3, 2, 5)
+let coordinates = (3, 3, 5)
 
 switch coordinates {
 case (0, 0, 0):
@@ -11,6 +11,10 @@ case (0, _, 0):
     print("on the y-axis")
 case (0, 0, _):
     print("on the z-axis")
+case let (x, y, _) where x == y:
+    print("on y = x line")
+case let (x, y, _) where y == x * x:
+    print("on y = x^2 line")
 case let (x, y, z):
     print("on x:\(x), y:\(y), z:\(z)")
 }
