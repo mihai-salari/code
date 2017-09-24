6995af2e3e145668542eb66cdd146b8658b85341
choonsiong <lee93287@gmail.com>
2017-09-24 17:45:09 +0800

    First commit

--- Game/Fly Penguin Fly/Fly Penguin Fly/GameScene.swift
+++ Game/Fly Penguin Fly/Fly Penguin Fly/GameScene.swift
@@ -2,47 +2,19 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-
-    // This function fires every time the game switches to this scene
-//    override func didMove(to view: SKView) {
-//        let mySprite = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 50, height: 50))
-//        mySprite.position = CGPoint(x: 100, y: 100) // relative to its parent node -- the scene
-//
-//        // Add the sprite node into the node tree
-//        self.addChild(mySprite)
-//
-//        // Create an action instance
-//        let demoAction = SKAction.moveBy(x: 100, y: 100, duration: 5)
-//        //mySprite.run(demoAction)
-//
-//        let scaleAction = SKAction.scale(to: 2, duration: 5)
-//        let rotateAction = SKAction.rotate(byAngle: 5, duration: 5)
-//        let srActionGroup = SKAction.group([scaleAction, rotateAction])
-//        //mySprite.run(srActionGroup)
-//
-//        let colorAction = SKAction.colorize(with: UIColor.red, colorBlendFactor: 0.5, duration: 5)
-//        //mySprite.run(colorAction)
-//
-//        let myActionSequence = SKAction.sequence([demoAction, srActionGroup, colorAction])
-//        mySprite.run(myActionSequence)
-//    }
+    let world = SKNode() // create the world as a generic node
+    let bee = SKSpriteNode(imageNamed: "bee.png")
     
-    override func didMove(to view: SKView) {
-        // Set the scene background color to sky blue
-        self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
-        
-        // Create our bee sprite node
-        let bee = SKSpriteNode(imageNamed: "bee.png")
-        //bee.size = CGSize(width: 56, height: 48)
-        bee.position = CGPoint(x: 400, y: 200)
-        self.addChild(bee)
+    func addTheFlyingBee() {
+        self.bee.position = CGPoint(x: 250, y: 250)
+        world.addChild(bee)
         
         let beeAtlas = SKTextureAtlas(named: "beeAtlas")
         let beeFrames: [SKTexture] = [beeAtlas.textureNamed("bee.png"), beeAtlas.textureNamed("beeFly.png")]
         let beeFlyAction = SKAction.animate(with: beeFrames, timePerFrame: 0.14)
         let beeRepeatFlyAction = SKAction.repeatForever(beeFlyAction)
         
-        bee.run(beeRepeatFlyAction)
+        self.bee.run(beeRepeatFlyAction)
         
         let flyRightAction = SKAction.moveBy(x: 200, y: 20, duration: 2)
         let flyLeftAction = SKAction.moveBy(x: -200, y: -20, duration: 2)
@@ -54,6 +26,22 @@ class GameScene: SKScene {
         let flightOfTheBee = SKAction.sequence([flyLeftAction, flipBeeNegative, flyRightAction, flipBeePositive])
         let neverEndingFlight = SKAction.repeatForever(flightOfTheBee)
         
-        bee.run(neverEndingFlight)
+        self.bee.run(neverEndingFlight)
+    }
+
+    // This function fires every time the game switches to this scene
+    override func didMove(to view: SKView) {
+        // Set the scene background color to sky blue
+        self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
+        
+        // Add the world node as a child of the scene
+        self.addChild(world)
+        self.addTheFlyingBee()
+    }
+    
+    override func didSimulatePhysics() {
+        let worldXPosition = -(bee.position.x * world.xScale - (self.size.width/2))
+        let worldYPosition = -(bee.position.y * world.yScale - (self.size.height/2))
+        world.position = CGPoint(x: worldXPosition, y: worldYPosition)
     }
 }
