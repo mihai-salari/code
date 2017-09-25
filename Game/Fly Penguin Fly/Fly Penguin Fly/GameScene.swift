37c8277412cba44c81f4343cc1e7a62e51d35ac3
choonsiong <lee93287@gmail.com>
2017-09-26 01:23:25 +0800

    First commit

--- Game/Fly Penguin Fly/Fly Penguin Fly/GameScene.swift
+++ Game/Fly Penguin Fly/Fly Penguin Fly/GameScene.swift
@@ -6,42 +6,44 @@ class GameScene: SKScene {
     let ground = Ground()
     let player = Player()
     
-    //let bee = SKSpriteNode(imageNamed: "bee.png")
-    /*
-    func addTheFlyingBee() {
-        self.bee.position = CGPoint(x: 250, y: 250)
-        world.addChild(bee)
-        
-        let beeAtlas = SKTextureAtlas(named: "beeAtlas")
-        let beeFrames: [SKTexture] = [beeAtlas.textureNamed("bee.png"), beeAtlas.textureNamed("beeFly.png")]
-        let beeFlyAction = SKAction.animate(with: beeFrames, timePerFrame: 0.14)
-        let beeRepeatFlyAction = SKAction.repeatForever(beeFlyAction)
-        
-        self.bee.run(beeRepeatFlyAction)
-        
-        let flyRightAction = SKAction.moveBy(x: 200, y: 20, duration: 2)
-        let flyLeftAction = SKAction.moveBy(x: -200, y: -20, duration: 2)
-        
-        // Flip the texture back and forth
-        let flipBeeNegative = SKAction.scaleX(to: -1, duration: 0)
-        let flipBeePositive = SKAction.scaleX(to: 1, duration: 0)
-        
-        let flightOfTheBee = SKAction.sequence([flyLeftAction, flipBeeNegative, flyRightAction, flipBeePositive])
-        let neverEndingFlight = SKAction.repeatForever(flightOfTheBee)
-        
-        self.bee.run(neverEndingFlight)
-    }
- */
+//    let bee = SKSpriteNode(imageNamed: "bee.png")
+
+//    func addTheFlyingBee() {
+//        self.bee.position = CGPoint(x: 250, y: 250)
+//        world.addChild(bee)
+//
+//        let beeAtlas = SKTextureAtlas(named: "beeAtlas")
+//        let beeFrames: [SKTexture] = [beeAtlas.textureNamed("bee.png"), beeAtlas.textureNamed("beeFly.png")]
+//        let beeFlyAction = SKAction.animate(with: beeFrames, timePerFrame: 0.14)
+//        let beeRepeatFlyAction = SKAction.repeatForever(beeFlyAction)
+//
+//        self.bee.run(beeRepeatFlyAction)
+//
+//        let flyRightAction = SKAction.moveBy(x: 200, y: 20, duration: 2)
+//        let flyLeftAction = SKAction.moveBy(x: -200, y: -20, duration: 2)
+//
+//        // Flip the texture back and forth
+//        let flipBeeNegative = SKAction.scaleX(to: -1, duration: 0)
+//        let flipBeePositive = SKAction.scaleX(to: 1, duration: 0)
+//
+//        let flightOfTheBee = SKAction.sequence([flyLeftAction, flipBeeNegative, flyRightAction, flipBeePositive])
+//        let neverEndingFlight = SKAction.repeatForever(flightOfTheBee)
+//
+//        self.bee.run(neverEndingFlight)
+//    }
 
     // This function fires every time the game switches to this scene
     override func didMove(to view: SKView) {
+        
         // Set the scene background color to sky blue
         self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
         
         // Add the world node as a child of the scene
         self.addChild(world)
+        
         //self.addTheFlyingBee()
         
+        // Spawn our bees
         let bee1 = Bee()
         let bee2 = Bee()
         let bee3 = Bee()
@@ -54,6 +56,7 @@ class GameScene: SKScene {
         // Position X: Negative one screen width
         // Position Y: 100 above the bottom (remember the ground's top left anchor point)
         let groundPosition = CGPoint(x: -self.size.width, y: 100)
+        //print("DEBUG: groundPosition = \(groundPosition)")
         
         // Width: 3x the width of the screen
         // Height: 0 - our child node will provide the height
@@ -78,8 +81,9 @@ class GameScene: SKScene {
         // centered
         
         //let worldXPosition = -(bee.position.x * world.xScale - (self.size.width/2))
-        let worldXPosition = -(player.position.x * world.xScale - (self.size.width/2))
         //let worldYPosition = -(bee.position.y * world.yScale - (self.size.height/2))
+        
+        let worldXPosition = -(player.position.x * world.xScale - (self.size.width/2))        
         let worldYPosition = -(player.position.y * world.yScale - (self.size.height/2))
         
         //print(world.xScale, bee.position.x, self.size.width, self.size.width/2, worldXPosition)
