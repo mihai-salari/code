572ceeea500d99d987bd971b332169441040deb2
choonsiong <lee93287@gmail.com>
2017-09-27 18:05:47 +0800

    First commit

---
+++ Swift/String/utf8/utf8/main.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let c = "\u{00bd}"
+print(c)
+
+for i in c.utf8 {
+    print("\(i): \(String(i, radix: 2))")
+}
+
+for i in 0...15 {
+    // 10...15 - A...F in hex
+    print("\(i): \(String(i, radix: 2))")
+}
+
+let c2 = "+\u{00bd}\u{21e8}\u{1f643}"
+print(c2)
+
+for i in c2.utf8 {
+    print("\(i): \(String(i, radix: 2))")
+}
