22ce5fa17d2060d344bdfbafe9ebeb041b696265
choonsiong <lee93287@gmail.com>
2017-09-30 02:01:38 +0800

    First commit

--- Swift/Protocol/Inheritance.playground/Contents.swift
+++ Swift/Protocol/Inheritance.playground/Contents.swift
@@ -9,3 +9,16 @@ protocol WheeledVehicle: Vehicle {
     var numberOfWheels: Int { get }
     var wheelSize: Double { get set }
 }
+
+class Bike: WheeledVehicle {
+    var numberOfWheels: Int = 2
+    var wheelSize: Double = 18.0
+    
+    func accelerate() {
+        print("accelerating...")
+    }
+    
+    func stop() {
+        print("stop...")
+    }
+}
