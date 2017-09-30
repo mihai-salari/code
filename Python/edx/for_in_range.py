16cafae6de7522c5c17361cfa6c53e0f8cf3e526
choonsiong <lee93287@gmail.com>
2017-09-30 19:20:03 +0800

    First commit

--- Python/edx/for_in_range.py
+++ Python/edx/for_in_range.py
@@ -29,3 +29,39 @@ for i in range(end+1):
     total += i
 
 print(total)
+
+print("-----------------")
+
+num = 10
+for num in range(5):
+    print(num)
+print(num)
+
+print("-----------------")
+
+for letter in 'hola':
+    print(letter)
+    
+print("-----------------")
+
+divisor = 2
+for num in range(0, 10, 2):
+    print(num/divisor) 
+
+print("-----------------")
+    
+school = 'Massachusetts Institute of Technology'
+numVowels = 0
+numCons = 0
+
+for char in school:
+    if char == 'a' or char == 'e' or char == 'i' \
+       or char == 'o' or char == 'u':
+        numVowels += 1
+    elif char == 'o' or char == 'M':
+        print(char)
+    else:
+        numCons -= 1
+
+print('numVowels is: ' + str(numVowels))
+print('numCons is: ' + str(numCons))
