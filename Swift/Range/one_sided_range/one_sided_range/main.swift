740479bd58e87c1dac877c148d84f4daf73e7152
choonsiong <lee93287@gmail.com>
2017-10-01 04:44:09 +0800

    First commit

--- Swift/Range/one_sided_range/one_sided_range/main.swift
+++ Swift/Range/one_sided_range/one_sided_range/main.swift
@@ -14,3 +14,41 @@ print(cities[4..<cities.endIndex]) // Swift 3
 
 print(cities[...4])
 print(cities[..<4])
+
+// Infinite sequences
+let uppercaseChars = ["A", "B", "C", "D", "E"]
+let asciiCodes = zip(65..., uppercaseChars)
+print(Array(asciiCodes))
+
+// Use in pattern matching
+func gameRank(_ index: Int) -> String {
+    switch index {
+    case ...1:
+        return "Oldie but goodie"
+    case 3...:
+        return "Meh"
+    default:
+        return "Awesome-sauce!"
+    }
+}
+
+print(gameRank(-1))
+print(gameRank(1))
+print(gameRank(2))
+print(gameRank(3))
+print(gameRank(10))
+
+func sentiment(_ rating: Double) -> String {
+    switch rating {
+    case ..<0.33:
+        return "😩"
+    case ..<0.66:
+        return "😔"
+    default:
+        return "😙"
+    }
+}
+
+print(sentiment(0.1))
+print(sentiment(0.4))
+print(sentiment(0.9))
