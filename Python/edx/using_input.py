03bc4cf698a91a58ba2a69bad6ed77bfacb06994
choonsiong <lee93287@gmail.com>
2017-09-30 17:35:20 +0800

    First commit

---
+++ Python/edx/using_input.py
@@ -0,0 +1,6 @@
+#!/Users/choonsiong/anaconda3/bin/python3
+
+name = input("Enter your name: ")
+yearOfBirth = int(input("Enter your year of birth: "))
+
+print("Hi, " + name + ". You are " + str(2017-yearOfBirth) + " years old.")
---
+++ Python/edx/using_print.py
@@ -0,0 +1,7 @@
+#!/Users/choonsiong/anaconda3/bin/python3
+
+x = 1
+a = "hello"
+
+print("test", x, "test", a)
+print("test" + str(x) + "test" + a)
