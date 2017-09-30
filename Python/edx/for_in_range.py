fe608fa72329b944cdb5fe5a979c0d9ff431b273
choonsiong <lee93287@gmail.com>
2017-09-30 18:53:10 +0800

    First commit

--- Python/edx/for_in_range.py
+++ Python/edx/for_in_range.py
@@ -12,3 +12,20 @@ print("-----------------")
 
 for number in range(1, 10, 2): # (start,end,step)
     print(number)
+
+print("-----------------")
+    
+print("Hello!")
+
+for i in range(10,0,-2):
+    print(i)
+
+print("-----------------")    
+
+total = 0
+end = 6
+
+for i in range(end+1):
+    total += i
+
+print(total)
