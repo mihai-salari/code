a040fbf422459f8f78815f92d0de4fea5ba2204c
choonsiong <lee93287@gmail.com>
2017-09-30 18:47:24 +0800

    First commit

---
+++ Python/edx/for_in_range.py
@@ -0,0 +1,14 @@
+#!/Users/choonsiong/anaconda3/bin/python3
+
+for number in range(10):
+    print(number)
+    
+print("-----------------")
+    
+for number in range(1,10):
+    print(number)
+    
+print("-----------------")
+
+for number in range(1, 10, 2): # (start,end,step)
+    print(number)
---
+++ Python/edx/loop_while.py
@@ -0,0 +1,11 @@
+#!/Users/choonsiong/anaconda3/bin/python3
+
+end = 6
+n = 1
+total = 0
+
+while n <= end:
+    total += n
+    n += 1
+
+print(total)
