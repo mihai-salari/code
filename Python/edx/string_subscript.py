487a09d23a51d22b9042bad8310dbd590d1be72c
choonsiong <lee93287@gmail.com>
2017-09-30 17:16:55 +0800

    First commit

--- Python/edx/string_subscript.py
+++ Python/edx/string_subscript.py
@@ -2,7 +2,19 @@
 
 name = "foo bar"
 print(name)
+print(name[0])
+print(name[-1])
 print(name[4:])
 print(name[1:])
 print(name[:3])
 print(name[:])
+
+print(type(name[0]))
+
+print('o' in name)
+print('low' in 'helloworld')
+
+print('world'[:-1])
+print('helloworld'[1:9])
+print('helloworld'[1:9:2])
+print('helloworld'[::-1])
