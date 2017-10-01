1e7f8beab8ddbfaedd5702ff70c112cb0df9a03b
choonsiong <lee93287@gmail.com>
2017-10-01 20:23:37 +0800

    First commit

--- Game/CharacterBattle/CharacterBattle/Characters.swift
+++ Game/CharacterBattle/CharacterBattle/Characters.swift
@@ -1,32 +1,50 @@
 import Foundation
 
-fileprivate enum GameCharacterType {
+enum GameCharacterType {
     case elf
     case giant
     case wizard
 }
 
-fileprivate protocol GameCharacter {
+protocol GameCharacter {
     var name: String { get }
-    var hitPoints: Int { get }
-    var attackPoints: Int { get }
+    var hitPoints: Int { get set }
+    var attackPoints: Int { get set }
 }
 
-fileprivate class Elf: GameCharacter {
+class Elf: GameCharacter {
     var name: String = "Elf"
     var hitPoints: Int = 3
     var attackPoints: Int = 10
+    
+    fileprivate init() { }
 }
 
-fileprivate class Giant: GameCharacter {
+class Giant: GameCharacter {
     var name: String = "Giant"
     var hitPoints: Int = 10
     var attackPoints: Int = 3
+    
+    fileprivate init() { }
 }
 
-fileprivate class Wizard: GameCharacter {
+class Wizard: GameCharacter {
     var name: String = "Wizard"
     var hitPoints: Int = 5
     var attackPoints: Int = 5
+    
+    fileprivate init() { }
 }
 
+struct GameCharacterFactor {
+    static func make(of type: GameCharacterType) -> GameCharacter {
+        switch type {
+        case .elf:
+            return Elf()
+        case .giant:
+            return Giant()
+        case .wizard:
+            return Wizard()
+        }
+    }
+}
--- Game/CharacterBattle/CharacterBattle/main.swift
+++ Game/CharacterBattle/CharacterBattle/main.swift
@@ -1,3 +1,43 @@
 import Foundation
 
+func battle(_ a: GameCharacter, vs b: GameCharacter) {
+    var loser: String = ""
+    var a = a, b = b
+    
+    while true {
+        b.hitPoints -= a.attackPoints
+        
+        if b.hitPoints <= 0 {
+            loser = b.name
+            break
+        }
+        
+        a.hitPoints -= b.attackPoints
+        
+        if a.hitPoints <= 0 {
+            loser = a.name
+            break
+        }
+    }
+    
+    print("Argh... \(loser) defeated!")
+}
 
+// We cannot instantiate like this
+//let test = Elf()
+
+// Can only use factory method to create our game characters
+let elf = GameCharacterFactor.make(of: .elf)
+let giant = GameCharacterFactor.make(of: .giant)
+let wizard = GameCharacterFactor.make(of: .wizard)
+
+battle(elf, vs: giant)
+battle(wizard, vs: giant)
+battle(wizard, vs: elf)
+
+/*
+ Argh... Giant defeated!
+ Argh... Giant defeated!
+ Argh... Elf defeated!
+ Program ended with exit code: 0
+ */
