0f399b93e1349384434acc224ae22460ef5cce71
choonsiong <lee93287@gmail.com>
2017-10-01 19:36:59 +0800

    First commit

--- Game/CharacterBattle/CharacterBattle/Characters.swift
+++ Game/CharacterBattle/CharacterBattle/Characters.swift
@@ -7,7 +7,25 @@ fileprivate enum GameCharacterType {
 }
 
 fileprivate protocol GameCharacter {
-    var name: String { get set }
-    var hitPoints: Int { get set }
-    var attackPoints: Int { get set }
+    var name: String { get }
+    var hitPoints: Int { get }
+    var attackPoints: Int { get }
+}
+
+class Elf: GameCharacter {
+    var name: String = "Elf"
+    var hitPoints: Int = 3
+    var attackPoints: Int = 10
+}
+
+class Giant: GameCharacter {
+    var name: String = "Giant"
+    var hitPoints: Int = 10
+    var attackPoints: Int = 3
+}
+
+class Wizard: GameCharacter {
+    var name: String = "Wizard"
+    var hitPoints: Int = 5
+    var attackPoints: Int = 5
 }
