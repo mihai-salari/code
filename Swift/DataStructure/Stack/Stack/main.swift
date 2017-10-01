c0e9f6aff99020d3d9563d931474823385d44687
choonsiong <lee93287@gmail.com>
2017-10-01 19:05:55 +0800

    First commit

---
+++ Swift/DataStructure/Stack/Stack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Stack
+//
+//  Created by Lee Choon Siong on 2017/10/01.
+//  Copyright © 2017 Lee Choon Siong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
