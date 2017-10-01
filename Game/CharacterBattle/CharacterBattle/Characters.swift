3b2d67e2347035b20eb60c8ec070e44b3369d4f5
choonsiong <lee93287@gmail.com>
2017-10-01 19:34:45 +0800

    First commit

--- Game/CharacterBattle/CharacterBattle/Characters.swift
+++ Game/CharacterBattle/CharacterBattle/Characters.swift
@@ -1,7 +1,13 @@
 import Foundation
 
-enum GameCharacterType {
+fileprivate enum GameCharacterType {
     case elf
     case giant
     case wizard
+}
+
+fileprivate protocol GameCharacter {
+    var name: String { get set }
+    var hitPoints: Int { get set }
+    var attackPoints: Int { get set }
 }
