19ec9e337457743d7a7c27fb0940996e3705355a
choonsiong <lee93287@gmail.com>
2017-10-01 16:37:56 +0800

    First commit

--- Swift/AccessControl/AccessControl/AccessControl/main.swift
+++ Swift/AccessControl/AccessControl/AccessControl/main.swift
@@ -76,3 +76,18 @@ print(account.balance)
 
 // Test checking account
 
+let fooCheckingAccount = CheckingAccount()
+fooCheckingAccount.deposit(amount: 500.00)
+print(fooCheckingAccount.balance)
+
+let fooCheck = fooCheckingAccount.writeCheck(amount: 100.00)!
+print(fooCheckingAccount.balance)
+
+let marryCheckingAccount = CheckingAccount()
+print(marryCheckingAccount.balance)
+
+marryCheckingAccount.deposit(fooCheck)
+print(marryCheckingAccount.balance)
+
+marryCheckingAccount.deposit(fooCheck) // Let's cheat the bank and see ;)
+print(marryCheckingAccount.balance)
