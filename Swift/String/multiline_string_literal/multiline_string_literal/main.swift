b21621858dfbf1c82dfd978eca108ba0fee743d3
choonsiong <lee93287@gmail.com>
2017-10-01 15:41:24 +0800

    First commit

---
+++ Swift/String/multiline_string_literal/multiline_string_literal/main.swift
@@ -0,0 +1,17 @@
+//
+// https://github.com/apple/swift-evolution/blob/master/proposals/0168-multi-line-string-literals.md
+//
+
+import Foundation
+
+let lyric = """
+夜空中最亮的星 ✶✶✶✶✶✶✶
+ 夜空中最亮的星 ✶✶✶✶✶✶✶
+  夜空中最亮的星 ✶✶✶✶✶✶✶
+   夜空中最亮的星 ✶✶✶✶✶✶✶
+  夜空中最亮的星 ✶✶✶✶✶✶✶
+ 夜空中最亮的星 ✶✶✶✶✶✶✶
+夜空中最亮的星 ✶✶✶✶✶✶✶
+"""
+
+print(lyric)
