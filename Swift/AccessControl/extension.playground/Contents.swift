f0841ea89ee54869fa666068ce8f5a9a4970f323
choonsiong <lee93287@gmail.com>
2017-10-01 18:14:51 +0800

    First commit

--- Swift/AccessControl/extension.playground/Contents.swift
+++ Swift/AccessControl/extension.playground/Contents.swift
@@ -1,5 +1,36 @@
-//: Playground - noun: a place where people can play
-
 import Cocoa
 
-var str = "Hello, playground"
+// In playground, all files under 'Sources' directory is treated as a single different module
+
+let account = BasicAccount()
+print(account.balance)
+
+// Test checking account
+let fooCheckingAccount = CheckingAccount()
+fooCheckingAccount.deposit(amount: 500.00)
+print(fooCheckingAccount.balance)
+
+let fooCheck = fooCheckingAccount.writeCheck(amount: 100.00)!
+print(fooCheckingAccount.balance)
+
+let marryCheckingAccount = CheckingAccount()
+print(marryCheckingAccount.balance)
+
+marryCheckingAccount.deposit(fooCheck)
+print(marryCheckingAccount.balance)
+
+marryCheckingAccount.deposit(fooCheck) // Let's cheat the bank and see ;)
+print(marryCheckingAccount.balance)
+
+let saving = SavingsAccount(interestRate: 0.28)
+saving.deposit(amount: 10)
+print(saving.balance)
+saving.deposit(amount: 10)
+print(saving.balance)
+
+class TestAccount: BasicAccount {
+    // This class located in a different module, since we are 'subclassing'
+    // BasicAccount, it is a potential to override our Super Class ability, therefore
+    // BasicAccount has to be marked as 'open'
+}
+
--- Swift/AccessControl/extension.playground/Sources/Saving.swift
+++ Swift/AccessControl/extension.playground/Sources/Saving.swift
@@ -1,2 +1,14 @@
 import Foundation
 
+public class SavingsAccount: BasicAccount {
+    var interestRate: Double
+    
+    public init(interestRate: Double) {
+        self.interestRate = interestRate
+    }
+    
+    public func processInterest() {
+        let interest = self.balance * interestRate
+        self.deposit(amount: interest)
+    }
+}
