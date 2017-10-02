4e54a86d841e148845ed84b290b8c1fd5359edd5
choonsiong <lee93287@gmail.com>
2017-10-03 04:36:09 +0800

    First commit

--- Apps/Passw0rd/Passw0rd WatchKit Extension/InterfaceController.swift
+++ Apps/Passw0rd/Passw0rd WatchKit Extension/InterfaceController.swift
@@ -1,16 +1,30 @@
-//
-//  InterfaceController.swift
-//  Passw0rd WatchKit Extension
-//
-//  Created by Lee Choon Siong on 2017/10/03.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 import WatchKit
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var passwordButton: WKInterfaceButton!
+    @IBOutlet weak var specialCharButton: WKInterfaceButton!
+    
+    var password = Password()
+    
+    lazy var specialCharacterOnFont: NSAttributedString = {
+        let boldFont = UIFont.boldSystemFont(ofSize: 15.0)
+        let textColor = UIColor(red: 0.01, green: 0.87, blue: 0.50, alpha: 1.0)
+        let attributes = [NSAttributedStringKey.font: boldFont, NSAttributedStringKey.foregroundColor: textColor]
+        let text = NSAttributedString(string: "!@#$%", attributes: attributes)
+        
+        return text
+    }()
+    
+    lazy var specialCharacterOffFont: NSAttributedString = {
+        let normalFont = UIFont.systemFont(ofSize: 15.0)
+        let textColor = UIColor(red: 0.01, green: 0.87, blue: 0.50, alpha: 0.14)
+        let attributes = [NSAttributedStringKey.font: normalFont, NSAttributedStringKey.foregroundColor: textColor]
+        let text = NSAttributedString(string: "!@#$%", attributes: attributes)
+        
+        return text
+    }()
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -21,11 +35,36 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        self.setButtonTitle(with: password.generateRandomString())
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    func generateNewPassword() -> String {
+        return password.generateRandomString()
+    }
+    
+    func setButtonTitle(with string: String) {
+        self.passwordButton.setTitle(string)
+    }
+    
+    @IBAction func newPassword() {
+        let newPassword = generateNewPassword()
+        self.setButtonTitle(with: newPassword)
+    }
+    
+    @IBAction func toggleSpecialButton(_ sender: WKInterfaceButton) {
+        if password.isSpecial {
+            print("DEBUG: turn off special characters")
+            self.specialCharButton.setAttributedTitle(specialCharacterOffFont)
+            password.isSpecial = false
+        } else {
+            print("DEBUG: turn on special characters")
+            self.specialCharButton.setAttributedTitle(specialCharacterOnFont)
+            password.isSpecial = true
+        }
+    }
 }
