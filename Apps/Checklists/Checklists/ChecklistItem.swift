71258ee3f95b0e978556871a3a894eb4a0329116
choonsiong <lee93287@gmail.com>
2017-10-03 01:54:47 +0800

    First commit

--- Apps/Checklists/Checklists/ChecklistItem.swift
+++ Apps/Checklists/Checklists/ChecklistItem.swift
@@ -1,11 +1,3 @@
-//
-//  ChecklistItem.swift
-//  Checklists
-//
-//  Created by Lee Choon Siong on 2017/07/02.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 import Foundation
 
 class ChecklistItem {
