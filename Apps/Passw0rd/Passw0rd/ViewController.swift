a2e11dadd4083daf0f81e467baecac36cb91363c
choonsiong <lee93287@gmail.com>
2017-10-03 04:19:00 +0800

    First commit

--- Apps/Passw0rd/Passw0rd/ViewController.swift
+++ Apps/Passw0rd/Passw0rd/ViewController.swift
@@ -1,25 +1,22 @@
-//
-//  ViewController.swift
-//  Passw0rd
-//
-//  Created by Lee Choon Siong on 2017/10/03.
-//  Copyright © 2017 Lee Choon Siong. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let url = Bundle.main.url(forResource: "About", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: url) {
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                self.webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
